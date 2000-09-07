9f830f98492ded4d7202044ff9629a60d73c55fd
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2000-09-07 00:19:43 +0000

    no_comment
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@193194 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -634,7 +634,7 @@ sub suggest_mount_points {
 	$part->{mntpoint} = $mnt; delete $part->{unsafeMntpoint};
 
 	#- try to find other mount points via fstab
-	fs::get_mntpoints_from_fstab([ fsedit::get_fstab(@$hds) ], $d, $uniq) if $mnt eq '/';
+	fs::get_mntpoints_from_fstab([ fsedit::get_fstab(@$hds) ], $d, $uniq) if $mnt eq '/' && $uniq;
     }
 #-    $_->{mntpoint} || fsedit::suggest_part($_, $hds) foreach @parts;
 
