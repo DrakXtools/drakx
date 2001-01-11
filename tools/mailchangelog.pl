80b8ba090bf5f5926a7dd5396ffb3387e5432865
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-01-11 11:54:57 +0000

    fix subject
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194073 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- tools/mailchangelog.pl
+++ tools/mailchangelog.pl
@@ -5,7 +5,7 @@ open F, '| /usr/sbin/sendmail -f devel@mandrakesoft.com';
 chomp($ver = <STDIN>);
 
 print F 
-q(Subject: [DrakX] DrakX snapshot #$ver uploaded
+q(Subject: [DrakX] DrakX snapshot #), $ver, q( uploaded
 From: devel@mandrakesoft.com
 To: changelog@linux-mandrake.com, install@mandrakesoft.com
 Reply-To: install@mandrakesoft.com
