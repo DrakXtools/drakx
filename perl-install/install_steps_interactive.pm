8fca7fd192d325079312e4d9b361124b539d84a5
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-02-07 16:46:39 +0000

    (choosePackages): call setSelectedFromCompssList with empty compssUsersChoice meaning max
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194298 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -416,7 +416,7 @@ sub choosePackages {
     my $b = pkgs::saveSelected($packages);
     pkgs::setSelectedFromCompssList($packages, $o->{compssUsersChoice}, $def_mark, 0);
     my $def_size = pkgs::selectedSize($packages) + 1; #- avoid division by zero.
-    my $level = pkgs::setSelectedFromCompssList($packages, $o->{compssUsersChoice}, $min_mark, 0);
+    my $level = pkgs::setSelectedFromCompssList($packages, '', $min_mark, 0);
     my $max_size = pkgs::selectedSize($packages) + 1; #- avoid division by zero.
     pkgs::restoreSelected($b);
 
