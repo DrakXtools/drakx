5ff4168c90ce0df5a1e0902f9822e1dfcb7e74d4
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-03-06 00:15:32 +0000

    use set_in_hash
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194624 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/fs.pm
+++ perl-install/fs.pm
@@ -427,7 +427,7 @@ sub write_fstab($;$$) {
 sub merge_fstabs {
     my ($fstab, $manualFstab) = @_;
     my %l; $l{$_->{device}} = $_ foreach @$manualFstab;
-    %$_ = (%$_, %{$l{$_->{device}} || next}) foreach @$fstab;
+    set_in_hash($_, $l{$_->{device}}) foreach @$fstab;
 }
 
 #sub check_mount_all_fstab($;$) {
