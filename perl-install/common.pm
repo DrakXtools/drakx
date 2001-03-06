f37970678f0882223bcbd2254c2bab7892658c02
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-03-06 00:08:45 +0000

    added setinhash
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194622 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/common.pm
+++ perl-install/common.pm
@@ -77,6 +77,7 @@ sub to_float { $_[0] =~ /(\d*(\.\d*)?)/; $1 }
 sub ikeys { my %l = @_; sort { $a <=> $b } keys %l }
 sub add2hash($$)  { my ($a, $b) = @_; while (my ($k, $v) = each %{$b || {}}) { $a->{$k} ||= $v } $a }
 sub add2hash_($$) { my ($a, $b) = @_; while (my ($k, $v) = each %{$b || {}}) { exists $a->{$k} or $a->{$k} = $v } $a }
+sub setinhash($$) { my ($a, $b) = @_; while (my ($k, $v) = each %{$b || {}}) { $a->{$k} = $v } $a }
 sub member { my $e = shift; foreach (@_) { $e eq $_ and return 1 } 0 }
 sub dirname { @_ == 1 or die "usage: dirname <name>\n"; local $_ = shift; s|[^/]*/*\s*$||; s|(.)/*$|$1|; $_ || '.' }
 sub basename { @_ == 1 or die "usage: basename <name>\n"; local $_ = shift; s|/*\s*$||; s|.*/||; $_ }
