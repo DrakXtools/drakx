35e42889375f151c20fc147b9f54b4350dddcdb6
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-03-16 17:19:31 +0000

    (headerGetEntry): use from_utf8 on group, summary and description
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194839 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/c/stuff.pm
+++ perl-install/c/stuff.pm
@@ -14,11 +14,11 @@ sub headerGetEntry {
     my ($h, $q) = @_;
     $h or log::l("empty header in headerGetEntry"), return;
     $q eq 'name' and return headerGetEntry_string($h, RPMTAG_NAME());
-    $q eq 'group' and return headerGetEntry_string($h, RPMTAG_GROUP());
+    $q eq 'group' and return from_utf8(headerGetEntry_string($h, RPMTAG_GROUP()));
     $q eq 'version' and return headerGetEntry_string($h, RPMTAG_VERSION());
     $q eq 'release' and return headerGetEntry_string($h, RPMTAG_RELEASE());
-    $q eq 'summary' and return headerGetEntry_string($h, RPMTAG_SUMMARY());
-    $q eq 'description' and return headerGetEntry_string($h, RPMTAG_DESCRIPTION());
+    $q eq 'summary' and return from_utf8(headerGetEntry_string($h, RPMTAG_SUMMARY()));
+    $q eq 'description' and return from_utf8(headerGetEntry_string($h, RPMTAG_DESCRIPTION()));
     $q eq 'arch' and return headerGetEntry_string($h, RPMTAG_ARCH());
     $q eq 'size' and return headerGetEntry_int($h, RPMTAG_SIZE());
     $q eq 'obsoletes' and return headerGetEntry_string_list($h, RPMTAG_OBSOLETES());
