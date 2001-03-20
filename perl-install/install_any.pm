839c3098c2df4de1288423c551b635d815f0e4b7
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-03-20 12:19:33 +0000

    (setPackages): install usbd in case of usb
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194875 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -303,6 +303,7 @@ sub setPackages {
 	push @{$o->{default_packages}}, "kernel-pcmcia-cs" if $o->{pcmcia};
 	push @{$o->{default_packages}}, "raidtools" if $o->{raid} && !is_empty_array_ref($o->{raid}{raid});
 	push @{$o->{default_packages}}, "lvm" if -e '/etc/lvmtab';
+	push @{$o->{default_packages}}, "usbd" if modules::get_alias("usb-interface");
 	push @{$o->{default_packages}}, "reiserfs-utils" if grep { isReiserfs($_) } @{$o->{fstab}};
 	push @{$o->{default_packages}}, "alsa", "alsa-utils" if modules::get_alias("sound-slot-0") =~ /^snd-card-/;
 
