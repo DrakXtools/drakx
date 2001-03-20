10a7d0bb9d732ef8c8ef1e94f02b3c343f83a6b9
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-03-20 17:36:05 +0000

    no wizard if warning
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@194886 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/interactive.pm
+++ perl-install/interactive.pm
@@ -91,6 +91,7 @@ sub exit { exit($_[0]) }
 #-######################################################################################
 sub ask_warn {
     my ($o, $title, $message) = @_;
+    local $::isWizard=0;
     ask_from_listf_no_check($o, $title, $message, undef, [ _("Ok") ]);
 }
 
