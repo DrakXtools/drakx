6204ebf688e546c51badc97d858fb9bfa3f6b0c5
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-04-03 12:10:48 +0000

    (setupBootloaderBefore): nicer Aurora presence detetection
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195121 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps.pm
+++ perl-install/install_steps.pm
@@ -685,9 +685,12 @@ sub setupBootloaderBefore {
 
 	require bootloader;
 	#- propose the default fb mode for kernel fb, if aurora is installed too.
-	my $aurora = pkgs::packageByName($o->{packages}, $::expert ? 'Aurora-Monitor-Traditional-WsLib' : 'Aurora-Monitor-NewStyle-WsLib');
+	my $has_aurora = do {
+	    my $p = pkgs::packageByName($o->{packages}, 'Aurora');
+	    $p && pkgs::packageFlagInstalled($p);
+	};
         bootloader::suggest($o->{prefix}, $o->{bootloader}, $o->{hds}, $o->{fstab}, install_any::kernelVersion($o),
-			    $aurora && pkgs::packageFlagInstalled($aurora) && $vga);
+			    $has_aurora && $vga);
 	bootloader::suggest_floppy($o->{bootloader}) if $o->{security} <= 3 && arch() !~ /ppc/;
 
 	$o->{bootloader}{keytable} ||= keyboard::keyboard2kmap($o->{keyboard});
