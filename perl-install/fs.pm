7d4fe6dd83b1c595e9adf35de8f4b12a5946282f
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-04-05 11:17:11 +0000

    (format_reiserfs): quietly format reiserfs
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195172 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/fs.pm
+++ perl-install/fs.pm
@@ -92,7 +92,7 @@ sub format_reiserfs($@) {
     my ($dev, @options) = @_;
 
     #TODO add -h tea
-    run_program::run("mkreiserfs", "-f", @options, devices::make($dev)) or die _("%s formatting of %s failed", "reiserfs", $dev);
+    run_program::run("mkreiserfs", "-f", "-q", @options, devices::make($dev)) or die _("%s formatting of %s failed", "reiserfs", $dev);
 }
 
 sub format_dos($@) {
