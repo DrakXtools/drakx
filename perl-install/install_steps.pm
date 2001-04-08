f43ae9ef880342c57e4e0c0842c4dfc8c36ca97e
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-04-08 22:03:03 +0000

    (afterInstallPackages): remove setting of kde charset in kdeglobals (kde is a grown-up now)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195236 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps.pm
+++ perl-install/install_steps.pm
@@ -441,9 +441,6 @@ Consoles 1,3,4,7 may also contain interesting information";
 				   $o->{packages}{mediums});
 	pkgs::saveCompssUsers($o->{prefix}, $o->{packages}, $o->{compssUsers}, $o->{compssUsersSorted});
     }
-    if (my $charset = lang::charset($o->{lang}, $o->{prefix})) {
-	eval { update_userkderc("$o->{prefix}/usr/share/config/kdeglobals", 'Locale', Charset => $charset) };
-    }
 
 #    #- update language and icons for KDE.
 #    update_userkderc($_, 'Locale', Language => "") foreach list_skels($o->{prefix}, '.kderc');
