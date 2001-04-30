faf448bff2c09e2b46797166c5cd8ba90ff44a49
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-04-30 19:46:06 +0000

    skip u14-34f on stage1, seems to be duplicate from ultrastor.o
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195568 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/modules.pm
+++ perl-install/modules.pm
@@ -421,6 +421,7 @@ my @skip_modules_on_stage1 = (
   qw(3w-xxxx pci2220i qla1280 qla2x00 i2o_block),
   qw(gdth eata eata_pio eata_dma ips),
   qw(ac3200 at1700 atp ni5010 ni52 ni65),  #- unused from Jeff
+  "u14-34f", #- duplicate from ultrastor.o
 );
 
 
