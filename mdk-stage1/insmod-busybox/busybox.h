65b8cecf4963feb4754b0275726d22df08a7ba1a
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-05-14 14:39:35 +0000

    fix conflicts with dietlibc headers
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195612 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/insmod-busybox/busybox.h
+++ mdk-stage1/insmod-busybox/busybox.h
@@ -40,7 +40,7 @@
 /* for the _syscall() macros */
 #include <sys/syscall.h>
 #include <linux/unistd.h>
-
+#include <linux/kernel.h>
 
 /* Some useful definitions */
 #define FALSE   ((int) 1)
@@ -438,21 +438,21 @@ int nfsmount(const char *spec, const char *node, int *flags,
 
 /* Include our own copy of struct sysinfo to avoid binary compatability
  * problems with Linux 2.4, which changed things.  Grumble, grumble. */
-struct sysinfo {
-	long uptime;			/* Seconds since boot */
-	unsigned long loads[3];		/* 1, 5, and 15 minute load averages */
-	unsigned long totalram;		/* Total usable main memory size */
-	unsigned long freeram;		/* Available memory size */
-	unsigned long sharedram;	/* Amount of shared memory */
-	unsigned long bufferram;	/* Memory used by buffers */
-	unsigned long totalswap;	/* Total swap space size */
-	unsigned long freeswap;		/* swap space still available */
-	unsigned short procs;		/* Number of current processes */
-	unsigned long totalhigh;	/* Total high memory size */
-	unsigned long freehigh;		/* Available high memory size */
-	unsigned int mem_unit;		/* Memory unit size in bytes */
-	char _f[20-2*sizeof(long)-sizeof(int)];	/* Padding: libc5 uses this.. */
-};
+//struct sysinfo {
+//	  long uptime;			/* Seconds since boot */
+//	  unsigned long loads[3];		/* 1, 5, and 15 minute load averages */
+//	  unsigned long totalram;		/* Total usable main memory size */
+//	  unsigned long freeram;		/* Available memory size */
+//	  unsigned long sharedram;	/* Amount of shared memory */
+//	  unsigned long bufferram;	/* Memory used by buffers */
+//	  unsigned long totalswap;	/* Total swap space size */
+//	  unsigned long freeswap;		/* swap space still available */
+//	  unsigned short procs;		/* Number of current processes */
+//	  unsigned long totalhigh;	/* Total high memory size */
+//	  unsigned long freehigh;		/* Available high memory size */
+//	  unsigned int mem_unit;		/* Memory unit size in bytes */
+//	  char _f[20-2*sizeof(long)-sizeof(int)];	/* Padding: libc5 uses this.. */
+//};
 extern int sysinfo (struct sysinfo* info);
 
 /* Bit map related macros -- libc5 doens't provide these... sigh.  */
