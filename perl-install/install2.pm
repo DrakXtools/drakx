69aa34ca7eb4cb7a02531a3bf62b8524566792bb
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-05-16 11:01:58 +0000

    added icons management
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195640 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -24,7 +24,6 @@ use run_program;
 use any;
 use log;
 use fs;
-
 #-$::corporate=1;
 
 #-######################################################################################
@@ -32,31 +31,31 @@ use fs;
 #-######################################################################################
 my (%installSteps, @orderedInstallSteps);
 {    
-    my @installStepsFields = qw(text redoable onError hidden needs); 
+    my @installStepsFields = qw(text redoable onError hidden needs icon); 
     #entered reachable toBeDone next done;
     my @installSteps = (
-  selectLanguage     => [ __("Choose your language"), 1, 1, '' ],
-  selectInstallClass => [ __("Select installation class"), 1, 1, '' ],
-  setupSCSI          => [ __("Hard drive detection"), 1, 0, '' ],
-  selectMouse        => [ __("Configure mouse"), 1, 1, '', "selectInstallClass" ],
-  selectKeyboard     => [ __("Choose your keyboard"), 1, 1, '', "selectInstallClass" ],
-  miscellaneous      => [ __("Security"), 1, 1, '!$::expert' ],
-  doPartitionDisks   => [ __("Setup filesystems"), 1, 0, '', "selectInstallClass" ],
-  formatPartitions   => [ __("Format partitions"), 1, -1, '', "doPartitionDisks" ],
-  choosePackages     => [ __("Choose packages to install"), 1, -2, '!$::expert', "formatPartitions" ],
-  installPackages    => [ __("Install system"), 1, -1, '', ["formatPartitions", "selectInstallClass"] ],
-  setRootPassword    => [ __("Set root password"), 1, 1, '', "installPackages" ],
-  addUser            => [ __("Add a user"), 1, 1, '', "installPackages" ],
-  configureNetwork   => [ __("Configure networking"), 1, 1, '', "formatPartitions" ],
+  selectLanguage     => [ __("Choose your language"), 1, 1, '', '', 'default' ],
+  selectInstallClass => [ __("Select installation class"), 1, 1, '', '', 'default' ],
+  setupSCSI          => [ __("Hard drive detection"), 1, 0, '', '', 'harddrive' ],
+  selectMouse        => [ __("Configure mouse"), 1, 1, '', "selectInstallClass", 'mouse' ],
+  selectKeyboard     => [ __("Choose your keyboard"), 1, 1, '', "selectInstallClass", 'keyboard' ],
+  miscellaneous      => [ __("Security"), 1, 1, '!$::expert', '', 'security' ],
+  doPartitionDisks   => [ __("Setup filesystems"), 1, 0, '', "selectInstallClass", 'default' ],
+  formatPartitions   => [ __("Format partitions"), 1, -1, '', "doPartitionDisks", 'default' ],
+  choosePackages     => [ __("Choose packages to install"), 1, -2, '!$::expert', "formatPartitions", 'default' ],
+  installPackages    => [ __("Install system"), 1, -1, '', ["formatPartitions", "selectInstallClass"], '' ],
+  setRootPassword    => [ __("Set root password"), 1, 1, '', "installPackages", 'rootpasswd' ],
+  addUser            => [ __("Add a user"), 1, 1, '', "installPackages", 'user' ],
+  configureNetwork   => [ __("Configure networking"), 1, 1, '', "formatPartitions", 'network' ],
 #-  installCrypto      => [ __("Cryptographic"), 1, 1, '!$::expert', "configureNetwork" ],
-  summary            => [ __("Summary"), 1, 0, '', "installPackages" ],
-  configureServices  => [ __("Configure services"), 1, 1, '!$::expert', "installPackages" ],
-((arch() !~ /alpha/) && (arch() !~ /ppc/)) ? (
-  createBootdisk     => [ __("Create a bootdisk"), 1, 0, '', "installPackages" ],
-) : (),
-  setupBootloader    => [ __("Install bootloader"), 1, 1, '', "installPackages" ],
-  configureX         => [ __("Configure X"), 1, 1, '', ["formatPartitions", "setupBootloader"] ],
-  exitInstall        => [ __("Exit install"), 0, 0, '!$::expert && !$::live' ],
+  summary            => [ __("Summary"), 1, 0, '', "installPackages", 'default' ],
+  configureServices  => [ __("Configure services"), 1, 1, '!$::expert', "installPackages", 'services' ],
+if_((arch() !~ /alpha/) && (arch() !~ /ppc/),
+  createBootdisk     => [ __("Create a bootdisk"), 1, 0, '', "installPackages", 'bootdisk' ],
+),
+  setupBootloader    => [ __("Install bootloader"), 1, 1, '', "installPackages", 'bootloader' ],
+  configureX         => [ __("Configure X"), 1, 1, '', ["formatPartitions", "setupBootloader"], 'X' ],
+  exitInstall        => [ __("Exit install"), 0, 0, '!$::expert && !$::live', '', 'default' ],
 );
     for (my $i = 0; $i < @installSteps; $i += 2) {
 	my %h; @h{@installStepsFields} = @{ $installSteps[$i + 1] };
@@ -549,6 +548,7 @@ sub main {
     MAIN: for ($o->{step} = $o->{steps}{first};; $o->{step} = getNextStep()) {
 	$o->{steps}{$o->{step}}{entered}++;
 	$o->enteringStep($o->{step});
+	$o->{steps}{$o->{step}}{icon} and $o->{icon} = $o->{steps}{$o->{step}}{icon};
 	eval {
 	    &{$install2::{$o->{step}}}($clicked, $o->{steps}{$o->{step}}{entered});
 	};
