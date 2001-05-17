5b6bb9cdd48f2cb2e0a8f91a5004e4238a42baed
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-05-17 23:46:28 +0000

    plop
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195657 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ mdk-stage1/frontend-common.c
@@ -0,0 +1,43 @@
+/*
+ * Guillaume Cottenceau (gc@mandrakesoft.com)
+ *
+ * Copyright 2000 MandrakeSoft
+ *
+ * This software may be freely redistributed under the terms of the GNU
+ * public license.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "frontend.h"
+
+
+void info_message(char *msg, ...)
+{
+	va_list args;
+	va_start(args, msg);
+	vinfo_message(msg, args);
+	va_end(args);
+}
+
+void wait_message(char *msg, ...)
+{
+	va_list args;
+	va_start(args, msg);
+	vwait_message(msg, args);
+	va_end(args);
+}
+
+void error_message(char *msg, ...)
+{
+	va_list args;
+	va_start(args, msg);
+	verror_message(msg, args);
+	va_end(args);
+}
