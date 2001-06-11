de7ff22d46a08d2398fb8a45d2566a7b92ebf910
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-06-11 15:44:18 +0000

    proxy
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@195737 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl-install/proxy.pm
@@ -0,0 +1,12 @@
+package proxy;
+
+sub main {
+    my ($prefix, $in, $install) = @_;
+  begin:
+    $::Wizard_no_previous = 1;
+    $in->ask_okcancel(_("Proxy configuration"), _("blabla proxy"), 1) or quit_global($in, 0);
+    my $url = $in->ask_from_entry($url, _("foo"), _("url"));
+    print STDERR $url, "\n";
+    undef $::Wizard_no_previous;
+    log::l("[drakproxy] Installation complete, exiting\n");
+}
