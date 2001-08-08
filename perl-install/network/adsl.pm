96278e7ab399a0de24c7177eecce5d78a90c0026
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-08 17:23:46 +0000

    cosmetic
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196136 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/adsl.pm
+++ perl-install/network/adsl.pm
@@ -72,7 +72,7 @@ sub adsl_conf {
   adsl_conf_step_1:
     adsl_ask_info ($adsl, $netc, $intf) or return;
   adsl_conf_step_2:
-    conf_network_card ($in, $netc, $intf, 'static' , '10.0.0.10' ) or goto adsl_conf_step_1;
+    conf_network_card($in, $netc, $intf, 'static' , '10.0.0.10' ) or goto adsl_conf_step_1;
     adsl_conf_backend($adsl, $netc, $adsl_type);
 
   adsl_conf_step_3:
