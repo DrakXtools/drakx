a9ee80d5da05e794f2ebd4cf355ed205e2e35ebc
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-08 17:24:32 +0000

    adding interactive_pkgs stuff
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196138 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/standalone.pm
+++ perl-install/standalone.pm
@@ -10,11 +10,34 @@ c::setlocale();
 c::bindtextdomain('libDrakX', "/usr/share/locale");
 
 
-sub pkgs_install {
-    my ($in, @l) = @_;
-    $in->suspend;
-    my $ret = system('urpmi --auto --best-output ' . join(' ', @l));
-    $in->resume;
+
+package interactive_pkgs;
+
+sub interactive::do_pkgs {
+    my ($in) = @_;
+    bless { in => $in }, 'interactive_pkgs';
+}
+
+sub install {
+    my ($o, @l) = @_;
+    $o->{in}->suspend;
+    my $ret = system('urpmi', '--auto', '--best-output', @l);
+    $o->{in}->resume;
+    $ret;
+}
+
+sub install_if {
+    my ($o, $deps, @l) = @_;
+    my @deps = deref($deps);
+    system('rpm', '-q', @deps) == 0 or return;
+    install($o, @l);
+}
+
+sub remove {
+    my ($o, @l) = @_;
+    $o->{in}->suspend;
+    my $ret = system('rpm', '-e ', @l);
+    $o->{in}->resume;
     $ret;
 }
 
