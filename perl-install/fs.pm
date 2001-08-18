f7ca0063b7f6dbca690f9bcb50ac883af07600be
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-18 21:22:23 +0000

    (write_fstab): add notail for reiserfs partitions in any case
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196282 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/fs.pm
+++ perl-install/fs.pm
@@ -525,8 +525,7 @@ sub write_fstab($;$$@) {
 	isTrueFS($_) and ($freq, $passno) = (1, ($_->{mntpoint} eq '/') ? 1 : 2);
 	isNfs($_) and $dir = '', $options = $_->{options} || $format_options->('ro,nosuid,rsize=8192,wsize=8192', 'iocharset');
 	isFat($_) and $options = $_->{options} || $format_options->("user,exec,umask=0", 'codepage', 'iocharset');
-	
-	#isThisFs("reiserfs", $_) && $_ == fsedit::get_root($fstab, 'boot') and add_options($options, "notail");
+	isThisFs("reiserfs", $_) and add_options($options, "notail");
 	
 	my $dev = isLoopback($_) ?
 	  ($_->{mntpoint} eq '/' ? "/initrd/loopfs$_->{loopback_file}" : $_->{device}) :
