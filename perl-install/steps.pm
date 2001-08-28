ce8280d2d3d6835c1fc89cb2d161e82cb42801d6
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-28 15:04:44 +0000

    new icons
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196448 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/steps.pm
+++ perl-install/steps.pm
@@ -11,27 +11,27 @@ use common;
     my @installStepsFields = qw(text redoable onError hidden needs icon); 
     #entered reachable toBeDone next done;
     my @installSteps = (
-  selectLanguage     => [ __("Choose your language"), 1, 1, '', '', '' ],
+  selectLanguage     => [ __("Choose your language"), 1, 1, '', '', 'language' ],
   selectInstallClass => [ __("Select installation class"), 1, 1, '', '', '' ],
   setupSCSI          => [ __("Hard drive detection"), 1, 0, '', '', 'harddrive' ],
   selectMouse        => [ __("Configure mouse"), 1, 1, '', "selectInstallClass", 'mouse' ],
   selectKeyboard     => [ __("Choose your keyboard"), 1, 1, '', "selectInstallClass", 'keyboard' ],
   miscellaneous      => [ __("Security"), 1, 1, '!$::expert', '', 'security' ],
-  doPartitionDisks   => [ __("Setup filesystems"), 1, 0, '', "selectInstallClass", '' ],
-  formatPartitions   => [ __("Format partitions"), 1, -1, '$o->{isUpgrade}', "doPartitionDisks", '' ],
-  choosePackages     => [ __("Choose packages to install"), 1, -2, '!$::expert', "formatPartitions", '' ],
+  doPartitionDisks   => [ __("Setup filesystems"), 1, 0, '', "selectInstallClass", 'partition' ],
+  formatPartitions   => [ __("Format partitions"), 1, -1, '$o->{isUpgrade}', "doPartitionDisks", 'partition' ],
+  choosePackages     => [ __("Choose packages to install"), 1, -2, '!$::expert', "formatPartitions", 'partition' ],
   installPackages    => [ __("Install system"), 1, -1, '', ["formatPartitions", "selectInstallClass"], '' ],
   setRootPassword    => [ __("Set root password"), 1, 1, '', "installPackages", 'rootpasswd' ],
   addUser            => [ __("Add a user"), 1, 1, '', "installPackages", 'user' ],
   configureNetwork   => [ __("Configure networking"), 1, 1, '', "formatPartitions", 'network' ],
-  summary            => [ __("Summary"), 1, 0, '', "installPackages", '' ],
+  summary            => [ __("Summary"), 1, 0, '', "installPackages", 'summary' ],
   configureServices  => [ __("Configure services"), 1, 1, '!$::expert', "installPackages", 'services' ],
 if_((arch() !~ /alpha/) && (arch() !~ /ppc/),
   createBootdisk     => [ __("Create a bootdisk"), 1, 0, '', "installPackages", 'bootdisk' ],
 ),
   setupBootloader    => [ __("Install bootloader"), 1, 1, '', "installPackages", 'bootloader' ],
   configureX         => [ __("Configure X"), 1, 1, '', ["formatPartitions", "setupBootloader"], 'X' ],
-  exitInstall        => [ __("Exit install"), 0, 0, '!$::expert && !$::live', '', '' ],
+  exitInstall        => [ __("Exit install"), 0, 0, '!$::expert && !$::live', '', 'exit' ],
 );
     for (my $i = 0; $i < @installSteps; $i += 2) {
 	my %h; @h{@installStepsFields} = @{ $installSteps[$i + 1] };
