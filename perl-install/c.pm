27891ce1a19eabefa17ceafa40e34046cb6f45d2
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-29 23:00:13 +0000

    better error message when function c::foo() is missing
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196488 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/c.pm
+++ perl-install/c.pm
@@ -3,10 +3,17 @@ package c; # $Id$
 use vars qw($AUTOLOAD);
 
 use c::stuff;
+use MDK::Common;
 
 sub AUTOLOAD {
     $AUTOLOAD =~ /::(.*)/;
-    goto &{$c::stuff::{$1}};
+    my @l = eval { &{$c::stuff::{$1}} };
+    if (my $err = $@) {
+	$err =~ /Undefined subroutine &main::/ ?
+	  die("can't find function $AUTOLOAD\n" . backtrace()) :
+	  die($err);	
+    }
+    wantarray ? @l : $l[0];
 }
 
 1;
