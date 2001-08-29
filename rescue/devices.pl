c78d1ee6b78e54970cbdb484261966eeec9f59e4
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-08-29 15:54:49 +0000

    cleanup, speedup, use mknod for devices
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196474 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ rescue/devices.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+@ARGV == 1 && chdir $ARGV[0] or die "usage: devices.pl <dir>\n";
+
+if ($>) { $sudo = "sudo"; $ENV{PATH} = "/sbin:/usr/sbin:$ENV{PATH}"; }
+
+foreach (<DATA>) {
+    chomp;
+    my ($typ, $maj, $min, @l) = split;
+    foreach (@l) {
+	my @l2 = do {
+	    if (my ($prefix, $ini, $end) = /(.*)(\d+)-(\d+)$/) {
+		map { "$prefix$_" } $ini .. $end;
+	    } else {
+		$_;
+	    }
+	};
+	system("$sudo mknod $_ $typ $maj " . $min++) foreach @l2;
+    }
+}
+
+__DATA__
+c   5   1 console
+b   2   0 fd0-1
+c   0   0 initctl
+c   1   2 kmem
+b   7   0 loop0-15
+c   1   1 mem
+c   1   3 null
+c   1   4 port
+b   1   1 ram
+b   1   0 ram0-19
+b   1   0 ramdisk
+c   1   8 random
+b  11   0 scd0-7
+c   0   0 stderr
+c   0   0 stdin
+c   0   0 stdout
+c   5   0 tty
+c   4   0 tty0-9
+c   4  64 ttyS0-3
+c   1   9 urandom
+c   1   5 zero
+b   3   0 hda hda1-16
+b   3  64 hdb hdb1-16
+b  22   0 hdc hdc1-16
+b  22  64 hdd hdd1-16
+b  33   0 hde hde1-16
+b  33  64 hdf hdf1-16
+b  34   0 hdg hdg1-16
+b  34  64 hdh hdh1-16
+b   8   0 sda sda1-15 sdb sdb1-15 sdc sdc1-15 sdd sdd1-15 sde sde1-15 sdf sdf1-15 sdg sdg1-15 sdh sdh1-15
