9af1e166eab6d3c22a4d40746d1baa96417bbb80
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-09-03 15:32:49 +0000

    added 1920x1440 and 2048x1536.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196554 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfigurator_consts.pm
+++ perl-install/Xconfigurator_consts.pm
@@ -11,8 +11,8 @@ use common;
 );
 @depths = ikeys(%depths);
 
-@resolutions        = qw(640x480 800x600 1024x768 1152x864 1280x1024           1600x1200);
-@resolutions_laptop = qw(640x480 800x600 1024x768 1152x864 1280x1024 1400x1050 1600x1200);
+@resolutions        = qw(640x480 800x600 1024x768 1152x864 1280x1024           1600x1200 1920x1440 2048x1536);
+@resolutions_laptop = qw(640x480 800x600 1024x768 1152x864 1280x1024 1400x1050 1600x1200 1920x1440 2048x1536);
 
 @window_managers = ('kdeinit: kwin', qw(gnome-session icewm wmaker kwm afterstep fvwm fvwm2 fvwm95 mwm twm enlightenment xfce blackbox sawfish));
 
