07500606228ea0fb86b961270e40960d1d5eb7a2
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-09-13 08:19:38 +0000

    disable testing with all SiS card except SiS 630 under XF4.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196760 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfigurator.pm
+++ perl-install/Xconfigurator.pm
@@ -561,7 +561,7 @@ sub testFinalConfig {
 
     #- needed for bad cards not restoring cleanly framebuffer, according to which version of XFree are used.
     my $bad_card = ($o->{card}{use_xf4} ?
-		    $o->{card}{identifier} =~ /Matrox/ :
+		    $o->{card}{identifier} =~ /Matrox|SiS.*SG86C2.5|SiS.*559[78]|SiS.*300|SiS.*540|SiS.*6C?326|SiS.*6C?236/ :
 		    $o->{card}{identifier} =~ /i740|ViRGE|Rage Mobility (?:P\/M|L) |3D Rage LT/);
     log::l("the graphic card does not like X in framebuffer") if $bad_card;
 
