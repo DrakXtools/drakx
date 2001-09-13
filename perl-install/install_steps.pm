2967c0fe85302faa8b84b40197d1eeb688fdd035
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-09-13 14:04:40 +0000

    in kdmrc GreetString, magic string HOSTNAME replaced by %n
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@196773 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps.pm
+++ perl-install/install_steps.pm
@@ -446,7 +446,7 @@ Consoles 1,3,4,7 may also contain interesting information";
 #    log::l("updating kde icons according to available devices");
 #    install_any::kdeicons_postinstall($o->{prefix});
 
-    my $welcome = any::to_utf8(_("Welcome to %s", "HOSTNAME"));
+    my $welcome = any::to_utf8(_("Welcome to %s", '%n'));
     substInFile { s/^(GreetString)=.*/$1=$welcome/ } "$o->{prefix}/usr/share/config/kdm/kdmrc";
     install_any::disable_user_view($o->{prefix}) if $o->{security} >= 3 || $o->{authentication}{NIS};
     run_program::rooted($o->{prefix}, "kdeDesktopCleanup");
