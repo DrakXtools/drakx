2a56ee7ae9a778c237e5170372d7e503e38bdca2
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-10-19 17:17:36 +0000

    cleanup
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197172 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/common.pm
+++ perl-install/common.pm
@@ -33,6 +33,7 @@ sub __ { $_[0] }
 sub salt {
     my ($nb) = @_;
     require devices;
+    local *F;
     open F, devices::make("random") or die "missing random";
     my $s; read F, $s, $nb;
     local $_ = pack "b8" x $nb, unpack "b6" x $nb, $s;
