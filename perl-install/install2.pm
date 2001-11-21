6d6f3817d994a660f3e24b88b234b949b3a6fc59
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-11-21 19:42:04 +0000

    i810fb
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197283 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -303,6 +303,21 @@ sub exitInstall {
 }
 
 
+sub start_i810fb {
+
+    my ($vga) = cat_('/proc/cmdline') =~ /vga=(\S+)/;
+    return if !$vga || listlength(cat_('/proc/fb'));
+
+    my %vga_to_xres = (0x311 => '640', 0x314 => '800', 0x317 => '1024');
+    my $xres = $vga_to_xres{$vga} || '800';
+
+    log::l("trying to load i810fb module with xres <$xres> (vga was <$vga>)");
+    eval { modules::load('i810fb', undef,
+			 ("xres=$xres", 'hsync1=32', 'hsync2=48', 'vsync1=50', 'vsync2=70',  #- this sucking i810fb does not accept floating point numbers in hsync!
+			  'vram=2', 'bpp=16', 'accel=1', 'mtrr=1', 'hwcur=1', 'xcon=4')) };
+}
+
+
 #-######################################################################################
 #- MAIN
 #-######################################################################################
@@ -464,6 +479,8 @@ sub main {
     eval { ($o->{mouse}, @{$o->{wacom} = []}) = mouse::detect() } unless $o->{nomouseprobe} || $o->{mouse};
 
     $o->{lang} = lang::set($o->{lang}); #- mainly for defcfg
+
+    start_i810fb();
 
     $o->{allowFB} = listlength(cat_("/proc/fb"));
 
