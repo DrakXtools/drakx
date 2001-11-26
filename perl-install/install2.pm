d22a5b750b9a5af0c95ee4cae32ef89fa58ee609
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-11-26 16:33:30 +0000

    add $o->{excludedocs} feature
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197308 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -171,6 +171,8 @@ sub formatPartitions {
 	home mnt tmp var var/tmp var/lib var/lib/rpm var/lib/urpmi);
     mkdir "$o->{prefix}/$_", 0700 foreach qw(root root/tmp);
 
+    substInFile { s/%_excludedocs.*//; $_ .= "%_excludedocs yes\n" if eof && $o->{excludedocs} } "$o->{prefix}/etc/rpm/macros";
+
     any::rotate_logs($o->{prefix});
 
     require raid;
