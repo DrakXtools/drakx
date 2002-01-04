b614fa7ddce0b9625922ce76118241eb058309c1
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-01-04 18:13:25 +0000

    removed test code.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197524 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/crypto.pm
+++ perl-install/crypto.pm
@@ -85,18 +85,15 @@ sub getPackages {
     $crypto::host = $mirror;
 
     #- extract hdlist of crypto, then depslist.
-    eval {
-	require pkgs;
-	my $update_medium = pkgs::psUsingHdlist($prefix, 'ftp', $packages, "hdlist-updates.cz", "1u", "RPMS",
-						#"Updates for Mandrake Linux $::VERSION", 1, getFile("base/hdlist.cz", $mirror)) and
-						"Updates for Mandrake Linux 8.1", 1, getFile("base/hdlist.cz", $mirror)) and
-						  log::l("read updates hdlist");
-	#- keep in mind where is the URL prefix used according to mirror (for install_any::install_urpmi).
-	$update_medium->{prefix} = dir($mirror);
+    require pkgs;
+    my $update_medium = pkgs::psUsingHdlist($prefix, 'ftp', $packages, "hdlist-updates.cz", "1u", "RPMS",
+					    #"Updates for Mandrake Linux $::VERSION", 1, getFile("base/hdlist.cz", $mirror)) and
+					    "Updates for Mandrake Linux 8.1", 1, getFile("base/hdlist.cz", $mirror)) and
+					      log::l("read updates hdlist");
+    #- keep in mind where is the URL prefix used according to mirror (for install_any::install_urpmi).
+    $update_medium->{prefix} = dir($mirror);
 
-	return $update_medium;
-    };
-    return; #- an exception occurred, so ignore it.
+    return $update_medium;
 }
 
 sub get {
