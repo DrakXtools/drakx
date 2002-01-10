9c0b23a07359c242a70bf80391790a6f37666b3b
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-01-10 12:55:09 +0000

    corrected read_resolv => read_conf
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197568 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -550,7 +550,7 @@ sub main {
 	#- get stage1 network configuration if any.
 	log::l('found /tmp/network');
 	$o->{netc} ||= {};
-	add2hash($o->{netc}, network::read_resolv_conf('/tmp/network'));
+	add2hash($o->{netc}, network::read_conf('/tmp/network'));
 	if (my ($file) = glob_('/tmp/ifcfg-*')) {
 	    log::l("found network config file $file");
 	    my $l = network::read_interface_conf($file);
