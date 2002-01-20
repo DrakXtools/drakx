e5197149e978fddfef98f614838ddd49a6e68c43
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-01-20 21:51:07 +0000

    call msec with run_commands=0 to fix the network reloading (causing various problems, esp. "Net::FTP: Timeout" at X config step)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197642 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -305,7 +305,7 @@ sub setupBootloader {
 
     local $ENV{DRAKX_PASSWORD} = $o->{bootloader}{password};
     local $ENV{DURING_INSTALL} = 1;
-    run_program::rooted($o->{prefix}, "/usr/sbin/msec", $o->{security});
+    run_program::rooted($o->{prefix}, "/usr/sbin/msec", "-o", "run_commands=0", "-o", "log=stderr", $o->{security});
 }
 #------------------------------------------------------------------------------
 sub configureX {
