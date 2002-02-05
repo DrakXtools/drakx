3cfe46ad37f848935cb2d65e8b75216c203306bd
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-05 17:43:37 +0000

    auto_allocate for oem mode is not needed as oem script create all necessary partition.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197849 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -516,7 +516,6 @@ sub main {
     $o->{meta_class} = 'server' if $VERSION =~ /server/i;
     if ($::oem) {
 	$o->{partitioning}{use_existing_root} = 1;
-	$o->{partitioning}{auto_allocate} = 1;
 	$o->{compssListLevel} = 4;
 	push @auto, 'selectInstallClass', 'doPartitionDisks', 'choosePackages', 'configureTimezone', 'exitInstall';
     }
