64336703f8282c91dc2a6ae7bd05b507f9153ad7
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-11 22:50:46 +0000

    (@needToCopy): add "ntp"
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197906 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -39,7 +39,7 @@ XFree86-Sun XFree86-SunMono XFree86-Sun24 XFree86-FBDev XFree86-server
 XFree86 XFree86-glide-module Device3Dfx Glide_V3-DRI Glide_V5 Mesa
 
 dhcpcd pump dhcpxd dhcp-client isdn-light isdn4net isdn4k-utils dev pptp-adsl rp-pppoe ppp ypbind
-autologin speedtouch speedtouch_mgmt nfs-utils-clients samba-client
+autologin ntp speedtouch speedtouch_mgmt nfs-utils-clients samba-client
 wireless-tools wlan_cs wavelan_cs aironet_cs aironet4500_cs hermes airo orinico_cs orinico
 
 foomatic printer-utils printer-testpages gimpprint rlpr samba-client ncpfs nc
