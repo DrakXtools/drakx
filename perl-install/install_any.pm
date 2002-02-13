b9e98412a893cbdcedd6da0732b8739c189f8c13
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-13 17:51:55 +0000

    avoid generating headers for copying file in postinstall-rpm directory. make sure not to crash if a package that should have been in CD1 is somewhere else.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197937 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -41,8 +41,7 @@ my $cdrom = undef;
 sub useMedium($) {
     #- before ejecting the first CD, there are some files to copy!
     #- does nothing if the function has already been called.
-    #$_[0] > 1 and $::o->{method} eq 'cdrom' and setup_postinstall_rpms($::o->{prefix}, $::o->{packages});
-    $_[0] > 1 and setup_postinstall_rpms($::o->{prefix}, $::o->{packages});
+    $_[0] > 1 and $::o->{method} eq 'cdrom' and setup_postinstall_rpms($::o->{prefix}, $::o->{packages});
 
     $asked_medium eq $_[0] or log::l("selecting new medium '$_[0]'");
     $asked_medium = $_[0];
@@ -169,8 +168,7 @@ sub setup_postinstall_rpms($$) {
     #- the complete filename of each package.
     #- copy the package files in the postinstall RPMS directory.
     #- last arg is default medium '' known as the CD#1.
-    pkgs::extractHeaders($prefix, \@toCopy, $packages->{mediums});
-    cp_af((map { "/tmp/image/" . relGetFile(pkgs::packageFile($_)) } @toCopy), $postinstall_rpms);
+    eval { cp_af((map { "/tmp/image/" . relGetFile(pkgs::packageFile($_)) } @toCopy), $postinstall_rpms) };
 
     log::l("copying Auto Install Floppy");
     getAndSaveInstallFloppy($::o, "$postinstall_rpms/auto_install.img");
