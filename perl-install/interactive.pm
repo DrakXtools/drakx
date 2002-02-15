714c35b89da7cbbac031abbb7865c9f542c6d320
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-15 13:25:18 +0000

    favour "combo" over "list" in any case
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@197961 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/interactive.pm
+++ perl-install/interactive.pm
@@ -261,7 +261,7 @@ sub ask_from_normalize {
 	    $e->{type} = 'iconlist' if $e->{icon2f};
 	    $e->{type} = 'treelist' if $e->{separator};
 	    add2hash_($e, { not_edit => 1 });
-	    $e->{type} ||= $e->{not_edit} && @$l == 1 ? 'list' : 'combo';
+	    $e->{type} ||= 'combo';
 	    ${$e->{val}} = $li->[0] if ($e->{type} ne 'combo' || $e->{not_edit}) && !member(${$e->{val}}, @$li);
 	    if ($e->{type} eq 'combo' && $e->{format}) {
 		my @l = map { $e->{format}->($_) } @{$e->{list}};
