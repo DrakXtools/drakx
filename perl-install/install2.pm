17c87c0f70d837335092a8d320be28eb79cc55d7
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-19 11:11:00 +0000

    anchor the -IP match at the end of the string
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198035 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -476,9 +476,9 @@ sub main {
 
     #- done after module dependencies are loaded for "vfat depends on fat"
     if ($::auto_install) {
-	if ($::auto_install =~ /-IP(\.pl)?/) {
+	if ($::auto_install =~ /-IP(\.pl)?$/) {
 	    my $ip = join('', map { sprintf "%02X", $_ } split '\.', $o->{intf}{IPADDR});
-	    $::auto_install =~ s/-IP(\.pl)?/-$ip$1/;
+	    $::auto_install =~ s/-IP(\.pl)?$/-$ip$1/;
 	}
 	require install_steps_auto_install;
 	eval { $o = $::o = install_any::loadO($o, $::auto_install) };
