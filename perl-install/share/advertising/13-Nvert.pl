74881d60c18fd959477d83f2af16bc3798be083b
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-20 10:40:29 +0000

    changed toll free
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198079 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/13-Nvert.pl
+++ perl-install/share/advertising/13-Nvert.pl
@@ -8,6 +8,6 @@ $icon_dy = 0;
 @data = (
 	 [ _("To access to more commercial informations, please contact us on the following toll free:"), 0, 0, 400, 100, 0],
 	 # This phone number has to be '1877 MDKCORP' for USA and Canada, '0821 231 231' for France. For other, I dunno! (dams)
-	 [ _("1877 MDKCORP"), 0, 120, 400, 150 ],
+	 [ _("http://www.mandrakesoft.com/sales/contact"), 0, 120, 400, 150 ],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
 	);
