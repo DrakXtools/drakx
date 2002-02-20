c0c4c75ec763508ff9911a96ee74ec9495c26df3
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-20 13:53:15 +0000

    added @allbutfbdrivers and @alldrivers for listing XFree 4 drivers.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198093 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfigurator_consts.pm
+++ perl-install/Xconfigurator_consts.pm
@@ -45,6 +45,11 @@ use common;
 @allbutfbservers = grep { arch() =~ /^sparc/ || $serversdriver{$_} ne "fbdev" } keys(%serversdriver);
 @allservers = keys(%serversdriver);
 
+@allbutfbdrivers = (arch() =~ /^sparc/ ? qw(sunbw2 suncg14 suncg3 suncg6 sunffb sunleo suntcx) :
+		    qw(apm ark chips cirrus cyrix glide i128 i740 i810 imstt mga neomagic newport nv rendition
+                       s3 s3virge savage siliconmotion sis tdfx tga trident tseng vmware)), qw(ati glint vga);
+@alldrivers = (@allbutfbdrivers, 'fbdev', 'vesa');
+
 %vgamodes = (
     '640xx8'       => 769,
     '640x480x8'    => 769,
