07c3594316b212e68f872e04cb0a7bd414e2299c
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-23 16:58:54 +0000

    handle MBR containing the same character (0 or "l")
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198182 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/partition_table_empty.pm
+++ perl-install/partition_table_empty.pm
@@ -21,14 +21,12 @@ sub read($$) {
     my ($hd, $sector) = @_;
     my $tmp;
 
-    my $magic = "\0" x 512;
-
     local *F; partition_table_raw::openit($hd, *F) or die "failed to open device";
     c::lseek_sector(fileno(F), $sector, 0) or die "reading of partition in sector $sector failed";
 
     #- check magic number
-    sysread F, $tmp, length $magic or die "error reading magic number";
-    $tmp eq $magic or die "bad magic number";
+    sysread F, $tmp, 512 or die "error reading magic number";
+    $tmp eq substr($tmp, 0, 1) x 512 or die "bad magic number";
 
     partition_table_raw::zero_MBR($hd);
 
