dd87999bca4554ac0fba8de377250f324ee2ccbb
damien <damien@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-02-25 13:51:14 +0000

    corrected advertisings
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198206 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/13-Nvert.pl
+++ perl-install/share/advertising/13-Nvert.pl
@@ -6,7 +6,7 @@ $icon = 0;
 $icon_dx = 0;
 $icon_dy = 0;
 @data = (
-	 [ _("For more information on MandrakeSoft's Professional Services and commercial offerings, please dial the toll-free number that you will find here:"), 0, 0, 400, 100, 0],
+	 [ _("For more information on MandrakeSoft's Professional Services and commercial offerings, please see the following web page:"), 0, 0, 400, 100, 0],
 	 # This phone number has to be '1877 MDKCORP' for USA and Canada, '0821 231 231' for France. For other, I dunno! (dams)
 	 [ _("http://www.mandrakesoft.com/sales/contact"), 0, 120, 400, 150 ],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
