bf91be353052d58fba4ab1c3e7c58ccc4b41d5ac
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-03-11 09:58:57 +0000

    add Rage Mobility as bad card for XF4 (was already bad card for XF3) (gégé report).
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198444 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfigurator.pm
+++ perl-install/Xconfigurator.pm
@@ -634,7 +634,7 @@ sub testFinalConfig {
 
     #- needed for bad cards not restoring cleanly framebuffer, according to which version of XFree are used.
     my $bad_card = ($o->{card}{use_xf4} ?
-		    $o->{card}{identifier} =~ /Matrox|SiS.*SG86C2.5|SiS.*559[78]|SiS.*300|SiS.*540|SiS.*6C?326|SiS.*6C?236|Tseng.*ET6\d00|Riva.*128/ :
+		    $o->{card}{identifier} =~ /Matrox|Rage Mobility [PL]|SiS.*SG86C2.5|SiS.*559[78]|SiS.*300|SiS.*540|SiS.*6C?326|SiS.*6C?236|Tseng.*ET6\d00|Riva.*128/ :
 		    $o->{card}{identifier} =~ /i740|Rage Mobility [PL]|3D Rage LT|Rage 128/);
     $::live and $bad_card ||= $o->{card}{identifier} =~ /S3.*ViRGE/;
     log::l("the graphic card does not like X in framebuffer") if $bad_card;
