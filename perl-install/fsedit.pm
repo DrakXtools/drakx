f7787c4db2a919561196febb3a9867ae13eab541
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-06-11 16:44:49 +0000

    added /tmp in @suggestions_mntpoints (as suggested by Philippe Coulon)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198623 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/fsedit.pm
+++ perl-install/fsedit.pm
@@ -42,7 +42,7 @@ foreach (values %suggestions) {
 }
 
 my @suggestions_mntpoints = (
-    "/var/ftp", "/var/www", "/boot",
+    "/var/ftp", "/var/www", "/boot", '/tmp',
     arch() =~ /sparc/ ? "/mnt/sunos" : arch() =~ /ppc/ ? "/mnt/macos" : "/mnt/windows",
 );
 
