87dd92e5c033dbd0067f5716d1ba1deacfa8fd99
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-07-04 11:26:39 +0000

    harddrake2: "the return of the vengeance son"
    - harddrake/data.pm: the data structure
    
    - harddrake/ui.pm: the ui code
    
    - standalone/service_harddrake: the init.d service (which need a few polishing (timeout, ...)
    
    - standalone/harddrake2: the ui caller which need to be dadou/ln -fied
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198659 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl-install/harddrake/data.pm
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+
+package data;
+
+use strict;
+use lib qw(/usr/lib/libDrakX);
+use vars qw(@ISA @EXPORT_OK);
+use detect_devices;
+use MDK::Common;
+
+@ISA = qw(Exporter);
+@EXPORT_OK = (qw(version tree));
+
+our ($version, $sbindir) = ("1.1.5", "/usr/sbin/");
+
+sub wait_message {}  #- needed to emulate $in required by printerdrake::auto_detect as argument
+
+# Update me each time you handle one more devices class (aka configurator)
+sub unknown {
+    grep { $_->{media_type} !~ /tape|DISPLAY|MULTIMEDIA_VIDEO|BRIDGE|NETWORK|MULTIMEDIA_AUDIO/ } detect_devices::probeall(1);
+}
+
+
+# tree format ("CLASS_ID", "type", "type_icon", configurator, detect_sub)
+# NEVER, NEVER alter CLASS_ID or you'll harddrake2 service to detect changes
+# in hw configuration ... :-(
+
+our @tree =
+    (
+	["FLOPPY","Floppy", "floppy.png", "",\&detect_devices::floppies],
+	["HARDDISK","Disk", "harddisk.png", "$sbindir/diskdrake", \&detect_devices::hds],
+	["CDROM","Cdrom", "cd.png", "", \&detect_devices::cdroms],
+	["TAPE","Tape", "tape.png", "", \&detect_devices::tapes],
+#	["CDBURNER","Cd burners", "cd.png", "", \&detect_devices::burners],
+
+	["VIDEO","Videocard", "video.png", "$sbindir/XFdrake", 
+	 sub {grep { $_->{driver} =~ /^(Card|Server):/ || $_->{media_type} =~ 'DISPLAY_VGA' } detect_devices::probeall(1) }],
+	["TV","Tvcard", "tv.png", "/usr/bin/XawTV", 
+	 sub {grep { $_->{media_type} =~ 'MULTIMEDIA_VIDEO' } detect_devices::probeall(1)}],
+	["AUDIO","Soundcard", "sound.png", "", 
+	 sub {grep { $_->{media_type} =~ 'MULTIMEDIA_AUDIO' } detect_devices::probeall(1)}],
+#	"MULTIMEDIA_AUDIO" => "/usr/bin/X11/sounddrake";
+	["WEBCAM","Webcam", "webcam.png", "", sub {}],
+	["ETHERNET","Ethernetcard", "hw_network.png", "$sbindir/draknet", sub {
+	    #- generic NIC detection for USB seems broken (class, subclass, 
+	    #- protocol report are not accurate) so I'll need to verify against
+	    #- known drivers :-(
+	    my @usbnet = qw/CDCEther catc kaweth pegasus usbnet/;
+	    # should be taken from detect_devices.pm or modules.pm. it's identical
+	    
+	    grep { $_->{media_type} =~ /^NETWORK/ || 
+				member($_->{driver}, @usbnet)
+			 } detect_devices::probeall(1)}],
+#	["","Tokenring cards", "Ethernetcard.png", "", \&detect_devices::getNet],
+#	["","FDDI cards", "Ethernetcard.png", "", \&detect_devices::getNet],
+#	["","Modem", "Modem.png", "", \&detect_devices::getNet],
+#	["","Isdn", "", "", \&detect_devices::getNet]
+
+	["BRIDGE","Bridge", "memory.png", "", sub {grep { $_->{media_type} =~ 'BRIDGE' } detect_devices::probeall(1)}],
+# 	["","Cpu", "cpu.png", "", sub {}],
+#	["","Memory", "memory.png", "", sub {}],
+	["UNKNOWN","Unknown/Others", "unknown.png", "" , \&unknown],
+
+	["PRINTER","Printer", "hw_printer.png", "$sbindir/printerdrake", 
+	 sub { require printerdrake; printerdrake::auto_detect(bless {}) } ],
+	["SCANNER","Scanner", "scanner.png", "$sbindir/scannerdrake",
+	 sub { require scanner; scanner::findScannerUsbport() }],
+	["MOUSE","Mouse", "hw_mouse.png", "$sbindir/mousedrake", sub { use mouse; &mouse::detect()}],
+	["JOYSTICK","Joystick", "joystick.png", "", sub {}]
+
+#	["","Ideinterface", "Ideinterface.png", "", "STORAGE_IDE"],
+#	["","Scsiinterface", "Scsiinterface.png", "", \&detect_devices::getSCSI],
+#	["","Usbinterface", "Usbinterface.png", "", \&detect_devices::usb_probe]
+	);
+
+
+1;
