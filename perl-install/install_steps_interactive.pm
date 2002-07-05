785971a4d1316935c61ee3092b5fac91ceda6607
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-07-05 12:59:23 +0000

    minimize startup: only load harddrake::bttv if the user click on "tv card" field in the hw summary window
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198697 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -47,7 +47,6 @@ use install_steps;
 use install_interactive;
 use install_any;
 use detect_devices;
-use harddrake::bttv;
 use run_program;
 use devices;
 use fsedit;
@@ -1030,7 +1029,7 @@ sub summary {
      } @sound_cards),
     if_($isa_sound_card, { label => _("Sound card"), clicked => $isa_sound_card }), 
     (map {
-{ label => _("TV card"), val => $_->{description}, clicked => \&bttv::config()} 
+{ label => _("TV card"), val => $_->{description}, clicked => sub { use harddrake::bttv; bttv::config($o) }} 
      } grep { $_->{driver} eq 'bttv' } detect_devices::probeall()),
 ]);
     install_steps::configureTimezone($o);  #- do not forget it.
