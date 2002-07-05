bbca0edcf54781230ac21241ff041e61db704aa8
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-07-05 11:34:55 +0000

    - do not use mar files for the list of modules, use list_modules.pm via kernel/modules.pl instead - use read_pcitable to parse usbtable
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@198691 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/usb-resource/update-usb-ids.pl
+++ mdk-stage1/usb-resource/update-usb-ids.pl
@@ -1,38 +1,29 @@
 #!/usr/bin/perl
 
+use strict;
+use MDK::Common;
 
-sub cat_ { local *F; open F, $_[0] or $_[1] ? die "cat of file $_[0] failed: $!\n" : return; my @l = <F>; wantarray ? @l : join '', @l }
-
-
--x "../mar/mar" or die "\t*FAILED* Sorry, need ../mar/mar binary\n";
-
-
-my @usbtable_tmp = cat_("/usr/share/ldetect-lst/usbtable");
-my @usbtable;
-foreach (@usbtable_tmp) {
-    next if /\s*#/;
-    /\s*(\S+)\s+(\S+)\s+"(\S+)"\s+"([^"]*)"/ or next;
-    push @usbtable, { 'vendor' => $1, 'id' => $2, 'module' => $3, 'description' => $4 };
-}
-
+require '/usr/bin/merge2pcitable.pl';
+my $pci = read_pcitable("/usr/share/ldetect-lst/pcitable");
+my $usb = read_pcitable("/usr/share/ldetect-lst/usbtable");
 
 print '
+
+
 struct usb_module_map {
 	unsigned short	vendor;     /* vendor */
 	unsigned short	id;         /* device */
 	const char      *name;      /* human readable name */
 	const char      *module;    /* module to load */
 };
+
 ';
 
 print "struct pci_module_map usb_pci_ids[] = {
 
 ";
 
-require '/usr/bin/merge2pcitable.pl';
-my $drivers = read_pcitable("/usr/share/ldetect-lst/pcitable");
-
-while (my ($k, $v) = each %$drivers) {
+while (my ($k, $v) = each %$pci) {
     $v->[0] =~ /^usb-|^ehci-hcd/ or next;
     $k =~ /^(....)(....)/;
     printf qq|\t{ 0x%s, 0x%s, "", "%s" },\n|,
@@ -43,27 +34,19 @@ print "};
 int usb_num_ids=sizeof(usb_pci_ids)/sizeof(struct pci_module_map);
 ";
 
-
-my @t = ('usb');
-
-
-foreach $type (@t) {
-    my $modulez;
-    foreach (glob("../../all.modules/*/${type}_modules.mar")) {
-	-f $_ or die "\t*FAILED* Sorry, need $_ mar file\n";
-	push @$modulez, (`../mar/mar -l $_`);
-    }
-
-    print "struct usb_module_map ${type}_usb_ids[] = {
+print "struct usb_module_map usb_usb_ids[] = {
 ";
-    foreach my $usbentry (@usbtable) {
-	grep(/^\t$usbentry->{'module'}\.o\s/, @$modulez) or next;
-	printf qq|\t{ %s, %s, "%s", "%s" },\n|,
-	   $usbentry->{'vendor'}, $usbentry->{'id'}, $usbentry->{'description'}, $usbentry->{'module'};
+
+my @modules = chomp_(`perl ../../kernel/modules.pl pci_modules4stage1:"network/usb disk/usb"`);
+
+    foreach my $k (sort keys %$usb) {
+	my $v = $usb->{$k};
+	member($v->[0], @modules) or next;
+	$k =~ /^(....)(....)/;
+	printf qq|\t{ 0x%s, 0x%s, "%s", "%s" },\n|,
+	  $1, $2, $v->[1], $v->[0];
     }
 
     print "};
-int ${type}_usb_num_ids=sizeof(${type}_usb_ids)/sizeof(struct usb_module_map);
+int usb_usb_num_ids=sizeof(usb_usb_ids)/sizeof(struct usb_module_map);
 ";
-
-}
