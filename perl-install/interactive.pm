b4ed898d23dc842994fc179b54c984826c814b87
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-08-09 09:24:14 +0000

    allow cancel button even for wizard.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199460 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/interactive.pm
+++ perl-install/interactive.pm
@@ -315,7 +315,7 @@ sub ask_from_normalize {
     $common->{advanced_label} ||= _("Advanced");
     $common->{advanced_label_close} ||= _("Basic");
     $common->{$_} = [ deref($common->{$_}) ] foreach qw(messages advanced_messages);
-    add2hash_($common, { ok => _("Ok"), cancel => _("Cancel") }) if !exists $common->{ok} && !$::isWizard;
+    add2hash_($common, { ok => _("Ok"), cancel => _("Cancel") }) if !exists $common->{ok}; # && !$::isWizard;
     add2hash_($common->{callbacks} ||= {}, { changed => sub {}, focus_out => sub {}, complete => sub { 0 }, canceled => sub { 0 } });
 }
 
