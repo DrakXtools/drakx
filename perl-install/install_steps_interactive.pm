ad701c45ff91833bb7428814d743768707ab9dc2
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-08-20 11:50:04 +0000

    firewire configuration is only automatic
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199628 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -321,7 +321,7 @@ sub setupSCSI {
 	my $w = $o->wait_message(_("IDE"), _("Configuring IDE"));
 	modules::load(modules::category2modules('disk/cdrom'));
     }
-    any::load_category($o, 'bus/firewire');
+    any::load_category($o, 'bus/firewire', 1);
 
     any::load_category($o, 'disk/scsi|hardware_raid', !$::expert && !$clicked, 0, $o->{pcmcia});
 
