86a3ce5d84a1ed98a4498cc742a68c478a8213d6
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-08-20 17:22:45 +0000

    - use draksound wizard - boot: only check for removed/added disks, video card, ethernet   devices and mouse
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199646 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/data.pm
+++ perl-install/harddrake/data.pm
@@ -22,24 +22,24 @@ sub unknown {
 
 our @tree =
     (
-	["FLOPPY","Floppy", "floppy.png", "",\&detect_devices::floppies],
-	["HARDDISK","Disk", "harddisk.png", "$sbindir/diskdrake", \&detect_devices::hds],
-	["CDROM","CDROM", "cd.png", "", sub { grep { !(detect_devices::isBurner($_) || detect_devices::isDvdDrive($_)) } &detect_devices::cdroms } ],
-	["BURNER","CD/DVD burners", "cd.png", "", \&detect_devices::burners],
-	["DVDROM","DVD-ROM", "cd.png", "", sub { grep { ! detect_devices::isBurner($_) } detect_devices::dvdroms}],
-	["TAPE","Tape", "tape.png", "", \&detect_devices::tapes],
-#	["CDBURNER","Cd burners", "cd.png", "", \&detect_devices::burners],
+	["FLOPPY","Floppy", "floppy.png", "",\&detect_devices::floppies, 0 ],
+	["HARDDISK","Disk", "harddisk.png", "$sbindir/diskdrake", \&detect_devices::hds, 1 ],
+	["CDROM","CDROM", "cd.png", "", sub { grep { !(detect_devices::isBurner($_) || detect_devices::isDvdDrive($_)) } &detect_devices::cdroms } , 0 ],
+	["BURNER","CD/DVD burners", "cd.png", "", \&detect_devices::burners, 0 ],
+	["DVDROM","DVD-ROM", "cd.png", "", sub { grep { ! detect_devices::isBurner($_) } detect_devices::dvdroms}, 0 ],
+	["TAPE","Tape", "tape.png", "", \&detect_devices::tapes, 0 ],
+#	["CDBURNER","Cd burners", "cd.png", "", \&detect_devices::burners, 1 ],
 
 	["VIDEO","Videocard", "video.png", "$sbindir/XFdrake", 
-	 sub { grep { $_->{driver} =~ /^(Card|Server):/ || $_->{media_type} =~ 'DISPLAY_VGA' } @devices }],
+	 sub { grep { $_->{driver} =~ /^(Card|Server):/ || $_->{media_type} =~ 'DISPLAY_VGA' } @devices }, 1 ],
 	["TV","Tvcard", "tv.png", "/usr/bin/XawTV", 
-	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_VIDEO' } @devices}],
+	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_VIDEO' } @devices}, 0 ],
 	["MULTIMEDIA_OTHER","Other MultiMedia devices", "tv.png", "", 
-	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_OTHER' } @devices}],
-	["AUDIO","Soundcard", "sound.png", "$bindir/aumix", 
-	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_AUDIO' } @devices}],
+	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_OTHER' } @devices}, 0 ],
+	["AUDIO","Soundcard", "sound.png", "$sbindir/draksound", 
+	 sub { grep { $_->{media_type} =~ 'MULTIMEDIA_AUDIO' } @devices}, 0 ],
 #	"MULTIMEDIA_AUDIO" => "/usr/bin/X11/sounddrake";
-	["WEBCAM","Webcam", "webcam.png", "", sub {}],
+	["WEBCAM","Webcam", "webcam.png", "", sub {}, 0 ],
 	["ETHERNET","Ethernetcard", "hw_network.png", "$sbindir/drakconnect", sub {
 	    #- generic NIC detection for USB seems broken (class, subclass, 
 	    #- protocol report are not accurate) so I'll need to verify against
@@ -47,35 +47,35 @@ our @tree =
 	    my @usbnet = qw/CDCEther catc kaweth pegasus usbnet/;
 	    # should be taken from detect_devices.pm or modules.pm. it's identical
 	    
-	    grep { $_->{media_type} =~ /^NETWORK/ || member($_->{driver}, @usbnet) || $_->{type} eq 'network' } @devices}],
-#	["","Tokenring cards", "Ethernetcard.png", "", \&detect_devices::getNet],
-#	["","FDDI cards", "Ethernetcard.png", "", \&detect_devices::getNet],
+	    grep { $_->{media_type} =~ /^NETWORK/ || member($_->{driver}, @usbnet) || $_->{type} eq 'network' } @devices}, 1 ],
+#	["","Tokenring cards", "Ethernetcard.png", "", \&detect_devices::getNet, 1 ],
+#	["","FDDI cards", "Ethernetcard.png", "", \&detect_devices::getNet, 1 ],
 	["MODEM","Modem", "modem.png", "", sub { require network::modem; 
 									 my $modem;
 									 network::modem::modem_detect_backend($modem);
 									 grep { $modem->{device} } @{ $modem };
-								  } ],
+								  } , 0 ],
 #	["","Isdn", "", "", \&detect_devices::getNet]
 
-	["BRIDGE","Bridge(s)", "memory.png", "", sub { grep { $_->{media_type} =~ 'BRIDGE' } @devices}],
-# 	["","Cpu", "cpu.png", "", sub {}],
-#	["","Memory", "memory.png", "", sub {}],
-	["UNKNOWN","Unknown/Others", "unknown.png", "" , \&unknown],
+	["BRIDGE","Bridge(s)", "memory.png", "", sub { grep { $_->{media_type} =~ 'BRIDGE' } @devices}, 0 ],
+# 	["","Cpu", "cpu.png", "", sub {}, 0 ],
+#	["","Memory", "memory.png", "", sub {}, 0 ],
+	["UNKNOWN","Unknown/Others", "unknown.png", "" , \&unknown, 0 ],
 
 	["PRINTER","Printer", "hw_printer.png", "$sbindir/printerdrake", 
 	 sub { 
-		require printerdrake; printerdrake::auto_detect(class_discard->new)  } ],
+		require printerdrake; printerdrake::auto_detect(class_discard->new)  } , 0 ],
 	["SCANNER","Scanner", "scanner.png", "$sbindir/scannerdrake",
 	 sub { 
-		require scanner; scanner::detect() }],
+		require scanner; scanner::detect() }, 0 ],
 	["MOUSE","Mouse", "hw_mouse.png", "$sbindir/mousedrake", sub { 
-	    require mouse; &mouse::detect() } ],
-	["JOYSTICK","Joystick", "joystick.png", "", sub {}],
+	    require mouse; &mouse::detect() } , 1 ],
+	["JOYSTICK","Joystick", "joystick.png", "", sub {}, 0 ],
 
-	["ATA_STORAGE","(E)IDE/ATA controllers", "ide_hd.png", "", sub { grep { $_->{media_type} =~ 'STORAGE_IDE' } @devices}],
-	["SCSI_CONTROLLER","SCSI controllers", "scsi.png", "", sub { grep { $_->{media_type} =~ 'STORAGE_SCSI' } @devices}],
-	["USB_CONTROLLER","USB controllers", "usb.png", "", sub { grep { $_->{media_type} =~ 'SERIAL_USB' } @devices}],
-	["SMB_CONTROLLER","SMBus controllers", "usb.png", "", sub { grep { $_->{media_type} =~ 'SERIAL_SMBUS' } @devices}],
+	["ATA_STORAGE","(E)IDE/ATA controllers", "ide_hd.png", "", sub { grep { $_->{media_type} =~ 'STORAGE_IDE' } @devices}, 0 ],
+	["SCSI_CONTROLLER","SCSI controllers", "scsi.png", "", sub { grep { $_->{media_type} =~ 'STORAGE_SCSI' } @devices}, 0 ],
+	["USB_CONTROLLER","USB controllers", "usb.png", "", sub { grep { $_->{media_type} =~ 'SERIAL_USB' } @devices}, 0 ],
+	["SMB_CONTROLLER","SMBus controllers", "usb.png", "", sub { grep { $_->{media_type} =~ 'SERIAL_SMBUS' } @devices}, 0 ],
 	);
 
 
