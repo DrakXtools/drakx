c586cf7198e9a12eaaca26bd2470e4f6fe14081d
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-08-30 07:21:10 +0000

    Added text for translators
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199888 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_newt.pm
+++ perl-install/install_steps_newt.pm
@@ -31,6 +31,7 @@ sub new($$) {
     interactive::newt->new;
 
     banner('');
+#-PO This string must fit in a 80-char wide text screen
     Newt::PushHelpLine(_("  <Tab>/<Alt-Tab> between elements  | <Space> selects | <F12> next screen "));
 
     (bless {}, ref $type || $type)->SUPER::new($o);
