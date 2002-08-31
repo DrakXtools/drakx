dd2a632212fc7b842ea9fe1ec2990914a3d3989f
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-08-31 14:05:13 +0000

    no need to translate web adresses
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199909 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/14-mdkexpert.pl
+++ perl-install/share/advertising/14-mdkexpert.pl
@@ -9,6 +9,6 @@ $icon_dy = 0;
 	 [ _("Become a MandrakeExpert"), 0, 0, 400, 20, 1],
 	 [ _("Find the solutions of your problems via MandrakeSoft's online support platform."), 0, 20, 400, 60 ],
 	 [ _("Join the MandrakeSoft support teams and the Linux Community online th share your knowledge and help your others by becoming a recognized Expert on the online tehnial support website:"), 0, 20, 400, 90 ],
-	 [ _("www.mandrakeexpert.com"), 0, 20, 400, 120],
+	 [ "www.mandrakeexpert.com", 0, 20, 400, 120],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
 	);
