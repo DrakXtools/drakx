78322b3ec9e77c5d8b825094fe0f8b146d557350
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-02 17:29:24 +0000

    small fix
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199952 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/14-mdkexpert.pl
+++ perl-install/share/advertising/14-mdkexpert.pl
@@ -8,6 +8,6 @@ $icon_dy = 0;
 @data = (
 	 [ _("Become a MandrakeExpert"), 0, 0, 400, 20, 1],
 	 [ _("Find the solutions to your problems via MandrakeSoft's online support platform"), 0, 30, 400, 0, 0 ],
-	 [ _("Join the MandrakeSoft support teams and the Linux Community online to share your knowledge and help your others by becoming a recognized Expert on the online tehnial support website:"), 0, 70, 400, 0, 0 ],
+	 [ _("Join the MandrakeSoft support teams and the Linux Community online to share your knowledge and help others by becoming a recognized Expert on the online tehnial support website:"), 0, 70, 400, 0, 0 ],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
 	);
