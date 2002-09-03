261c3c438d7bef2df3f59695ce63c3a454b180ad
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-03 14:26:16 +0000

    Add MDKclub screen and text
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@199993 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl-install/share/advertising/17-mdkclub.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$width = 400;
+$height = 270;
+$icon = 0;
+$icon_dx = 0;
+$icon_dy = 0;
+@data = (
+	 [ _("Discover MandrakeClub and Mandrake Corporate Club"), 0, 0, 400, 20, 1 ],
+	 [ _("MandrakeClub and Mandrake Corporate Club were created for business and private users of Mandrake Linux who would like to directly support their favorite Linux distribution while also receiving special privileges. If you enjoy our products, if your company benefits from our products to gain a competititve edge, if you want to support Mandrake Linux development, join MandrakeClub!"), 0, 60, 400, 20, 0 ],
+	 [ ("www.mandrakeclub.com"), 0, 180, 400, 40, 0 ],
+ [ ("www.mandrakesoft.com/corporateclub"), 0, 200, 400, 40, 0 ],
+#	 [ 'text', $x, $y, $area_width, $area_height ],
+	);
