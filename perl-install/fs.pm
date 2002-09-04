458600e627c43a1f2d08f4e01ef052810d402af9
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-04 13:39:08 +0000

    handle no options in fs::mount (is it really needed?... it should not!)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200021 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/fs.pm
+++ perl-install/fs.pm
@@ -678,7 +678,7 @@ sub mount {
     my @fs_modules = qw(vfat hfs romfs ufs reiserfs xfs jfs ext3);
 
     if (member($fs, 'smb', 'smbfs', 'nfs', 'davfs', 'ntfs') && $::isStandalone) {
-	system('mount', '-t', $fs, $dev, $where, '-o', $options) == 0 or die _("mounting partition %s in directory %s failed", $dev, $where);
+	system('mount', '-t', $fs, $dev, $where, if_($options, '-o', $options)) == 0 or die _("mounting partition %s in directory %s failed", $dev, $where);
 	return; #- do not update mtab, already done by mount(8)
     } elsif (member($fs, 'ext2', 'proc', 'usbdevfs', 'iso9660', @fs_modules)) {
 	$where =~ s|/$||;
