d7fa16b1eda54e2fb68f23f06055512d9da49cda
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-10 11:10:56 +0000

    removed old code no more used.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200192 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/c/stuff.pm
+++ perl-install/c/stuff.pm
@@ -13,22 +13,4 @@ bootstrap c::stuff $VERSION;
 sub from_utf8 { iconv($_[0], "utf-8", standard_charset()) }
 sub to_utf8 { iconv($_[0], standard_charset(), "utf-8") }
 
-sub headerGetEntry {
-    my ($h, $q) = @_;
-    $h or log::l("empty header in headerGetEntry"), return;
-    $q eq 'name' and return headerGetEntry_string($h, RPMTAG_NAME());
-    $q eq 'group' and return from_utf8(headerGetEntry_string($h, RPMTAG_GROUP()));
-    $q eq 'version' and return headerGetEntry_string($h, RPMTAG_VERSION());
-    $q eq 'release' and return headerGetEntry_string($h, RPMTAG_RELEASE());
-    $q eq 'epoch' and return headerGetEntry_int($h, RPMTAG_EPOCH());
-    $q eq 'summary' and return from_utf8(headerGetEntry_string($h, RPMTAG_SUMMARY()));
-    $q eq 'description' and return from_utf8(headerGetEntry_string($h, RPMTAG_DESCRIPTION()));
-    $q eq 'arch' and return headerGetEntry_string($h, RPMTAG_ARCH());
-    $q eq 'size' and return headerGetEntry_int($h, RPMTAG_SIZE());
-    $q eq 'obsoletes' and return headerGetEntry_string_list($h, RPMTAG_OBSOLETES());
-    $q eq 'requires' and return headerGetEntry_string_list($h, RPMTAG_REQUIRENAME());
-    $q eq 'fileflags' and return headerGetEntry_int_list($h, RPMTAG_FILEFLAGS());
-    $q eq 'filenames' and return headerGetEntry_filenames($h);
-}
-
 1;
