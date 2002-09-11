5cf914a98f81118d2e93fb60634cd651d68a24d5
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-11 16:20:02 +0000

    Update
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200221 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/09-server.pl
+++ perl-install/share/advertising/09-server.pl
@@ -7,6 +7,6 @@ $icon_dx = 0;
 $icon_dy = 0;
 @data = (
 	 [ _("Turn your machine into a reliable server"), 0, 10, 400, 20, 1],
-	 [ _("Transform your machine into a powerful Linux server in a few clicks of your mouse: Web server, mail, firewall, router, file and print server, ..."), 0, 50, 400, 60, 0],
+	 [ _("Transform your machine into a powerful Linux server with a few clicks of your mouse: Web server, mail, firewall, router, file and print server, ..."), 0, 50, 400, 60, 0],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
 	);
