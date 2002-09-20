0573ff51e366362cd4053a42719abb5ceb5cd8ea
tkamppeter <tkamppeter@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-09-20 21:29:52 +0000

    The LPD/LPRng port is 515, not 5015.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200370 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/shorewall.pm
+++ perl-install/network/shorewall.pm
@@ -10,7 +10,8 @@ use common;
 use log;
 
 my @drakgw_ports = qw(domain bootps http https 631 imap pop3 smtp nntp ntp);
-my @internal_ports = qw(631 5015 137 138 139);
+# Ports for CUPS (631), LPD/LPRng (515), SMB (137, 138, 139)
+my @internal_ports = qw(631 515 137 138 139);
 
 sub check_iptables {
     my ($in) = @_;
