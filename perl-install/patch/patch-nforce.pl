f6d54f38416fc800884ff2366ab59f5c96379ca8
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-10-02 14:37:46 +0000

    update from oem patch
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200440 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/patch/patch-nforce.pl
+++ perl-install/patch/patch-nforce.pl
@@ -10,7 +10,7 @@ undef *afterInstallPackages;
 
     my ($o) = @_;
 
-    #- workaround nforce stuff.
+    # workaround nforce stuff.
     #
     # modules.pm uses /lib/modules/VERSION/modules*map to know which
     # sound drivers to use : this'll cause i810_audio to override
@@ -25,11 +25,11 @@ undef *afterInstallPackages;
     #   for depmod :
     #   MODULE_DEVICE_TABLE (pci, <name_of_the struct pci_device_id variable>);
 
+    #- try to workaround nforce stuff.
     foreach (keys %{$o->{packages}{provides}{kernel}}) {
 	my $p = $o->{packages}{depslist}[$_];
 	my ($ext, $version, $release) = $p->name =~ /^kernel-([^\d\-]*)-?([^\-]*)\.([^\-\.]*)$/ or next;
 	-s "$o->{prefix}/lib/modules/$version-$release$ext/kernel/drivers/sound/nvaudio.o.gz" and
-	  unlink "$o->{prefix}/lib/modules/$version-$release$ext/kernel/drivers/sound/i810_audio.o.gz";
+	  run_program::rooted($o->{prefix}, "cp -f /lib/modules/$version-$release$ext/kernel/drivers/sound/nvaudio.o.gz /lib/modules/$version-$release$ext/kernel/drivers/sound/i810_audio.o.gz");
     }
-
 };
