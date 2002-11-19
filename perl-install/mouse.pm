642be58352f574e2f47228cf8bb7f01d0c234e53
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-11-19 07:07:23 +0000

    perldoc: explain basic usage of mouse.pm, regarding problems that show up in early interaction between harddrake::data and mouse
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@200677 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/mouse.pm
+++ perl-install/mouse.pm
@@ -550,3 +550,41 @@ sub test_mouse {
     $darea->signal_connect(expose_event => sub { $drawarea->() });
     $darea->size($width, $height);
 }
+
+
+=begin
+
+=head1 NAME
+
+mouse - Perl functions to handle mice
+
+=head1 SYNOPSYS
+
+   require modules;
+   require mouse;
+   modules::mergein_conf('/etc/modules.conf') if -r '/etc/modules.conf';
+   &mouse::detect();
+
+=head1 DESCRIPTION
+
+C<mouse> is a perl module used by mousedrake to detect and configure the mouse.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2000-2002 MandrakeSoft <tvignaud@mandrakesoft.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+=cut
