60a82946d113631029e0f9bd1c964d7384e72157
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-01-07 10:09:48 +0000

    simplif installed spoolers "detection"
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@201134 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/printer/main.pm
+++ perl-install/printer/main.pm
@@ -49,11 +49,7 @@ sub spooler {
 
     # LPRng is not officially supported any more since Mandrake 9.0, so
     # show it only in the spooler menu when it was manually installed.
-    my @res;
-    my @spoolers = qw(cups pdq);
-    push @spoolers, 'lprng' if files_exist(qw(/usr/lib/filters/lpf /usr/sbin/lpd));
-    push @res, $spoolers{$_}{long_name} foreach @spoolers;
-    return @res;
+    return map { $spoolers{$_}{long_name} } qw(cups pdq), if_(files_exist(qw(/usr/lib/filters/lpf /usr/sbin/lpd)), 'lprng' );
 }
 
 sub printer_type($) {
