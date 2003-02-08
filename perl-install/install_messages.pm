b6d4f7c79dc7c58050fee096f813c6e43b88401e
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-08 21:52:08 +0000

    change address for errata to reflect the future 9.1
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@201703 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_messages.pm
+++ perl-install/install_messages.pm
@@ -138,7 +138,7 @@ consult the Errata available from:
 
 Information on configuring your system is available in the post
 install chapter of the Official Mandrake Linux User's Guide.",
-N("http://www.mandrakelinux.com/en/90errata.php3"));
+N("http://www.mandrakelinux.com/en/91errata.php3"));
 }
 
 1;
