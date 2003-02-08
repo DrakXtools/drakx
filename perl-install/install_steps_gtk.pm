609f859b08efc4d4f335dc964d3e745551b7089f
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-08 20:13:37 +0000

    say we're low on resources if we have less than 70 Mb of RAM (was 60 Mb)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@201701 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_gtk.pm
+++ perl-install/install_steps_gtk.pm
@@ -172,7 +172,7 @@ sub selectLanguage {
     $o->ask_warn('',
 N("Your system is low on resources. You may have some problem installing
 Mandrake Linux. If that occurs, you can try a text install instead. For this,
-press `F1' when booting on CDROM, then enter `text'.")) if $first_time && availableRamMB() < 60; # 60MB
+press `F1' when booting on CDROM, then enter `text'.")) if $first_time && availableRamMB() < 70; # 70MB
 
 }
 
