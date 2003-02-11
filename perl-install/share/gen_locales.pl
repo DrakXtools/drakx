4dcd20e85d5e7dbc2d7a193301a6bc34750011e8
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-11 11:05:40 +0000

    update to changes in lang.pm
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@201733 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/gen_locales.pl
+++ perl-install/share/gen_locales.pl
@@ -3,14 +3,12 @@
 use MDK::Common;
 use lang;
 
-foreach (lang::list()) {
-    my $LANG = lang::lang2LANG($_);
-
-    my $c = lang::during_install__lang2charset($_) or next;
-    -e "usr/share/locale/$c" or warn("not handled language $_ ($LANG, $c)\n"), next;
-    if (my $exist = readlink "usr/share/locale/$LANG") {
-	$exist eq $c or die "symlink $LANG already exist and is $exist instead of $c\n";
+foreach (lang::list_langs()) {
+    my $c = lang::during_install__l2charset($_) or next;
+    -e "usr/share/locale/$c" or warn("not handled lang $_ (charset $c)\n"), next;
+    if (my $exist = readlink "usr/share/locale/$_") {
+	$exist eq $c or die "symlink $_ already exist and is $exist instead of $c\n";
     } else {
-	symlink $c, "usr/share/locale/$LANG" or die "can't create symlink $LANG (for $_)";
+	symlink $c, "usr/share/locale/$_" or die "can't create symlink $_";
     }
 }
