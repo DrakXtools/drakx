2b3db998cef91e83359683eba0ea8f063d010166
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-25 12:30:27 +0000

    added dmidecode in bug report.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202210 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -727,6 +727,7 @@ sub report_bug {
     join '', map { chomp; "$_\n" }
       header("lspci"), detect_devices::stringlist(),
       header("pci_devices"), cat_("/proc/bus/pci/devices"),
+      header("dmidecode"), `$ENV{LD_LOADER} dmidecode`,
       header("fdisk"), arch() =~ /ppc/ ? `$ENV{LD_LOADER} pdisk -l` : `$ENV{LD_LOADER} fdisk -l`,
       header("scsi"), cat_("/proc/scsi/scsi"),
       header("lsmod"), cat_("/proc/modules"),
