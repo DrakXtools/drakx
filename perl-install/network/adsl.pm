f882a27690ba5ea5ee2718b82216c8b7fdb0408f
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-25 16:47:04 +0000

    cleaned a bit the code.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202222 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/adsl.pm
+++ perl-install/network/adsl.pm
@@ -72,22 +72,22 @@ If you don't know, choose 'use pppoe'"), $l) or return;
 sub adsl_ask_info {
     my ($adsl, $netc, $_intf, $adsl_type) = @_;
     my $pppoe_file = "/etc/ppp/pppoe.conf";
-    my $pppoe_conf = { getVarsFromSh($pppoe_file) } if $adsl_type =~ /pppoe/ && -f $pppoe_file;
-    my $login = $pppoe_conf->{USER};
+    my %pppoe_conf; %pppoe_conf = getVarsFromSh($pppoe_file) if $adsl_type =~ /pppoe/ && -f $pppoe_file;
+    my $login = $pppoe_conf{USER};
     foreach (qw(/etc/ppp/peers/adsl /etc/ppp/options)) {
 	next if $login && ! -r $_;
 	($login) = map { if_(/^user\s+\"([^\"]+)\"/, $1) } cat_($_);
     }
-    my $passwd = passwd_by_login($pppoe_conf->{USER});
+    my $passwd = passwd_by_login($pppoe_conf{USER});
     foreach (qw(/etc/ppp/pap-secrets /etc/ppp/chap-secrets)) {
 	next if $passwd && ! -r $_;
 	my $qlogin = quotemeta $login;
 	($passwd) = map { if_(/^(['"]?)$qlogin\1\s+\S+\s+(['"]?)(\S*)\2/, $3) } cat_($_);
     }
-    $pppoe_conf->{DNS1} ||= '';
-    $pppoe_conf->{DNS2} ||= '';
-    add2hash($netc, { dnsServer2 => $pppoe_conf->{DNS1}, dnsServer3 => $pppoe_conf->{DNS2}, DOMAINNAME2 => '' });
-    add2hash($adsl, { login => $user, passwd => $passwd, passwd2 => '' });
+    $pppoe_conf{DNS1} ||= '';
+    $pppoe_conf{DNS2} ||= '';
+    add2hash($netc, { dnsServer2 => $pppoe_conf{DNS1}, dnsServer3 => $pppoe_conf{DNS2}, DOMAINNAME2 => '' });
+    add2hash($adsl, { login => $login, passwd => $passwd, passwd2 => '' });
     ask_info2($adsl, $netc);
 }
 
