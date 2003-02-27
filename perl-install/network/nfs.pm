92e1984b3a8d72acdab1ae5221ebb90562635f7c
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-02-27 19:38:17 +0000

    (check): start portmap if needed
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202316 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/nfs.pm
+++ perl-install/network/nfs.pm
@@ -30,6 +30,7 @@ sub to_dev_raw {
 sub check {
     my ($_class, $in) = @_;
     $in->do_pkgs->ensure_is_installed('nfs-utils-clients', '/usr/sbin/showmount');
+    system('/etc/init.d/portmap start') if system('/etc/init.d/portmap status') != 0;
 }
 
 sub find_servers {
