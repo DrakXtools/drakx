c184f0b48172c3d775b6f7e498e98fb5356bd75d
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-03-06 14:01:50 +0000

    mptscsih doesn't fit on hd.img
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202479 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/modules.pl
+++ kernel/modules.pl
@@ -39,7 +39,7 @@ my %modules_only_for_all_img = (
     qw(u14-34f), #- duplicate from ultrastor.o
     #- still used, keeping them: qw(aha1542 sym53c416),
 
-    qw(dc395x_trm BusLogic fdomain),
+    qw(dc395x_trm mptscsih BusLogic fdomain),
     qw(pci2220i eata eata_pio eata_dma),
   ],
 
