86a2d5270d437f09137168607aed835aa9c69491
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-03-06 21:08:46 +0000

    Update
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202517 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/05-desktop.pl
+++ perl-install/share/advertising/05-desktop.pl
@@ -7,6 +7,6 @@ $icon_dx = 0;
 $icon_dy = 0;
 @data = (
 	 [ _("A customizable environment"), 0, 0, 400, 20, 1],
-	 [ _("Perfectly adapt your computer to your needs thanks to the 11 available Mandrake Linux user interfaces which can be fully modified: GNOME 2.2, KDE 3.1, Window Maker, ..."), 0, 30, 400, 0, 0],
+	 [ _("Perfectly adapt your computer to your needs thanks to the 11 available Mandrake Linux user interfaces which can be fully modified: KDE 3.1, GNOME 2.2, Window Maker, ..."), 0, 30, 400, 0, 0],
 #	 [ 'text', $x, $y, $area_width, $area_height ],
 	);
