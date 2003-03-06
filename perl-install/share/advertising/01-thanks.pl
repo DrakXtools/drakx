2ed04d3b5a5b8c9b4f76d7531d5e669f2e5a3856
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-03-06 19:42:14 +0000

    Use texts from specifications
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202508 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/01-thanks.pl
+++ perl-install/share/advertising/01-thanks.pl
@@ -6,9 +6,9 @@ $icon = 0;
 $icon_dx = 0;
 $icon_dy = 0;
 @data = (
-	 [ N("Thank you for choosing Mandrake Linux 9.1"), 0, 80, 400, 0, 1],
-	 [ N("Welcome to the Open Source world"), 0, 110, 400, 0, 0],
-	 [ N("The success of MandrakeSoft is based upon the principle of Free Software. Your new operating system is the result of collaborative work on the part of the worldwide Linux Community"), 0, 140, 400, 0, 0],
+	 [ _("Thank you for choosing Mandrake Linux 9.1"), 0, 80, 400, 0, 1],
+	 [ _("Welcome to the Open Source world."), 0, 110, 400, 0, 0],
+	 [ _("The success of MandrakeSoft is based upon the principle of Free Software. Your new operating system is the result of collaborative work of the worldwide Linux Community."), 0, 140, 400, 0, 0],
 
 
 #	 [ 'text', $x, $y, $area_width, $area_height, $bold ],
--- perl-install/share/advertising/02-community.pl
+++ perl-install/share/advertising/02-community.pl
@@ -6,9 +6,10 @@ $icon = 0;
 $icon_dx = 0;
 $icon_dy = 0;
 @data = (
-	 [ N("Get involved in the Free Software world"), 0, 30, 400, 0, 1],
-	 [ N("Want to know more about the Open Source community?"), 0, 90, 400, 20, 0],
-	 [ N("To share your own knowledge and help build Linux tools, join the discussion forums you'll find on our \"Community\" webpages"), 0, 150, 400, 0, 0],
+	 [ _("Build the future of Linux!"), 0, 30, 400, 0, 1],
+	 [ _("Want to know more and to contribute to the Open Source community? Get involved in the Free Software world!"), 0, 90, 400, 20, 0],
+	 [ _("To share your own knowledge and help build Linux software, join our discussion forums on our \"Community\" webpages."), 0, 150, 400, 0, 0],
+	 [ ("www.mandrakesoft.com/company/community"), 0, 180, 400, 0, 0],
 
 
 #	 [ 'text', $x, $y, $area_width, $area_height, $bold ],
