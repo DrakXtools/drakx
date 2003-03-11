4c9527c54ae1c19d57964964ee99fcd2ab9a3058
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-03-11 14:09:13 +0000

    install additional locales package for country according to the locale that will be really used, not according to the default locale for a given country (fixing unecessary installing of locales-de in case of a fr_CH install)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@202576 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -898,7 +898,7 @@ sub summary {
 	val => sub { lang::c2name($o->{locale}{country}) },
 	clicked => sub {
 	    any::selectCountry($o, $o->{locale}) or return;
-	    $o->do_pkgs->install('locales-' . substr(lang::c2locale($o->{locale}{country}), 0, 2));
+	    $o->do_pkgs->install('locales-' . substr(lang::getlocale_for_country($o->{locale}{lang}, $o->{locale}{country}), 0, 2));
 	    lang::write($o->{prefix}, $o->{locale});
 	    if (!$timezone_manually_set) {
 		delete $o->{timezone};
