adbb19774f5272a4a502ee768cbc32f28e84d5a9
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-07-17 19:00:42 +0000

    (_create_dialog) default is "as much as needed" size
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@203299 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/ugtk2.pm
+++ perl-install/ugtk2.pm
@@ -345,7 +345,7 @@ sub _create_dialog {
     $dialog->signal_connect(delete_event => sub { Gtk2->main_quit });
     $dialog->set_title($title);
     $dialog->set_position('center-on-parent');  # center-on-parent doesn't work
-    $dialog->set_size_request(-1, 400) if !$o_options->{small};
+    $dialog->set_size_request(-1, $o_options->{small} ? -1 : 400);
     $dialog->set_modal(1);
     $dialog;
 }
