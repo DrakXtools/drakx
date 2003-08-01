7cafd49967aada1beef1839a883f1c807c889366
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-08-01 16:18:34 +0000

    Intel drivers don't need DontVTSwitch anymore (dixit Arnaud de Lorbeau)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@203439 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfig/card.pm
+++ perl-install/Xconfig/card.pm
@@ -294,10 +294,6 @@ sub configure {
 
     xfree_and_glx_choose($in, $card, $auto) or return;
 
-    if (member($card->{card_name}, 'Intel 845', 'Intel 85x', 'Intel 865')) {
-	$raw_X->{xfree4}->get_Section('ServerFlags')->{DontVTSwitch} = [ { val => 'yes', Option => 1 } ];
-    }
-
     $card->{prog} = install_server($card, $options, $do_pkgs);
     
     if ($card->{needVideoRam} && !$card->{VideoRam}) {
