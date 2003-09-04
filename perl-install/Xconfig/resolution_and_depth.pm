2cc7d31c3f829fc9f6aa34a507246e513c1ab34a
fpons <fpons@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-09-04 09:05:02 +0000

    XF 3.3 should use depth 16 if use_UTAH_GLX, this has been glitched in XF4 mode (so never available).
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@203922 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/Xconfig/resolution_and_depth.pm
+++ perl-install/Xconfig/resolution_and_depth.pm
@@ -84,11 +84,14 @@ sub allowed {
     if ($using_xf4 ? $card->{Driver} eq 'fbdev' : $card->{server} eq 'FBDev') {
 	push @resolution_and_depth, grep { $_->{Depth} == 16 } @bios_vga_modes;
     } elsif ($using_xf4) {
-	if ($card->{use_DRI_GLX} || $card->{use_UTAH_GLX}) {
+	if ($card->{use_DRI_GLX}) {
 	    $prefered_depth = 16;
 	    push @depths, 16;
 	    push @depths, 24 if member($card->{Driver}, 'mga', 'tdfx', 'r128', 'radeon');
 	}
+    } elsif ($card->{use_UTAH_GLX}) {
+	$prefered_depth = 16;
+	push @depths, 16;
     } else {
 	   if ($card->{server} eq 'Sun24')   { push @depths, 24, 8, 2 }
 	elsif ($card->{server} eq 'Sun')     { push @depths, 8, 2 }
