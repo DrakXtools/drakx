ec34696672046fdb092df80024ce467ffce1e0b2
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-09-09 09:08:46 +0000

    fix network install
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@203984 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps.pm
+++ perl-install/install_steps.pm
@@ -701,7 +701,6 @@ sub configureNetwork {
 	require network::netconnect;
 	$o->{netcnx}{$_} = $o->{netc}{$_} foreach qw(NET_DEVICE NET_INTERFACE);
 	$o->{netcnx}{NET_INTERFACE} and network::netconnect::set_net_conf($o->{netcnx}, $o->{netc});
-	network::netconnect::save_conf($o->{netcnx});
     }
 }
 
