5f71a36df907f65b09c7d861a0345f6bdf350141
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-10-10 11:08:42 +0000

    give more details when using class_discard
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204337 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/class_discard.pm
+++ perl-install/class_discard.pm
@@ -5,7 +5,7 @@ use log;
 sub new { bless {}, "class_discard" }
 
 sub AUTOLOAD {
-    log::l("class_discard: $AUTOLOAD called at ", caller);
+    log::l("class_discard: $AUTOLOAD called at ", caller, ", params ", join(', ', @_));
 }
 
 1;
