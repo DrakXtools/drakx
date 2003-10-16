301bc0cc6ed3c28f0d8389637531b89f04c54924
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-10-16 15:33:43 +0000

    no steps in move mode; for the moment, no logo as well, we'll see what graphical aspect look like with ln
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204418 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_gtk.pm
+++ perl-install/install_gtk.pm
@@ -225,8 +225,8 @@ sub init_sizes() {
     ($::rootwidth,  $::rootheight)    = (Gtk2::Gdk->screen_width, Gtk2::Gdk->screen_height);
     $::live and $::rootheight -= 80;
     #- ($::rootheight,  $::rootwidth)    = (min(768, $::rootheight), min(1024, $::rootwidth));
-    $::stepswidth = $::rootwidth <= 640 ? 0 : 160;
-    ($::logowidth,   $::logoheight)   = $::rootwidth <= 640 ? (0, 0) : (500, 40);
+    $::move or $::stepswidth = $::rootwidth <= 640 ? 0 : 160;
+    $::move or ($::logowidth, $::logoheight) = $::rootwidth <= 640 ? (0, 0) : (500, 40);
     ($::helpwidth,   $::helpheight)   = ($::rootwidth - $::stepswidth, 0);
     ($::windowwidth, $::windowheight) = ($::rootwidth - $::stepswidth, $::rootheight - $::helpheight - $::logoheight);
 }
