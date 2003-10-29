892aa6d111868743a89dca64d8702ba83a5da328
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-10-29 13:52:28 +0000

    fix installer when network::ethernet wasn't loaded before
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204520 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/network.pm
+++ perl-install/network/network.pm
@@ -174,6 +174,7 @@ sub write_interface_conf {
 
     $intf->{BOOTPROTO} =~ s/dhcp.*/dhcp/;
 
+    require network::ethernet;
     my %cards = map { $_->[0] => $_->[1] } network::ethernet::conf_network_card_backend($netc, $intf);
     setVarsInSh($file, $intf, qw(DEVICE BOOTPROTO IPADDR NETMASK NETWORK BROADCAST ONBOOT HWADDR MII_NOT_SUPPORTED), 
                 if_(is_wireless_intf($cards{$intf->{DEVICE}}), qw(WIRELESS_MODE WIRELESS_ESSID WIRELESS_NWID WIRELESS_FREQ WIRELESS_SENS WIRELESS_RATE WIRELESS_ENC_KEY WIRELESS_RTS WIRELESS_FRAG WIRELESS_IWCONFIG WIRELESS_IWSPY WIRELESS_IWPRIV)),
