b0f0bbccad6dde85c0881666fb820e2377ba0d55
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-10-30 19:14:13 +0000

    fix OOo startup
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204557 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- move/move.pm
+++ move/move.pm
@@ -30,7 +30,7 @@ sub init {
       foreach qw(alternatives shadow man.config services shells pam.d security inputrc ld.so.conf 
                  DIR_COLORS bashrc profile profile.d rc.d init.d devfsd.conf devfs gtk-2.0 pango fonts modules.devfs 
                  dynamic gnome-vfs-2.0 gnome-vfs-mime-magic gtk gconf menu menu-methods nsswitch.conf default login.defs 
-                 skel ld.so.cache);
+                 skel ld.so.cache openoffice);
     symlinkf "/image/etc/X11/$_", "/etc/X11/$_"
       foreach qw(encodings.dir app-defaults applnk fs lbxproxy proxymngr rstart wmsession.d xinit.d xinit xkb xserver xsm);
 
