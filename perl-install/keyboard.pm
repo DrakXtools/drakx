08663c05ee154f4fd5b3b630cf94629b4ccf225e
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-10-30 18:46:49 +0000

    check_prog won't work if arguments are not properly splitted when passed to run_program
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204555 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/keyboard.pm
+++ perl-install/keyboard.pm
@@ -507,7 +507,7 @@ sub write {
     $keyboard->{KEYTABLE} = keyboard2kmap($keyboard);
 
     setVarsInSh("$::prefix/etc/sysconfig/keyboard", $keyboard);
-    run_program::rooted($::prefix, "dumpkeys > /etc/sysconfig/console/default.kmap") or log::l("dumpkeys failed");
+    run_program::rooted($::prefix, 'dumpkeys', '>', '/etc/sysconfig/console/default.kmap') or log::l("dumpkeys failed");
     if (arch() =~ /ppc/) {
 	my $s = "dev.mac_hid.keyboard_sends_linux_keycodes = 1\n";
 	substInFile { 
