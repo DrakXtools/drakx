da27aa7f2b16ee8dfac91c01a932a24a4e79ad8d
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-11-06 13:04:15 +0000

    have ifcfg-lo
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@204695 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- move/move.pm
+++ move/move.pm
@@ -39,12 +39,15 @@ sub init {
 
     mkdir_p("/etc/$_"), system("cp -R /image/etc/$_/* /etc/$_") foreach qw(cups profile.d sysconfig/network-scripts);
  
+    #- for /etc/sysconfig/networking/ifcfg-lo
+    mkdir "/etc/sysconfig/networking";
+
     #- ro things
     symlinkf_short("/image/etc/$_", "/etc/$_")
       foreach qw(alternatives shadow man.config services shells pam.d security inputrc ld.so.conf 
                  DIR_COLORS bashrc profile rc.d init.d devfsd.conf gtk-2.0 pango fonts modules.devfs 
                  dynamic hotplug gnome-vfs-2.0 gnome-vfs-mime-magic gtk gconf menu menu-methods nsswitch.conf default login.defs 
-                 skel ld.so.cache openoffice xinetd.d xinetd.conf syslog.conf sysctl.conf);
+                 skel ld.so.cache openoffice xinetd.d xinetd.conf syslog.conf sysctl.conf sysconfig/networking/ifcfg-lo);
     symlinkf_short("/image/etc/X11/$_", "/etc/X11/$_")
       foreach qw(encodings.dir app-defaults applnk fs lbxproxy proxymngr rstart wmsession.d xinit.d xinit xkb xserver xsm);
 
