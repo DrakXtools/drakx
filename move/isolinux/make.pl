a6c584d09752fa81de5bbd68da24f452924f6d05
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-11-28 13:48:34 +0000

    fit on 80 columns
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205130 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- move/isolinux/make.pl
+++ move/isolinux/make.pl
@@ -7,14 +7,15 @@ output 'help.msg', pack("C*", 0x0E, 0x80, 0x03, 0x00),
                   0aWelcome to 09Mandrake Move0a help07
 
 In most cases, the best way to get started is to simply press the 0e<Enter>07 key.
-If you experience problems, you can add the following options on the command line :
+If you experience problems, you can try to add on the command line :
 
  o  0fnoauto07 to disable automatic detection (generally used with 0fexpert07).
  o  0fupdatemodules07 to use the special update floppy containing modules updates.
  o  0fpatch07 to use a patch from the floppy (file named 09patch.pl07).
- o  0fcleankey07 to remove previously saved system configuration files on the USB key.
- o  0fvirtual_key=/dev/hda1,/key07 to use file 0f/key07 on device 0f/dev/hda107 as a
-    virtual key instead of a physical one (must be an existing file containing a valid FS).
+ o  0fcleankey07 to remove previously saved system config files from the USB key.
+ o  0fvirtual_key=09/dev/hda10f,09/key07 to use file 09/key07 on device 09/dev/hda107 as a
+    virtual key instead of a physical one (must be an existing file containing a
+    valid filesystem - e.g. a loopback).
 
 You can also pass some 0f<specific kernel options>07 to the Linux kernel. 
 For example, try 0flinux mem=128M07 if your system has 128Mb of RAM but we
