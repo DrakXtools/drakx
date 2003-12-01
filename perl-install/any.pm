381131007d36ce33170b935bece93295aa73cad7
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-12-01 16:22:10 +0000

    drakx_version: have a suitable value for move
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205176 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -20,7 +20,8 @@ use log;
 use c;
 
 sub drakx_version() { 
-    sprintf "DrakX v%s built %s", $::testing ? ('TEST', scalar gmtime()) : (split('/', cat_("$ENV{SHARE_PATH}/VERSION")))[2,3];
+    $::move ? sprintf "DrakX-move v%s", cat_('/usr/bin/stage2/move.pm') =~ /move\.pm,v (\S+ \S+ \S+)/
+            : sprintf "DrakX v%s built %s", $::testing ? ('TEST', scalar gmtime()) : (split('/', cat_("$ENV{SHARE_PATH}/VERSION")))[2,3];
 }
 
 sub facesdir() {
