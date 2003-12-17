21aa260ad68e5e4912ea6989c2fc054d2ddddeb4
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-12-17 10:41:56 +0000

    fix wrong message in error patch (catched by new perl_checker)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205330 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/sound.pm
+++ perl-install/harddrake/sound.pm
@@ -234,8 +234,8 @@ The new \"%s\" driver'll only be used on next bootstrap.", $driver, $new_driver)
                       [ &get_any_driver_entry($in, $driver, $device) ]);
     } else {
         $in->ask_warn(N("Unknown driver"), 
-                      N("Error: The \"%s\" driver for your sound card is unlisted"),
-                      $driver);
+                      N("Error: The \"%s\" driver for your sound card is unlisted",
+                      $driver));
     }
   end:
 }
