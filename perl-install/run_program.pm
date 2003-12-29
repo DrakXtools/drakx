10f1be96b79ca2c96146621a9bb3aa36375f5c40
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2003-12-29 07:47:36 +0000

    fix to enable drakconnect to be run in "install" mode while being a standalone tool
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205368 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/run_program.pm
+++ perl-install/run_program.pm
@@ -47,7 +47,7 @@ sub raw {
     return 1 if $root && $<;
 
     $root ? ($root .= '/') : ($root = '');
-    install_any::check_prog(ref($name) ? $name->[0] : $name) if !$root && $::isInstall;
+    install_any::check_prog(ref($name) ? $name->[0] : $name) if !$root && !$::isStandalone;
 
 
     my ($stdout_raw, $stdout_mode, $stderr_raw, $stderr_mode);
