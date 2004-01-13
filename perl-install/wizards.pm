51b72259f8c4b342f753c837238489295d47b91d
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-13 18:02:55 +0000

    add support for yes/no questions
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205542 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/wizards.pm
+++ perl-install/wizards.pm
@@ -152,11 +152,15 @@ sub process {
             }
         }
         my $name = ref($page->{name}) ? $page->{name}->() : $page->{name};
+        my %yesno = (yes => N("Yes"), no => N("No"));
+        @data2 = [ { val => \$yes, type => 'list', list => [ values %yesno ] } ] if $page->{type} eq "yesorno";
         my $a = $in->ask_from_({ title => $o->{name}, 
                                  messages => $name, 
                                  callbacks => { map { $_ => $page->{$_} || $default_callback{$_} } qw(focus_out complete) },
                                  if_($page->{interactive_help_id}, interactive_help_id => $page->{interactive_help_id}),
                                }, $data2);
+        # interactive->ask_yesorno does not support stepping forward or backward:
+        $a = $a eq $yesno{yes} if $page->{type} eq "yesorno";
         if ($a) {
             # step forward:
             push @steps, $next if !$page->{ignore} && $steps[-1] ne $next;
