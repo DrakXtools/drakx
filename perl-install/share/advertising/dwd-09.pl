7b084ef55fe9c8a0822e1eda90ac945b67cfcfe8
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-17 16:38:48 +0000

    Fix (r)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205626 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/dwd-09.pl
+++ perl-install/share/advertising/dwd-09.pl
@@ -9,4 +9,4 @@ $title = N("Note");
 
 @text = (N("This is the Mandrake Linux \"Download version\"."),
          '',
-		 N("The free download version does not include commercial software, and therefore may not work with certain proprietary network cards and video cards such as NVIDIA® nForce. To avoid possible compatibiity issues with these devices, we recommend the purchase of one of our retail products that includes commercial drivers and additional software.")); 
+		 N("The free download version does not include commercial software, and therefore may not work with certain proprietary network cards and video cards such as NVIDA(r) nForce. To avoid possible compatibiity issues with these devices, we recommend the purchase of one of our retail products that includes commercial drivers and additional software.")); 
