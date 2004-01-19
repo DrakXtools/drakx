a33fa44962a8b327b6f3b6e7207c0ca31d3cb3fe
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-19 14:38:34 +0000

    add forcedeth network driver (as requested on cooker)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205645 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -25,7 +25,7 @@ our %l = (
         qw(ewrk3 hamachi hp hp-plus hp100 ibmtr),
         qw(lance natsemi ne ne2k-pci ni5010 ni52 ni65 nvnet olympic pcnet32 plip rcpci),
         qw(sb1000 sis900 smc-ultra smc9194 starfire tlan tmspci tulip via-rhine),
-        qw(wd winbond-840),
+        qw(wd winbond-840 forcedeth),
 	qw(fealnx 3c990 3c990fx bcm4400),
         qw(skfp tc35815 lanstreamer farsync sdladrv orinoco_pci prism2_plx),
 
