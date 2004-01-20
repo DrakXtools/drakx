2d0ac8c19687db5fd2869608c6775f6e92639dd6
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-20 13:27:13 +0000

    simplify
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205715 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/netconnect.pm
+++ perl-install/network/netconnect.pm
@@ -592,9 +592,7 @@ notation (for example, 1.2.3.4).")),
                         $ethntf->{HWADDR} = $track_network_id or delete $ethntf->{HWADDR};
                         $in->do_pkgs->install($netcnx->{dhcp_client}) if $auto_ip;
 
-
-                        return "wireless" if is_wireless_intf($module);
-                        return "static_hostname";
+                        return is_wireless_intf($module) "wireless" : "static_hostname";
                     },
                    },
                    
