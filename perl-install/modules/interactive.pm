ff46a0590862afd50aa0bb5a5e4e9bf28c613118
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-22 17:48:28 +0000

    help perl_checker
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205840 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/modules/interactive.pm
+++ perl-install/modules/interactive.pm
@@ -35,6 +35,7 @@ sub load_category {
 	my $w;
 	my $wait_message = sub { $w = wait_load_module($in, $category, @_) };
 	@l = modules::load_category($category, $wait_message);
+	undef $w; #- help perl_checker
     }
     if (my @err = grep { $_ } map { $_->{error} } @l) {
 	my $return = $in->ask_warn('', join("\n", @err));
