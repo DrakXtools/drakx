54f1c64c9c3191b93da0abeb53ba2a1c808b4b19
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-22 22:14:09 +0000

    s/(Mandrake Linux is one) (the most widely used)/\1 of \2/
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205855 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/dwd-03.pl
+++ perl-install/share/advertising/dwd-03.pl
@@ -7,6 +7,6 @@ $y_start = 80;
 
 $title = N("What is Mandrake Linux?");
 
-@text = (N("Mandrake Linux is an Open Source distribution created with thousands of the choicest applications from the Free Software world. Mandrake Linux is one the most widely used Linux distributions worldwide!"),
+@text = (N("Mandrake Linux is an Open Source distribution created with thousands of the choicest applications from the Free Software world. Mandrake Linux is one of the most widely used Linux distributions worldwide!"),
          '',
 		 N("Mandrake Linux includes the famous graphical desktops KDE and GNOME, plus the latest versions of the most popular Open Source applications.")); 
