626f87cafec3612e431975270f2985c32e0e4b0d
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-01-25 21:56:51 +0000

    modem connection: default to pap/chap authentification method (should work for most people)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@205896 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/netconnect.pm
+++ perl-install/network/netconnect.pm
@@ -425,6 +425,7 @@ killall pppd
                         $modem ||= $netcnx->{$netcnx->{type}};
                         $modem->{device} ||= $first_modem->()->{device};
                         my %l = getVarsFromSh("$::prefix/usr/share/config/kppprc");
+                        $l{Authentication} = 4 if !exists $l{Authentication};
                         $modem->{auth} ||= { 0 => N("Script-based"), 1 => N("PAP"), 2 => N("Terminal-based"), 3 => N("CHAP"), 4 => N("PAP/CHAP") }->{$l{Authentication}};
                         $modem->{$_} ||= $l{$_} foreach qw(Gateway IPAddr SubnetMask);
                         $modem->{connection} ||= $l{Name};
