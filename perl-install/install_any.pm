1de14bf46cc60d5ddd986a9c19698a513faa2162
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-02-12 20:37:10 +0000

    add kernel-2.4* in group LSB
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@206316 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -304,6 +304,14 @@ sub setPackages {
 	pkgs::read_rpmsrate($o->{packages}, getFile("Mandrake/base/rpmsrate"));
 	($o->{compssUsers}, $o->{compssUsersSorted}) = pkgs::readCompssUsers($o->{meta_class});
 
+	#- set kernel-2.4xxx in group LSB
+	foreach (grep { $_->[1] eq '' && $_->[2] =~ /^\Q2.4/ } pkgs::packages2kernels($o->{packages})) {
+	    my $pkg = $_->[0];
+	    $pkg->rate and internal_error("package " . $pkg->name . " should not have a rate");
+	    $pkg->set_rate(5);
+	    $pkg->set_rflags('LSB');
+	}
+
 	#- preselect default_packages and compssUsersChoices.
 	setDefaultPackages($o);
 	pkgs::selectPackage($o->{packages}, pkgs::packageByName($o->{packages}, $_) || next) foreach @{$o->{default_packages}};
