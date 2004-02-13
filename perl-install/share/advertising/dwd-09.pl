716d00d3ed2ee7a865ba6ff7322812de0ec6d788
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-02-13 14:45:16 +0000

    USE ASCII OR UTF-8 ONLY; **DON'T** USE ISO-8859-1!
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@206350 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/dwd-09.pl
+++ perl-install/share/advertising/dwd-09.pl
@@ -7,6 +7,9 @@ $y_start = 10;
 $border = 10;  #- for leftish text
 
 # Use <b>text</b> for bold
+#
+# And text MUST BE in ASCII or UTF-8 *ONLY*
+# Don't use iso-8859-1 or any other encoding, it breaks everything! 
 
 # @text = ([ N("text to display"), center => 1 ],
 
@@ -14,4 +17,4 @@ $border = 10;  #- for leftish text
         ' ',
 	N("This is the Mandrake Linux <b>Download version</b>."),
 	' ',
-	N("The free download version does not include commercial software, and therefore may not work with certain modems (such as some ADSL and RTC ones) and video cards (such as ATI® and NVIDIA® ones).")); 
+	N("The free download version does not include commercial software, and therefore may not work with certain modems (such as some ADSL and RTC ones) and video cards (such as ATIÂ® and NVIDIAÂ® ones).")); 
