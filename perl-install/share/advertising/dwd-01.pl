aee16ff7e2e6c75dcda1a25f964eca7e34d5f84a
baudens <baudens@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-02-19 16:31:12 +0000

    Add missing string
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@206509 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/dwd-01.pl
+++ perl-install/share/advertising/dwd-01.pl
@@ -12,6 +12,8 @@ $border = 10;  #- for leftish text
 
 @text = ([ N("<b>Congratulations for choosing Mandrake Linux!</b>"), center => 1 ],
          ' ',
+		 N("Welcome to the Open Source world!"),
+		 ' ',
          N("Mandrake Linux is committed to the Open Source Model and fully respects the General Public License. This new release is the result of collaboration between MandrakeSoft's team of developers and the worldwide community of Mandrake Linux contributors."),
          ' ',
          N("We would like to thank everyone who participated in the development of this latest release."));
