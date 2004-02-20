1a0110d7a5b9b56fe51f029fbe17d170ae5b550c
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-02-20 14:45:46 +0000

    uim-anthy works ok in on-the-spot mode with KDE
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@206544 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/lang.pm
+++ perl-install/lang.pm
@@ -956,7 +956,7 @@ sub write {
 
 	configure_kdeglobals($locale, $confdir);
 
-	my %qt_xim = (zh => 'Over The Spot', ko => 'On The Spot', ja => 'Over The Spot');
+	my %qt_xim = (zh => 'Over The Spot', ko => 'On The Spot', ja => 'On The Spot');
 	if ($b_user_only && (my $qt_xim = $qt_xim{substr($locale->{lang}, 0, 2)})) {
 	    update_gnomekderc("$ENV{HOME}/.qt/qtrc", General => (XIMInputStyle => $qt_xim));
 	}
