90071a04d78dd05ec00b594a4431646fb7a60ec2
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-03-26 11:26:43 +0000

    misc space cleanups (thx perl_checko)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@207174 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/adsl.pm
+++ perl-install/network/adsl.pm
@@ -75,9 +75,9 @@ sub adsl_probe_info {
     }
     ($login) = map { if_(/\sname\s+([^ \n]+)/, $1) } cat_($pptp_file) if (! defined $adsl_type || $adsl_type eq 'pptp') && -r $pptp_file;
     my $passwd = passwd_by_login($login);
-    if (!$netc->{vpi} && !$netc->{vpi} &&member($adsl_modem, qw(eci speedtouch))) {
+    if (!$netc->{vpi} && !$netc->{vpi} && member($adsl_modem, qw(eci speedtouch))) {
       ($netc->{vpi}, $netc->{vci}) = 
-	(map { if_(/^.*-vpi\s+(\d+)\s+-vci\s+(\d+)/, $1, $2) } cat_("$::prefix/etc/ppp/peers/adsl")) ;
+	(map { if_(/^.*-vpi\s+(\d+)\s+-vci\s+(\d+)/, $1, $2) } cat_("$::prefix/etc/ppp/peers/adsl"));
     }
     $pppoe_conf{DNS1} ||= '';
     $pppoe_conf{DNS2} ||= '';
