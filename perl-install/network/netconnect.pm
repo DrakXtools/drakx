80759d55bc7df0302ac9875756f21b5dfa3f03cd
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-03-26 11:15:08 +0000

    warn than speedtouch only works with 2.4.x kernels for now
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@207171 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/netconnect.pm
+++ perl-install/network/netconnect.pm
@@ -650,6 +650,12 @@ killall pppd
                     name => N("ADSL configuration") . "\n\n" . N("Select the network interface to configure:"),
                     data =>  [ { label => N("Net Device"), type => "list", val => \$ntf_name, allow_empty_list => 1,
                                list => \@adsl_devices, format => sub { $eth_intf{$_[0]} || $adsl_devices{$_[0]} } } ],
+                    complete => sub {
+			if (c::kernel_version() !~ /^\Q2.4/ && $ntf_name eq 'speedtouch') {
+			    $in->ask_warn(N("Warning"), N("Sorry, we support only 2.4 and above kernels."));
+			}
+                        return 0;
+                    },
                     post => sub {
                         my %packages = (
                                         'eci'        => [ 'eciadsl', 'missing' ],
