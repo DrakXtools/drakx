fb5a3266e20ad06287f342eee4ca894bf2e95b10
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-06-23 09:03:12 +0000

    fix another typo from Titi in #9112 fix (#6802)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@207853 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/modules.pm
+++ perl-install/modules.pm
@@ -318,7 +318,7 @@ sub write_conf() {
     }
     my @l;
     push @l, 'scsi_hostadapter' if !is_empty_array_ref($conf{scsi_hostadapter}{probeall});
-    push @l, grep { detect_devices::matching_driver('^$_$') } qw(bttv cx8800 saa7134);
+    push @l, grep { detect_devices::matching_driver("^$_\$") } qw(bttv cx8800 saa7134);
     my @l_26 = @l;
     if (my ($agp) = probe_category('various/agpgart')) {
 	push @l_26, $agp->{driver};
