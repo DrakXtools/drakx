75d27a8bdbea50866c02840bedfac1b35c1deb5a
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-07-08 07:11:07 +0000

    add vincent
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208068 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- tools/cvslog2changelog.pl
+++ tools/cvslog2changelog.pl
@@ -88,5 +88,6 @@ BEGIN {
               'nplanel' => 'Nicolas Planel  <nplanel at mandrakesoft.com>',
               'rgarciasuarez' => 'Rafael Garcia-Suarez <rgarciasuarez at mandrakesoft.com>',
               'oblin' => 'Olivier Blin <oblin at mandrakesoft.com>',
+              'vguardiola' => 'Vincent Guardiola <vguardiola at mandrakesoft.com>',
 	     );
 }
