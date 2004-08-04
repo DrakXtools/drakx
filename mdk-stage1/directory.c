768d3ea090546248245a40f9c3b6c4eb4070869b
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-04 08:35:47 +0000

    prefer to use a ramdisk for disk installs
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208425 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/directory.c
+++ mdk-stage1/directory.c
@@ -143,7 +143,7 @@ enum return_type try_with_directory(char *directory, char *method_live, char *me
 		add_to_env("METHOD", method_live);
 	}
 #ifndef MANDRAKE_MOVE
-	if (IS_SPECIAL_STAGE2 || (loopdev && ramdisk_possible())) {
+	if (IS_SPECIAL_STAGE2 || ((loopdev || streq(method_live, "disk")) && ramdisk_possible())) {
 		/* RAMDISK install */
 		if (access(IMAGE_LOCATION RAMDISK_LOCATION, R_OK)) {
 			stg1_error_message("I can't find the " DISTRIB_NAME " Distribution in the specified directory. "
