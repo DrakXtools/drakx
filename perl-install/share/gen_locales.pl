2dbc3f4d1f83792258209f09d0bc4f012d334714
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-10 07:49:51 +0000

    updated use UTF-8 locales for everything
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208588 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/gen_locales.pl
+++ perl-install/share/gen_locales.pl
@@ -4,11 +4,9 @@ use MDK::Common;
 use lang;
 
 foreach (lang::list_langs()) {
-    my $c = lang::during_install__l2charset($_) or next;
-    -e "usr/share/locale/$c" or warn("not handled lang $_ (charset $c)\n"), next;
     if (my $exist = readlink "usr/share/locale/$_") {
-	$exist eq $c or die "symlink $_ already exist and is $exist instead of $c\n";
+	die "symlink $_ already exist and is $exist\n";
     } else {
-	symlink $c, "usr/share/locale/$_" or die "can't create symlink $_";
+	symlink "UTF-8", "usr/share/locale/$_" or die "can't create symlink $_";
     }
 }
--- perl-install/share/gen_locales.sh
+++ perl-install/share/gen_locales.sh
@@ -8,39 +8,9 @@ for i in LC_ADDRESS LC_COLLATE LC_CTYPE LC_IDENTIFICATION LC_MEASUREMENT LC_MONE
     install -D -m 644 /usr/share/locale/UTF-8/$i usr/share/locale/UTF-8/$i
 done
 
-# lc_ctype for non common encodings
-rm -rf .tmp2 ; mkdir .tmp2 ; cd .tmp2
-for i in ja ko ; do
-    ii=locales-`echo $i | sed 's/\(..\).*/\1/'`
-    rpm2cpio /RPMS/$ii-*.rpm | cpio -id --quiet
-    f=usr/share/locale/$i/LC_CTYPE
-    [ -e $f ] || { echo missing $f in package $ii ; exit 1 ; }
-    install -D -m 644 $f ../$f
-    (cd ../usr/share/locale/$i ; ln -s ../UTF-8/* . 2>/dev/null)
-    rm -rf *
-done
-cd .. ; rm -rf .tmp2
-
-# special case for chineese (why is it needed?)
-rm -rf .tmp2 ; mkdir .tmp2 ; cd .tmp2
-for i in zh_CN.GB2312 zh_TW.Big5 ; do
-    ii=locales-`echo $i | sed 's/\(..\).*/\1/'`
-    rpm2cpio /RPMS/$ii-*.rpm | cpio -id --quiet
-    for f in LC_ADDRESS LC_COLLATE LC_CTYPE LC_IDENTIFICATION LC_MEASUREMENT LC_MONETARY LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE LC_TIME LC_MESSAGES/SYS_LC_MESSAGES ; do
-	f=usr/share/locale/$i/$f
-	[ -e $f ] || { echo missing $f in package $ii ; exit 1 ; }
-	install -D -m 644 $f ../$f
-    done
-    rm -rf *
-done
-cd .. ; rm -rf .tmp2
-
-(cd usr/share/locale ; mv zh_CN.GB2312 GB2312 ; mv zh_TW.Big5 BIG5)
-
 perl -I../.. ../gen_locales.pl || exit 1
 
-
-for i in C en_US.UTF-8 iso8859-1 ja ko zh_CN zh_TW.big5 ; do
+for i in C en_US.UTF-8 iso8859-1 ; do
     cp -a /usr/X11R6/lib/X11/locale/$i usr/X11R6/lib/X11/locale
 done
 
