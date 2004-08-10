e7e88dea4fa9c3c11a17c0fb8d8d7c17b503b678
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-10 02:51:54 +0000

    - document "formatkey" - s/MandrakeMove/Move/
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208579 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- move/isolinux/make.pl
+++ move/isolinux/make.pl
@@ -4,7 +4,7 @@ use MDK::Common;
 
 output 'help.msg', pack("C*", 0x0E, 0x80, 0x03, 0x00),
 "
-                  0aWelcome to 09Mandrake Move0a help07
+                  0aWelcome to 09Move0a help07
 
 In most cases, the best way to get started is to simply press the 0e<Enter>07 key.
 If you experience problems, you can try to add on the command line :
@@ -13,6 +13,7 @@ If you experience problems, you can try to add on the command line :
  o  0fupdatemodules07 to use the special update floppy containing modules updates.
  o  0fpatch07 to use a patch from the floppy (file named 09patch.pl07).
  o  0fcleankey07 to remove previously saved system config files from the USB key.
+ o  0fformatkey07 to format the USB key (ie: erase all data)
  o  0fwaitkey07 to wait 15 more seconds at boot time while detecting the USB key,
     that might be useful for some keys for which detection doesn't work nicely
  o  0fvirtual_key=09/dev/hda10f,09/key07 to use file 09/key07 on device 09/dev/hda107 as a
