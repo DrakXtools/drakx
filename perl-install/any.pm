9f6ef7d95fdb86771877f372753d58ba1d5be1dc
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-10 10:04:07 +0000

    create /etc/udev/conf.d/xxx.conf
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208596 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -811,6 +811,12 @@ UNREGISTER	^$devfs_if\$	CFUNCTION GLOBAL unlink $of
 UNREGISTER	^$devfs_if\$	CFUNCTION GLOBAL unlink $if
 ") if $devfs_if ne $if && $if !~ /^hd[a-z]/ && $if !~ /^sr/ && $if !~ /^sd[a-z]/;
 
+    {
+	my $f = "$::prefix/etc/udev/conf.d/$of.conf";
+	output_p($f, "ln -sf $if /dev/$of\n");
+	chmod 0755, $f;
+    }
+
     #- when creating a symlink on the system, use devfs name if devfs is mounted
     symlinkf($devfs_if, "$::prefix/dev/$if") if $devfs_if ne $if && detect_devices::dev_is_devfs();
     symlinkf($if, "$::prefix/dev/$of");
