9f3b964d39221d844923ebd97c87f0fb3f7af8c8
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-16 11:32:18 +0000

    alsaconf is in alsa-utils package (bug 10358)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208762 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -984,7 +984,7 @@ sub summary {
 	    val => sub {},
 	    clicked => sub {
 	        if ($o->ask_yesorno('', N("Do you have an ISA sound card?"))) {
-	    	  $o->do_pkgs->install(qw(alsaconf sndconfig));
+	    	  $o->do_pkgs->install(qw(alsa-utils sndconfig));
 	    	  $o->ask_warn('', N("Run \"alsaconf\" or \"sndconfig\" after installation to configure your sound card"));
 	        } else {
 	    	  $o->ask_warn('', N("No sound card detected. Try \"harddrake\" after installation"));
