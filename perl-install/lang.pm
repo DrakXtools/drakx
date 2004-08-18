3ae906da9ae8c7cc3ff581f143c8737cde2befe6
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-18 10:21:23 +0000

    fix default IM setting when switching language
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208824 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/lang.pm
+++ perl-install/lang.pm
@@ -624,10 +624,20 @@ my %xim = (
 #-  },
 );
 
+my %default_im;
+
+sub get_default_im {
+    my ($lang) = @_;
+    $default_im{$lang}{IM};
+}
+
 sub set_default_im {
     my ($im, @langs) = @_;
     add2hash($xim{$_}, $gtkqt_im{$im}) foreach @langs;
     add2hash($xim{$_}, { XIM_PROGRAM => $im_xim_program{$im}{$_} }) foreach @langs;
+    foreach (@langs) {
+        $default_im{$_}{IM} = $im foreach $_, analyse_locale_name($_)->{main};
+    }
 }
 
 set_default_im('x-unikey',  qw(vi_VN vi_VN.TCVN vi_VN.UTF-8 vi_VN.VISCII));
