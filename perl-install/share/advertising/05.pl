36f40679bb10823307bcf142c0960d174d76bbaf
pablo <pablo@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-20 07:05:25 +0000

    standardized on using the asterisk as bullet list for all advertisings
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@208859 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/share/advertising/05.pl
+++ perl-install/share/advertising/05.pl
@@ -15,7 +15,7 @@ $border = 10;  #- for leftish text
 	 N("You are now installing <b>Mandrakelinux Download</b>. This is the free version that Mandrakesoft wants to keep <b>available to everyone</b>."),
 		 ' ',
          N("The Download version <b>cannot include</b> all the software that is not open source. Therefore, you won't find in the Download version:"),
-	 N("	- <b>Proprietary drivers</b> (such as drivers for NVIDIA®, ATI™, etc.)."),
-	 N("	- <b>Proprietary software</b> (such as Acrobat® Reader®, RealPlayer®, Flash™, etc.)."),
+	 N("	* <b>Proprietary drivers</b> (such as drivers for NVIDIA®, ATI™, etc.)."),
+	 N("	* <b>Proprietary software</b> (such as Acrobat® Reader®, RealPlayer®, Flash™, etc.)."),
 	 ' ',
 	 N("You won't have access to the <b>services included</b> in the other Mandrakesoft products either."));
