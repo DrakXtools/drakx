66de47256925f991b147a3b37c96110de4db57c5
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-09-03 05:06:52 +0000

    (get_interface_type) enhance detection for adsl devices
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209144 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/tools.pm
+++ perl-install/network/tools.pm
@@ -297,7 +297,8 @@ sub get_interface_type {
     my ($interface) = @_;
     $interface->{DEVICE} =~ /^(eth|ath|wlan)/ && "ethernet" ||
     $interface->{DEVICE} =~ /^ippp/ && "isdn" ||
-    $interface->{DEVICE} =~ /^ppp/ && (member($interface->{TYPE}, "xDSL", "ADSL") ? "adsl" : "modem") ||
+    member($interface->{TYPE}, "xDSL", "ADSL") && "adsl" ||
+    $interface->{DEVICE} =~ /^ppp/ && "modem" ||
     "unknown";
 }
 
