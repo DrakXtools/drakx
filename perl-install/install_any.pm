a95510b4214d493b3f73e47e3fff24c8eda11788
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-09-09 09:54:51 +0000

    BIGMEM now means >4GB
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209350 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_any.pm
+++ perl-install/install_any.pm
@@ -663,7 +663,7 @@ sub setDefaultPackages {
     $o->{rpmsrate_flags_chosen}{USB} = 1 if $o->{modules_conf}->get_probeall("usb-interface");
     $o->{rpmsrate_flags_chosen}{PCMCIA} = 1 if detect_devices::hasPCMCIA();
     $o->{rpmsrate_flags_chosen}{HIGH_SECURITY} = 1 if $o->{security} > 3;
-    $o->{rpmsrate_flags_chosen}{BIGMEM} = 1 if !$::oem && availableRamMB() > 800 && arch() !~ /ia64|x86_64/;
+    $o->{rpmsrate_flags_chosen}{BIGMEM} = 1 if c::dmiDetectMemory() > 4 * 1024;
     $o->{rpmsrate_flags_chosen}{SMP} = 1 if detect_devices::hasSMP();
     $o->{rpmsrate_flags_chosen}{CDCOM} = 1 if any { $_->{descr} =~ /commercial/i } values %{$o->{packages}{mediums}};
     $o->{rpmsrate_flags_chosen}{TV} = 1 if detect_devices::getTVcards();
