a114d3ca0c08c4fa3ae3d0ea60c2761bec6444d1
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-09-09 02:53:05 +0000

    don't check_prog when the prog is a complex command (containing a pipe)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209307 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/run_program.pm
+++ perl-install/run_program.pm
@@ -54,7 +54,7 @@ sub raw {
     $root ? ($root .= '/') : ($root = '');
     if (!$root && !$::isStandalone) {
 	require install_any;
-	install_any::check_prog($real_name);
+	install_any::check_prog($real_name) if $real_name !~ /\|/;
     }
     
     $ENV{HOME} || $::isInstall or $ENV{HOME} = '/root';
