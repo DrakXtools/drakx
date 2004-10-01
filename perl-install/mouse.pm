3bcbe2865f21de067f2ca553b24e969f267af808
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-10-01 07:03:06 +0000

    (detect) fix synaptics auto-detection
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209729 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/mouse.pm
+++ perl-install/mouse.pm
@@ -294,7 +294,7 @@ sub detect {
         if (my $mouse_nb = grep { /^H: Handlers=mouse/ } @input_devices) {
             my $univ_mouse = fullname2mouse('Universal|Any PS/2 & USB mice', wacom => \@wacom);
             if (any { m!^N: Name="(?:SynPS/2 Synaptics TouchPad|AlpsPS/2 ALPS TouchPad)"$! } @input_devices) {
-                $synaptics_mouse = fullname2mouse('PS/2|Synaptics Touchpad');
+                $synaptics_mouse = fullname2mouse('Universal|Synaptics Touchpad');
                 $::isInstall and $synaptics_mouse->{alternate_install} = $univ_mouse; #- don't try to use synpatics at beginning of install
                 $mouse_nb < 2 and return $synaptics_mouse;
                 $univ_mouse->{auxmouse} = $synaptics_mouse;
