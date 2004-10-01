5513e770b4798a426e88f395a11a252e32d6b037
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-10-01 01:59:12 +0000

    fix logging rpmsrate_flags_chosen
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209718 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/pkgs.pm
+++ perl-install/pkgs.pm
@@ -683,7 +683,8 @@ sub setSelectedFromCompssList {
 	    last;
 	}
     }
-    log::l("setSelectedFromCompssList: reached size ", formatXiB($nb), ", up to indice $min_level (less than ", formatXiB($max_size), ") for flags ", sort(map_each { if_($::b, $::a) } %$rpmsrate_flags_chosen));
+    my @flags = map_each { if_($::b, $::a) } %$rpmsrate_flags_chosen;
+    log::l("setSelectedFromCompssList: reached size ", formatXiB($nb), ", up to indice $min_level (less than ", formatXiB($max_size), ") for flags ", join(' ', sort @flags));
     log::l("setSelectedFromCompssList: ", join(" ", sort map { $_->name } grep { $_->flag_selected } @{$packages->{depslist}}));
     $min_level;
 }
