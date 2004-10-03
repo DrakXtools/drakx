3d1f6fb170e84f536f1e5cb38576fc7c26a81518
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-10-03 07:52:04 +0000

    (configureNetwork) load only ethernet modules before easy_dhcp (this will load firewire modules after other ethernet modules, so firewire interfaces won't always take the name eth0)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@209736 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_interactive.pm
+++ perl-install/install_steps_interactive.pm
@@ -789,7 +789,8 @@ sub updateModulesFromFloppy {
 sub configureNetwork {
     my ($o) = @_;
     require network::network;
-    modules::load_category($o->{modules_conf}, 'network/*');
+    require network::ethernet;
+    modules::load_category($o->{modules_conf}, network::ethernet::get_eth_categories());
     network::network::easy_dhcp($o->{modules_conf}, $o->{netc}, $o->{intf}) and $o->{netcnx}{type} = 'lan';
     $o->SUPER::configureNetwork;
 }
