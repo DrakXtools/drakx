1632b8ffdd9ebd78a8dec2d304b28eaf7289f2d5
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-11-16 11:23:42 +0000

    add ahci and sata_uli SATA drivers (from latest libata as in 2.8.10-rc1-mm5)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@210095 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -80,7 +80,7 @@ our %l = (
         qw(qla1280 qla2x00 qlogicfas qlogicfc),
         qw(seagate wd7000 sim710 sym53c416 t128 tmscsim u14-34f ultrastor),
         qw(eata eata_pio eata_dma mptscsih nsp32),
-        qw(sata_nv ata_piix sata_promise sata_sil sata_sis sata_svw sata_sx4 sata_via sata_vsc sx8),
+        qw(ahci ata_piix sata_nv sata_promise sata_sil sata_sis sata_svw sata_sx4 sata_uli sata_via sata_vsc sx8),
       ),
       qw(aic7xxx aic7xxx_old aic79xx pci2000 qlogicisp sym53c8xx lpfcdd), # ncr53c8xx
     ],
