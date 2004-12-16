441c9ab132cda4b0150a0ee02668566dd9806de9
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-12-16 14:53:16 +0000

    help testing
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@210349 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install2.pm
+++ perl-install/install2.pm
@@ -433,7 +433,7 @@ sub main {
     log::openLog(($::testing || $o->{localInstall}) && 'debug.log');
     log::l("second stage install running (", any::drakx_version(), ")");
 
-    output('/proc/sys/kernel/modprobe', ''); #- disable kmod, otherwise we get a different behaviour in kernel vs kernel-BOOT
+    output('/proc/sys/kernel/modprobe', '') if !$::testing; #- disable kmod, otherwise we get a different behaviour in kernel vs kernel-BOOT
     eval { fs::mount('none', '/sys', 'sysfs', 1) };
 
     if ($::move) {
