90fdfd889ae098510ec69ce6444743f3d1793fc9
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-03-21 16:21:26 +0000

    add a new SATA driver: ata_adma
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@211553 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -97,7 +97,7 @@ our %l = (
     ],
     sata => [
       # note that ata_piix manage RAID devices on ICH6R
-      qw(ahci ata_piix sata_nv sata_promise sata_qstor sata_sil sata_sis sata_svw sata_sx4 sata_uli sata_via sata_vsc sx8),
+      qw(ahci ata_adma ata_piix sata_nv sata_promise sata_qstor sata_sil sata_sis sata_svw sata_sx4 sata_uli sata_via sata_vsc sx8),
     ],
     hardware_raid => [
       if_(arch() =~ /^sparc/, qw(pluto)),
