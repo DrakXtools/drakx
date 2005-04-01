3d66aef25f24a31b0ae15e2eeee4e2877b90f2a9
rgarciasuarez <rgarciasuarez@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-04-01 16:45:11 +0000

    When presenting the list of media to deselect, group by CDs even when using the mini ISO
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@211779 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install_steps_gtk.pm
+++ perl-install/install_steps_gtk.pm
@@ -643,6 +643,9 @@ sub deselectFoundMedia {
     my $totalsize = 0;
     foreach (@$hdlists) {
 	(my $cd) = $_->[3] =~ /\b(?:CD|DVD) ?(\d+)\b/i;
+	if (!$cd) { #- test for mini-ISO
+	    $cd = 1 if $_->[3] =~ /\bmini.?cd\b/i;
+	}
 	if (!$cd || !@{$cdlist{$cd} || []}) {
 	    push @hdlist2, $_;
 	    $corresp[$i] = [ $i ];
