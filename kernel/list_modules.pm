669b419056f0070723568db7c23ab3c52f0a94de
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-04-01 16:54:36 +0000

    add comments
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@211781 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -76,7 +76,9 @@ our %l = (
   ################################################################################
   disk => 
   {
+    # ide drivers compiled as modules:
     ide => [ qw(aec62xx cs5520 cs5530 delkin_cb ide-generic rz1000 sc1200 slc90e66 triflex trm290) ],
+    # ide drivers compiled in core kernel:
     all_ide => [
         qw(ali14xx amd74xx dtc2278 ht6560b qd65xx umc8672 aec62xx alim15x3 atiixp cmd64x cs5520 cs5530 cy82c693),
         qw(hpt34x hpt366 ns87415 opti621 pdc202xx_new pdc202xx_old piix rz1000 sc1200 serverworks),
