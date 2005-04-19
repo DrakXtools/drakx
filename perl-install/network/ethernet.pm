fb96829402315ddc857fd70f36771362ba875cae
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-04-19 16:19:06 +0000

    do not write zeroed MAC addresses in iftab, it confuses ifrename
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@212020 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/ethernet.pm
+++ perl-install/network/ethernet.pm
@@ -132,6 +132,8 @@ sub get_eth_card_mac_address {
 sub update_iftab() {
     foreach my $intf (detect_devices::getNet()) {
         my ($link_type, $mac_address) = get_eth_card_mac_address($intf) or next;
+        #- do not write zeroed MAC addresses in iftab, it confuses ifrename
+        $mac_address = /^[0:]+$/ and next;
         my $descriptor = ${{ ether => 'mac', ieee1394 => 'mac_ieee1394' }}{$link_type} or next;
         substInFile {
             s/^$intf\s+.*\n//;
