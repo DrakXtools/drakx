677f5ba2d453d297cf9d3e2e3353c37082c0c806
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-06-09 13:03:01 +0000

    bump copyright + s/Mandrakesoft/Mandriva/
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@212695 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- globetrotter/move.pm
+++ globetrotter/move.pm
@@ -1,6 +1,6 @@
 package move; # $Id$
 
-#- Copyright (c) 2004 Mandrakesoft
+#- Copyright (c) 2004-2005 Mandriva
 #-
 #- This program is free software; you can redistribute it and/or modify
 #- it under the terms of the GNU General Public License as published by
