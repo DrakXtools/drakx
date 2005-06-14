710ad44053cd12cc049e0f1877ac9a4b5c9da9d7
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-06-14 05:54:20 +0000

    move ext2 resizing code here so that it's easier to use
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@212719 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl-install/diskdrake/resize_ext2.pm
@@ -0,0 +1,42 @@
+package diskdrake::resize_ext2; # $Id$
+
+use diagnostics;
+use strict;
+
+use run_program;
+use common;
+
+
+sub new {
+    my ($type, $_device, $dev) = @_;
+
+    my $o = bless { dev => $dev }, $type;
+
+    my $r = run_program::get_stdout('dumpe2fs', $dev);
+    $o->{block_count} = $r =~ /^Block count:\s*(\d+)/m && $1;
+    $o->{free_block} = $r =~ /^Free blocks:\s*(\d+)/m && $1;
+    $o->{block_size} = $r =~ /^Block size:\s*(\d+)/m && $1;
+    log::l("dumpe2fs $dev gives: Block_count=$o->{block_count}, Free_blocks=$o->{free_block}, Block_size=$o->{block_size}");
+
+    $o->{block_size} && $o;
+}
+
+sub check_prog {
+    my ($in) = @_;
+    $::isInstall || $in->do_pkgs->ensure_binary_is_installed('ntfsprogs', 'ntfsresize');
+}
+
+sub min_size {
+    my ($o) = @_;
+    ($o->{block_count} - $o->{free_block}) * ($o->{block_size} / 512);
+}
+
+sub resize {
+    my ($o, $size) = @_;
+
+    my $s = int($size / ($o->{block_size} / 512));
+    log::l("resize2fs $o->{dev} to size $s in block of $o->{block_size} bytes");
+    run_program::run("resize2fs", "-pf", $o->{dev}, $s);
+}
+
+1;
