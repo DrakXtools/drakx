214ccab654bac3aabb3d5a33ff05ec78656164ef
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-01 04:54:49 +0000

    a PV has no "Options" (bugzilla #16168)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@212899 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/diskdrake/interactive.pm
+++ perl-install/diskdrake/interactive.pm
@@ -445,7 +445,7 @@ sub part_possible_actions {
     my %actions = my @l = (
         N_("Mount point")      => '$part->{real_mntpoint} || (!isBusy && !isSwap && !isNonMountable)',
         N_("Type")             => '!isBusy && $::expert && (!readonly || $part->{pt_type} == 0x83)',
-        N_("Options")          => '$::expert',
+        N_("Options")          => '!isNonMountable && $::expert',
         N_("Resize")	       => '!isBusy && !readonly && !isSpecial || isLVM($hd) && LVM_resizable',
         N_("Format")           => '!isBusy && !readonly && ($::expert || $::isStandalone)',
         N_("Mount")            => '!isBusy && (hasMntpoint || isSwap) && maybeFormatted && ($::expert || $::isStandalone)',
