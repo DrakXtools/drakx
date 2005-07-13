401623a62c050355eab3fee11bda23cd180888f3
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-13 03:51:36 +0000

    initial import
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@213045 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ perl-install/network/monitor.pm
@@ -0,0 +1,27 @@
+package network::monitor;
+
+use dbus_object;
+
+our @ISA = qw(dbus_object);
+
+sub new {
+    my ($type, $bus) = @_;
+    dbus_object::new($type,
+		       $bus,
+		       "com.mandriva.monitoring",
+		       "/com/mandriva/monitoring/network",
+		       "com.mandriva.monitoring.network");
+}
+
+sub list_wireless {
+    my ($o) = @_;
+    my $networks;
+    eval { $networks = $o->call_method('ListWireless') };
+    my %networks;
+    while ($networks =~ /^((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\t(\d+)\t(\d+)\t(.*?)\t(.*)$/mg) {
+        $networks{$1} = { frequency => $2, signal_level => $3, flags => $4, ssid => $5 };
+    }
+    \%networks;
+}
+
+1;
