9d53389b4039d5509b9fca7d4252324206af3138
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-18 06:06:17 +0000

    get_eth_card_mac_address: handle firewire mac_addresses again
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@213082 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/ethernet.pm
+++ perl-install/network/ethernet.pm
@@ -107,7 +107,8 @@ sub get_eth_cards_names {
 #- returns (link_type, mac_address)
 sub get_eth_card_mac_address {
     my ($intf) = @_;
-    `LC_ALL= LANG= $::prefix/sbin/ip -o link show $intf 2>/dev/null` =~ m|.*link/(\S+)\s((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\s|;
+    #- don't look for 6 bytes addresses only because of firewire
+    `LC_ALL= LANG= $::prefix/sbin/ip -o link show $intf 2>/dev/null` =~ m|.*link/(\S+)\s((?:[0-9a-f]{2}:?)+)\s|;
 }
 
 #- write interfaces MAC address in iftab
