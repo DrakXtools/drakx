0dc54f3cf405b2cf67efae36b0ba81519f588500
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-20 06:27:13 +0000

    get network ids and current network
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@213123 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/monitor.pm
+++ perl-install/network/monitor.pm
@@ -1,5 +1,6 @@
 package network::monitor;
 
+use common;
 use dbus_object;
 
 our @ISA = qw(dbus_object);
@@ -16,10 +17,17 @@ sub new {
 sub list_wireless {
     my ($o) = @_;
     my $networks;
-    eval { $networks = $o->call_method('ListWireless') };
+    eval { $networks = $o->call_method('ScanResults') };
     my %networks;
     while ($networks =~ /^((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\t(\d+)\t(\d+)\t(.*?)\t(.*)$/mg) {
         $networks{$1} = { frequency => $2, signal_level => $3, flags => $4, ssid => $5 };
+    }
+    eval { $networks = $o->call_method('ListNetworks') };
+    while ($networks =~ /^(\d+)\t(.*?)\t(.*?)\t(.*)$/mg) {
+        if (my $net = $networks{$3} || find { $_->{ssid} eq $2 } values(%networks)) {
+            $net->{id} = $1;
+            $net->{current} = to_bool($4 eq '[CURRENT]');
+        }
     }
     \%networks;
 }
