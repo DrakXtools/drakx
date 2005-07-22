f3f9c19832f1edb9dda13cc3b78796d531942445
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-07-22 03:09:50 +0000

    add some comments, use meaningfull variable names
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@213142 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/network/monitor.pm
+++ perl-install/network/monitor.pm
@@ -16,14 +16,17 @@ sub new {
 
 sub list_wireless {
     my ($o) = @_;
-    my $networks;
-    eval { $networks = $o->call_method('ScanResults') };
+    my $results;
+    eval { $results = $o->call_method('ScanResults') };
     my %networks;
-    while ($networks =~ /^((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\t(\d+)\t(\d+)\t(.*?)\t(.*)$/mg) {
+    #- bssid / frequency / signal level / flags / ssid
+    while ($results =~ /^((?:[0-9a-f]{2}:){5}[0-9a-f]{2})\t(\d+)\t(\d+)\t(.*?)\t(.*)$/mg) {
         $networks{$1} = { frequency => $2, signal_level => $3, flags => $4, ssid => $5 };
     }
-    eval { $networks = $o->call_method('ListNetworks') };
-    while ($networks =~ /^(\d+)\t(.*?)\t(.*?)\t(.*)$/mg) {
+    my $list;
+    eval { $list = $o->call_method('ListNetworks') };
+    #- network id / ssid / bssid / flags
+    while ($list =~ /^(\d+)\t(.*?)\t(.*?)\t(.*)$/mg) {
         if (my $net = $networks{$3} || find { $_->{ssid} eq $2 } values(%networks)) {
             $net->{id} = $1;
             $net->{ssid} ||= $2;
