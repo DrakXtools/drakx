20d0a16984a14b1573ee9fad2ffe7b3559eafd8d
prigaux <prigaux@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-08-09 07:18:33 +0000

    we can't differentiate all the scim+xxx IMs, so we ensure we prefer "scim+(default)"
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@213373 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/lang.pm
+++ perl-install/lang.pm
@@ -969,7 +969,7 @@ sub read {
     my $locale = system_locales_to_ourlocale($h{LC_MESSAGES} || 'en_US', $h{LC_MONETARY} || 'en_US');
     
     if (find { $h{$_} } @IM_i18n_fields) {
-	my @l = keys %IM_config;
+	my @l = grep { !/^scim/ || /\(default\)/ } keys %IM_config;
 	foreach my $field ('XMODIFIERS', 'XIM_PROGRAM') {
 	    $h{$field} or next;
 	    my @m = grep { $h{$field} eq $IM_config{$_}{$field} } @l or last;
