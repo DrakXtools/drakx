eef6a1fbac5e2e6c9ccbd2e9cf9430102ad737c8
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-09-16 19:27:26 +0000

    (complete_usb_storage_info) using a local value is just saner
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@214234 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/detect_devices.pm
+++ perl-install/detect_devices.pm
@@ -141,11 +141,10 @@ sub complete_usb_storage_info {
 
     foreach my $usb (usb_probe()) {
 	if (my $e = find { !$_->{found} && $_->{usb_vendor} == $usb->{vendor} && $_->{usb_id} == $usb->{id} } @usb) {
-		$e->{found} = 1;
+         local $e->{found} = 1;
 	    $e->{"usb_$_"} = $usb->{$_} foreach keys %$usb;
 	}
     }
-    delete $_->{found} foreach @usb;
 }
 
 sub get_devfs_devices {
