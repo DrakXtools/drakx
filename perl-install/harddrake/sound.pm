7977d6f60e697ca53ec0c339a52fb7af4b9a265f
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2005-11-28 18:27:53 +0000

    (do_switch) make package installation actually work
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@214650 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/sound.pm
+++ perl-install/harddrake/sound.pm
@@ -200,7 +200,8 @@ sub do_switch {
     $modules_conf->set_sound_slot("sound-slot-$index", $new_driver);
     $modules_conf->write;
     if ($new_driver =~ /^snd-/) {   # new driver is an alsa one
-        $in->do_pkgs->ensure_binary_is_installed(qw(alsactl alsa-utils aoss));
+        $in->do_pkgs->ensure_binary_is_installed(@$_) foreach ([ qw(alsa-utils alsactl) ],
+                                                               [ qw(aoss aoss) ]);
         rooted("service alsa start") if $::isStandalone && !$blacklisted;
         rooted("/sbin/chkconfig --add alsa")  if $::isStandalone;
         load($modules_conf, $new_driver) if $::isStandalone;   # service alsa is buggy
