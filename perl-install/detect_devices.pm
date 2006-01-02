d07a51d2911a8077e601207cd6c8bbf75395f35d
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2006-01-02 12:28:06 +0000

    better not dying (#20340) it looks like there do be some devices out for which we failed to get proper data
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@214825 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/detect_devices.pm
+++ perl-install/detect_devices.pm
@@ -292,7 +292,7 @@ sub getSCSI_26() {
 	# handle both old and new kernels:
 	my $node =  -e "$dir/block" ? "$dir/block" : top(glob_("$dir/block*"));
 	my ($device) = readlink($node) =~ m!/block/(.*)!;
-	die "cannot get info for device ($host, $channel, $id, $lun)" if !$device;
+	warn "cannot get info for device ($host, $channel, $id, $lun)" if !$device;
 
 	my $media_type = ${{ st => 'tape', sr => 'cdrom', sd => 'hd' }}{substr($device, 0, 2)};
 	# Old hp scanners report themselves as "Processor"s
