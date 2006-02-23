3f6bee210b2d79744df569c4afd86d519020c571
oblin <oblin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2006-02-23 14:27:15 +0000

    remove untested patch
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@215050 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -722,19 +722,11 @@ sub acceptLicense {
     my ($o) = @_;
     require install_messages;
 
-    $o->{release_notes} = join("\n\n", map {
-        if ($::isInstall) {
-            my $f = install_any::getFile($_);
-            $f && cat__($f);
-        } else {
-            my $file = $_;
-            my $d = find { -e "$_/$file" } glob_("/usr/share/doc/*-release-*");
-            print cat_("$d/$file");
-            $d && cat_("$d/$file");
-        }
-    } 'release-notes.txt', 'release-notes.' . arch() . '.txt');
+    $o->{release_notes} = join("\n\n", map { 
+	my $f = install_any::getFile($_);
+	$f && cat__($f);
+    } 'release-notes.txt', 'release-notes.' . arch() . '.txt') if $::isInstall;
 
-    print $o->{release_notes};
     return if $o->{useless_thing_accepted};
 
     my $r = $::testing ? 'Accept' : 'Refuse';
@@ -744,7 +736,7 @@ sub acceptLicense {
 		     cancel => N("Quit"),
 		     messages => formatAlaTeX(install_messages::main_license() . "\n\n\n" . install_messages::warning_about_patents()),
 		     interactive_help_id => 'acceptLicense',
-		     more_buttons => [ [ N("Release Notes"), sub { $o->ask_warn(N("Release Notes"), $o->{release_notes}) }, 1 ] ],
+		     if_(!$::globetrotter, more_buttons => [ [ N("Release Notes"), sub { $o->ask_warn(N("Release Notes"), $o->{release_notes}) }, 1 ] ]),
 		     callbacks => { ok_disabled => sub { $r eq 'Refuse' } },
 		   },
 		   [ { list => [ N_("Accept"), N_("Refuse") ], val => \$r, type => 'list', format => sub { translate($_[0]) } } ])
