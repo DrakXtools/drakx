5ae8382cb72601d931d651bdf15b18e34a8ffb50
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-07-16 21:30:48 +0000

    load ide-disk module when loading disk/ide category
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@224879 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/modules.pm
+++ perl-install/modules.pm
@@ -261,6 +261,7 @@ sub when_load_category {
 
     if ($category =~ m,disk/ide,) {
 	$conf->add_probeall('ide-controller', $name);
+	eval { load('ide-disk') };
     } elsif ($category =~ m,disk/(scsi|hardware_raid|sata|usb|firewire),) {
 	$conf->add_probeall('scsi_hostadapter', $name);
 	eval { load('sd_mod') };
