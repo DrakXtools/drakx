5d10460cb4ae3cbc429d8cc44b0cf76ddcccbb2f
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-07-17 13:46:22 +0000

    iniline init-libc-headers.h in init.c
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@224892 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/init.c
+++ mdk-stage1/init.c
@@ -20,7 +20,30 @@
  */
 
 #ifndef INIT_HEADERS
-#include "init-libc-headers.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mount.h>
+#include <linux/un.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <linux/unistd.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
+
+#ifndef SOCK_STREAM
+#define SOCK_STREAM 1
+#endif
+
+static inline _syscall3(int, syslog, int, type, char *, bufp, int, len);
+static inline _syscall3(int, reboot, int, magic, int, magic2, int, flag);
 #else
 #include INIT_HEADERS
 #endif
