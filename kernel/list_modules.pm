8277294b0157031d6b78bf7ada639dd4eb8857e7
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-07-20 15:21:27 +0000

    add unionfs module (for rescue images)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@224933 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -167,7 +167,7 @@ our %l = (
       if_(arch() =~ /^ppc/, qw(hfs)),
       qw(reiserfs jfs xfs),
     ],
-    various => [ qw(smbfs romfs ext3 ufs ntfs) ],
+    various => [ qw(smbfs romfs ext3 ufs ntfs unionfs) ],
 
   },
 
