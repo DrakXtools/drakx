3a1f08160f99551132a649269cce51cc6c3d2f10
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-08-06 09:10:03 +0000

    (bug_handler) drakbug: do not report "wizcancel" exceptions (#32308)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@225416 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/standalone.pm
+++ perl-install/standalone.pm
@@ -194,8 +194,8 @@ sub bug_handler {
     # we want the full backtrace:
     $error .= common::backtrace() if $error;
     my $progname = $0;
-    # do not loop if drakbug crashes:
-    if ($progname =~ /drakbug/) {
+    # do not loop if drakbug crashes and do not complain about wizcancel:
+    if ($progname =~ /drakbug/ || $error =~ /wizcancel/) {
     	warn $error;
     	exit(1);
     }
