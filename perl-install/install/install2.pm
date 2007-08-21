edc692654acb7432487f0e1d7dc15e10e325e6b5
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-08-21 14:43:42 +0000

    kill old usb-storage code, mkinitrd now loads it when necessary
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@226197 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install/install2.pm
+++ perl-install/install/install2.pm
@@ -184,16 +184,6 @@ sub formatPartitions {
 
     any::rotate_logs($::prefix);
 
-    if (any { $_->{usb_media_type} && any { $_->{mntpoint} } partition_table::get_normal_parts($_) } @{$o->{all_hds}{hds}}) {
-	log::l("we use a usb-storage based drive, so keep it as a normal scsi_hostadapter");
-    } else {
-	log::l("we do not need usb-storage for booting system, rely on hotplug");
-	#- when usb-storage is in scsi_hostadapter, 
-	#- hotplug + scsimon do not load sd_mod/sr_mod when needed
-	#- (eg: when plugging a usb key)
-	$o->{modules_conf}->remove_probeall('scsi_hostadapter', 'usb-storage');
-    }
-
     require raid;
     raid::write_conf($o->{all_hds}{raids});
 
