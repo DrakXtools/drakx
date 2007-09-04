654e9a216d25b70a4a7f2efb092d38cf00405e84
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-09-04 16:54:48 +0000

    (getSCSI) enumerate generic SCSI devices again
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@226985 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/detect_devices.pm
+++ perl-install/detect_devices.pm
@@ -199,8 +199,8 @@ sub getSCSI() {
 	my $dir = "$dev_dir/$_";
 
 	# handle both old and new kernels:
-	my $node =  find { -e $_ } "$dir/block", top(glob_("$dir/block*")), "$dir/tape";
-	my ($device) = readlink($node) =~ m!/(?:scsi_tape|block)/(.*)!;
+	my $node =  find { -e $_ } "$dir/block", top(glob_("$dir/block*")), "$dir/tape", top(glob_("$dir/scsi_generic*"));
+	my ($device) = readlink($node) =~ m!/(?:scsi_(?:generic|tape)|block)/(.*)!;
 	warn("cannot get info for device ($_)"), next if !$device;
 
 	my $usb_dir = readlink("$node/device") =~ m!/usb! && "$node/device/../../../..";
