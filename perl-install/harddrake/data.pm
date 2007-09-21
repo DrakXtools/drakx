90e836b70c49d0da327fe5d0c492377911e7259b
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-09-21 11:16:06 +0000

    harddrake: do not auto-configure CD-Rom drives in fstab, it fordbids umounting with hal (install does not configure them in fstab either)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@228859 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/data.pm
+++ perl-install/harddrake/data.pm
@@ -35,7 +35,7 @@ sub f {
 
 # FIXME: add translated items
 
-sub is_removable { $_[0] =~ /FLOPPY|ZIP|DVDROM|CDROM|BURNER/ }
+sub is_removable { $_[0] =~ /FLOPPY|ZIP/ }
 
 sub set_removable_configurator {
     my ($class, $device) = @_;
@@ -98,7 +98,7 @@ our @tree =
       icon => "cd.png",
       configurator => "",
       detector => sub { f(grep { !(detect_devices::isBurner($_) || detect_devices::isDvdDrive($_)) } &detect_devices::cdroms) },
-      checked_on_boot => 1,
+      checked_on_boot => 0,
       automatic => 1,
      },
 
@@ -108,7 +108,7 @@ our @tree =
       icon => "cd.png",
       configurator => "",
       detector => sub { f(detect_devices::burners()) },
-      checked_on_boot => 1,
+      checked_on_boot => 0,
       automatic => 1,
      },
 
@@ -118,7 +118,7 @@ our @tree =
       icon => "cd.png",
       configurator => "",
       detector => sub { f(grep { ! detect_devices::isBurner($_) } detect_devices::dvdroms()) },
-      checked_on_boot => 1,
+      checked_on_boot => 0,
       automatic => 1,
      },
 
