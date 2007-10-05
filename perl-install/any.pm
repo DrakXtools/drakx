6d852bc5930a5146964a527075139cb572b8f640
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-10-05 19:35:50 +0000

    (display_release_notes) fix release notes window size in !install mode
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@230913 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -871,7 +871,7 @@ sub display_release_notes {
                 ),
        );
     # make parent visible still visible:
-    local ($::real_windowwidth, $::real_windowheight) = ($::real_windowwidth - 50, $::real_windowheight - 50);
+    local ($::real_windowwidth, $::real_windowheight) = ($::real_windowwidth - 50, $::real_windowheight - 50) if $::isInstall;
     mygtk2::set_main_window_size($w->{rwindow});
     $w->{real_window}->grab_focus;
     $w->{real_window}->show_all;
