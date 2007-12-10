118fceef20e5436ebdb0dee59e6e4f630ee8f483
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-12-10 18:33:46 +0000

    handle new wireless drivers: libertas_cs (PCMCIA)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@232420 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -50,7 +50,7 @@ our %l = (
     ],
     pcmcia => [ 
       qw(3c574_cs 3c589_cs axnet_cs fmvj18x_cs),
-      qw(ibmtr_cs nmclan_cs pcnet_cs smc91c92_cs),
+      qw(ibmtr_cs libertas_cs nmclan_cs pcnet_cs smc91c92_cs),
       qw(xirc2ps_cs xircom_cb xircom_tulip_cb),
     ],
    #- generic NIC detection for USB seems broken (class, subclass, 
