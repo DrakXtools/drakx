3864d859f033ac0245bb2cfd329556253711f711
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-12-21 10:32:03 +0000

    - handle atl2 ethernet driver
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@232657 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- kernel/list_modules.pm
+++ kernel/list_modules.pm
@@ -19,7 +19,7 @@ our %l = (
       if_(arch() =~ /^sparc/, qw(sunbmac sunhme sunqe)),
       if_(arch() !~ /alpha|sparc/,
         qw(3c501 3c503 3c505 3c507 3c509 3c515 3c990 3c990fx),
-        qw(82596 ac3200 acenic aironet4500_card amd8111e at1700 atp),
+        qw(82596 ac3200 acenic aironet4500_card amd8111e at1700 atl2 atp),
         qw(bcm4400 cassini cs89x0 de600 de620),
         qw(depca dmfe e2100 eepro eexpress eth16i),
         qw(ewrk3 hp hp-plus hp100),
