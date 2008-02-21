f8da38be345444bc5f56152a253b5ae88629f7eb
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-02-21 14:19:47 +0000

    (to_utf8) export it (for rpmdrake)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@237150 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/common.pm
+++ perl-install/common.pm
@@ -10,7 +10,7 @@ use run_program;
 
 use Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw($SECTORSIZE N P N_ check_for_xserver files_exist formatTime MB formatXiB get_parent_uid makedev mandrake_release mandrake_release_info removeXiBSuffix require_root_capability setVirtual set_alternative set_l10n_sort set_permissions translate unmakedev);
+our @EXPORT = qw($SECTORSIZE N P N_ check_for_xserver files_exist formatTime MB formatXiB get_parent_uid makedev mandrake_release mandrake_release_info removeXiBSuffix require_root_capability setVirtual set_alternative set_l10n_sort set_permissions to_utf8  translate unmakedev);
 
 # perl_checker: RE-EXPORT-ALL
 push @EXPORT, @MDK::Common::EXPORT;
