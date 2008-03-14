c57cf28ff90d7214bdd4a7e2e57be99c8bdf2eca
pixel <pixel@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-03-14 10:25:13 +0000

    add $line_nb in message
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@239365 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/pkgs.pm
+++ perl-install/pkgs.pm
@@ -67,10 +67,10 @@ sub read_rpmsrate_raw {
 		    } elsif (@diff1 == 1 && @diff2 == 1) {
 			@new_flags = (@common, join('||', $diff1[0], $diff2[0]));
 		    } else {
-			log::l("can not handle complicate flags for packages appearing twice ($name)");
+			log::l("$line_nb: can not handle complicate flags for packages appearing twice ($name)");
 			$fatal_error++;
 		    }
-		    log::l("package $name appearing twice with different rates ($rate != " . $rates{$name} . ")") if $rate != $rates{$name};
+		    log::l("$line_nb: package $name appearing twice with different rates ($rate != " . $rates{$name} . ")") if $rate != $rates{$name};
 		}
 		$rates{$name} = $rate;
 		$flags{$name} = \@new_flags;
