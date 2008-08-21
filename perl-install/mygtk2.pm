d94f1270e81edce753a52618bb43b25ae0913cb5
pixel <pixel@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-08-21 17:00:11 +0000

    remove 2 hacks: - it seems the ->show is unneeded - and then the special case for 200x200 window is no more needed
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244314 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/mygtk2.pm
+++ perl-install/mygtk2.pm
@@ -1033,13 +1033,10 @@ sub _create_Window {
 	    return if $w_size[2] < 5;
 	    return if $w_size[2] == $wi && $w_size[3] == $he; #BUG
 	    (undef, undef, $wi, $he) = @w_size;
-	    return if $wi <= 200 && $he <= 200;
 
             $w->move(max(0, $::rootwidth - ($::o->{windowwidth} + $wi) / 2), 
 		     max(0, ($::o->{windowheight} - $he) / 2));
 	});
-	#- without this, the focus is broken during install, though this is not needed during X test, why??
-	$w->show;
     }
 
     $w;
