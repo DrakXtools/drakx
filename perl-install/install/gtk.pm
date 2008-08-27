DrakXtools/drakx

commit 0e6e7d3201ddb229319d5c9ffc0346d463d1b3ae

(create_steps_window) first steps toward new installer style
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@243712 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/gtk.pm
@@ -122,7 +122,7 @@ sub create_steps_window {
     };
 
     my @l = (
-        create_logo(),
+        gtknew('HBox', height => 145),
         $category->(N("Installation"))
     );
     foreach (grep { !eval $o->{steps}{$_}{hidden} } @{$o->{orderedSteps}}) {
@@ -138,13 +138,35 @@ sub create_steps_window {
         ]);
     }
 
-    my $offset = 20;
-    $o->{steps_window} =
-      gtknew('Window', width => ($::stepswidth - $offset), widget_name => 'Steps', title => 'Steps',
-	     position => [ lang::text_direction_rtl() ? $::rootwidth - $::stepswidth : $offset, 0 ],
-	     child => gtknew('VBox', spacing => 6, children_tight => \@l));
+    my $offset = 10;
+    $o->{steps_widget} =
+      gtknew('Fixed', widget_name => 'Steps', pixbuf_file => 'left-background',
+             has_window => 1, x => 0, y => 0, height => 500, width => $::stepswidth, # -1
+	     child => gtknew('VBox', spacing => 6, width => ($::stepswidth - $offset), children_tight => \@l));
+    $o->{steps_widget}->put(
+        # FIXME: not RTL compliant (lang::text_direction_rtl() ? ...)
+        gtknew('VBox', height => $::rootheight,
+               children => [
+                   0, gtknew('Image', file => 'left-top-corner'),
+                   (map { (1, gtknew('Image', file => 'left-border')) } 1 .. $::rootheight),
+                   0, gtknew('Image', file => 'left-bottom-corner'),
+               ]),
+        $::stepswidth - 21, 0);
 
-    $o->{steps_window}->show;
+    $o->{steps_widget}->show;
+
+    $root_window->add(
+        $o->{steps_window} = 
+          gtknew('HBox',
+                 children =>
+                   [
+                       0, $o->{steps_widget},
+                       1, gtknew('Label', width => -1, height => -1),
+                   ],
+             )
+    );
+        
+    $root_window->show_all
 }
 
 sub update_steps_position {

commit 2b331e553eec0df94f3e9e545cfd909b239a4e2f

(create_steps_window) better layout in 640x480 (#43231)
("regression" introduced in r243712 on 2008-07-30:
"(create_steps_window) first steps toward new installer style")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244419 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/gtk.pm
@@ -109,8 +109,6 @@ my %steps;
 sub create_steps_window {
     my ($o) = @_;
 
-    return if $::stepswidth == 0;
-
     $o->{steps_window} and $o->{steps_window}->destroy;
 
     $steps{$_} ||= gtknew('Pixbuf', file => "steps_$_") foreach qw(on off done);
@@ -177,6 +175,7 @@ sub create_steps_window {
           gtknew('HBox',
                  children =>
                    [
+                       if_($::stepswidth != 0, 0, $o->{steps_widget}),
                        0, $o->{steps_widget},
                        1, gtknew('Label', width => -1, height => -1),
                    ],