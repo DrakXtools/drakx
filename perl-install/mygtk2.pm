c850c67a1b473811a4d6f9944ccb115d37472ffe
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-08-27 17:47:00 +0000

    (get_label_width) introduce it in order to workaround infamous 6 years old gnome bug #101968
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244428 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/mygtk2.pm
+++ perl-install/mygtk2.pm
@@ -1198,6 +1198,10 @@ sub _allow_scroll_TextView_to_bottom {
     };
 }
 
+sub get_label_width() {
+    first(mygtk2::get_main_window_size()) - 50;
+}
+
 sub set_main_window_size {
     my ($window) = @_;
     my ($width, $height) = $::real_windowwidth ? ($::real_windowwidth, $::real_windowheight) : $::isWizard ? (540, 360) : (600, 400);
