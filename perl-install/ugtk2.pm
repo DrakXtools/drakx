be374f6de117f034534971d9f0da5d01988aab33
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-09-02 13:17:16 +0000

    (gtkradio,create_boxradio) move label sizing one level upper (needed for next commit)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244628 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/ugtk2.pm
+++ perl-install/ugtk2.pm
@@ -148,7 +148,6 @@ sub gtkradio {
     my $radio;
     map {
         my $w = gtkset_active($radio = Gtk2::RadioButton->new_with_label($radio ? $radio->get_group : undef, $_), $_ eq $def);
-        $w->child->set_size_request(mygtk2::get_label_width(), -1);
         $w->get_child->set_line_wrap(1);
         $w;
       } @_;
