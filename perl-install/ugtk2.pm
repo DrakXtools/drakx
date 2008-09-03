cf0886f0f7eb58094ff7e7f931f93e9c3a671072
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-09-03 15:44:46 +0000

    (ask_browse_tree_info) explain
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@244701 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/ugtk2.pm
+++ perl-install/ugtk2.pm
@@ -935,6 +935,7 @@ sub ask_browse_tree_info {
 		   ]));
 
     gtkpack__($box2, gtknew(($::isInstall ? 'Install_Button' : 'Button'), text => N("Help"), clicked => sub {
+                            # will be a little small in standalone mode:
                             if (defined $::o) {
                                 $::o->ask_warn(N("Help"), $common->{interactive_help}->());
                             } else {
