fbb8ad819bb45f413af6da04f9ed5bf76de1ef89
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-09-11 16:45:52 +0000

    (selectKeyboard) render title in blue in advanced dialog
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@245095 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install/steps_interactive.pm
+++ perl-install/install/steps_interactive.pm
@@ -96,13 +96,13 @@ sub selectKeyboard {
 	$o->ask_from_(
 		      { title => N("Keyboard"), 
 			interactive_help_id => 'selectKeyboard',
-			advanced_messages => N("Here is the full list of available keyboards"),
 			advanced_label => N("More"),
 		      },
 		      [
                           { label => N("Please choose your keyboard layout."), title => 1 },
                           if_(@best, { val => \$KEYBOARD, type => 'list', format => $format, sort => 1,
 				     list => [ @best ], changed => sub { $other = 0 } }),
+                          { label => N("Here is the full list of available keyboards"), title => 1, advanced => @best },
 			{ val => \$ext_keyboard, type => 'list', format => $format, changed => sub { $other = 1 },
 			  list => [ difference2([ keyboard::KEYBOARDs() ], \@best) ], advanced => @best > 1 }
 		      ]);
