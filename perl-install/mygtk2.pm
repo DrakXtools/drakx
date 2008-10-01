1e6c0ede60ddba57051d7f91cf0d11b43582f268
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-10-01 10:44:41 +0000

    (_gtk__Image_using_pixbuf,_gtk__MDV_Notebook) adapt to latest commit
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@247232 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/mygtk2.pm
+++ perl-install/mygtk2.pm
@@ -332,6 +332,7 @@ sub _gtk__Image {
             my $pixbuf = _pixbuf_render_alpha(gtknew('Pixbuf', file => $file, %{$w->{options}}), 255);
             my ($width, $height) = ($pixbuf->get_width, $pixbuf->get_height);
             $w->set_size_request($width, $height);
+            $w->{pixbuf} = $pixbuf;
             $w->signal_connect(expose_event => sub {
                                    if (!$w->{x}) {
                                        my $alloc = $w->allocation;
@@ -679,13 +680,13 @@ sub _gtk__MDV_Notebook {
         my $filler = gtknew('Image', file => 'left-background-filler.png');
         my $filler_height = $filler->get_pixbuf->get_height;
         my $left_background = gtknew('Image_using_pixbuf', file => 'left-background.png');
-        my $lf_height = $left_background->get_pixbuf->get_height;
+        my $lf_height = $left_background->{pixbuf}->get_height;
         my @right_background = $::isInstall ? 
           gtknew('Image', file => "right-white-background_left_part$suffix", flip => $is_flip_needed)
             : map {
                 gtknew('Image', file => "right-white-background_left_part-$_", flip => $is_flip_needed)
             } 1, 2, 2, 3;
-        my $width1 = $left_background->get_pixbuf->get_width;
+        my $width1 = $left_background->{pixbuf}->get_width;
         my $total_width = $width1 + $right_background[0]->get_pixbuf->get_width;
         my $arrow_x = text_direction_rtl() ? $offset/2 -4 : $width1 - $offset -3;
         $w = gtknew('HBox', spacing => 0, children => [
