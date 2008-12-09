78f89f926cd10d2721cff2a7c74de12495344ba3
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2008-12-09 19:04:20 +0000

    no need to probe input devices thrice (they are already listed in @devices through detect_devices::probe_all() -> getInputDevices_and_usb() -> getInputDevices())
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@250345 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/data.pm
+++ perl-install/harddrake/data.pm
@@ -476,9 +476,10 @@ our @tree =
       icon => "hw-keyboard.png",
       configurator => "$sbindir/keyboarddrake",
       detector => sub {
-          f(grep { $_->{description} =~ /Keyboard/i || $_->{media_type} =~ /Subclass\|Keyboard/i } @devices),
-            # USB devices are filtered out since we already catch them through probeall():
-          f(grep { $_->{bus} ne 'usb' && $_->{driver} eq 'kbd' && $_->{description} !~ /PC Speaker/ } detect_devices::getInputDevices());
+          f(grep { $_->{description} =~ /Keyboard/i || $_->{media_type} =~ /Subclass\|Keyboard/i ||
+                     # USB devices are filtered out since we already catch them through probeall():
+                     $_->{bus} ne 'usb' && $_->{driver} eq 'kbd' && $_->{description} !~ /PC Speaker/
+                 } @devices),
       },
       checked_on_boot => 0,
      },
@@ -498,9 +499,10 @@ our @tree =
       icon => "hw_mouse.png",
       configurator => "$sbindir/mousedrake",
       detector => sub {
-          f(grep { $_->{driver} =~ /^Mouse:|^Tablet:/ || $_->{media_type} =~ /class\|Mouse/ } @devices),
-            # USB devices are filtered out since we already catch them through probeall():
-            f(grep { $_->{bus} ne 'usb' && $_->{Handlers}{mouse} } detect_devices::getInputDevices());
+          f(grep { $_->{driver} =~ /^Mouse:|^Tablet:/ || $_->{media_type} =~ /class\|Mouse/ ||
+                     # USB devices are filtered out since we already catch them through probeall():
+                     $_->{bus} ne 'usb' && $_->{Handlers}{mouse}
+                 } @devices),
       },
       checked_on_boot => 1,
       automatic => 1,
