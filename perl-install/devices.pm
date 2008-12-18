DrakXtools/drakx

commit 440989583a9f28d840b27132f00a68066005378c

(entry) use sysfs in order to discover major/minor for SCSI like devices since they're dynamic with kernel-2.6.28+
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@250534 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/devices.pm
@@ -77,10 +77,9 @@ sub entry {
     if (/^0x([\da-f]{3,4})$/i) {
 	$type = c::S_IFBLK();
 	($major, $minor) = unmakedev(hex $1);
-    } elsif (/^sd(.)(\d{0,2})/) {
+    } elsif (/^(sd.)(\d{0,2})/) {
+        ($major, $minor) = split(':', chomp_(cat_("/sys/block/$1/$1$2/dev")));
 	$type = c::S_IFBLK();
-	$major = 8;
-	$minor = 16 * (ord($1) - ord('a')) + ($2 || 0);
     } elsif (/^hd(.)(\d{0,2})/) {
 	$type = c::S_IFBLK();
 	($major, $minor) =

commit 5144e82af9ccfde0487729d29669e692de9fbd1a

(entry) fix retrieving major/minor for whole discs (regression introduced in previous commit (r250534))
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@250536 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/devices.pm
@@ -78,7 +78,8 @@ sub entry {
 	$type = c::S_IFBLK();
 	($major, $minor) = unmakedev(hex $1);
     } elsif (/^(sd.)(\d{0,2})/) {
-        ($major, $minor) = split(':', chomp_(cat_("/sys/block/$1/$1$2/dev")));
+        my $path = $2 ? "/sys/block/$1/$1$2/dev" : "/sys/block/$1/dev";
+        ($major, $minor) = split(':', chomp_(cat_($path)));
 	$type = c::S_IFBLK();
     } elsif (/^hd(.)(\d{0,2})/) {
 	$type = c::S_IFBLK();