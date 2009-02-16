f38225f22f9bd5cc51259a8cbece78ddc88f77c1
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-02-16 10:21:53 +0000

    revert latest commit (useless since do_pkgs already do it)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@252892 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/harddrake/sound.pm
+++ perl-install/harddrake/sound.pm
@@ -347,7 +347,7 @@ sub switch {
             set_5_1_in_pulseaudio($is_5_1_in_pulseaudio_enabled);
             set_user_switching($is_user_switching);
             if ($is_pulseaudio_routing_enabled) {
-                $in->do_pkgs->ensure_is_installed('alsa-plugins-pulseaudio', "$::prefix/usr/"
+                $in->do_pkgs->ensure_is_installed('alsa-plugins-pulseaudio', '/usr/'
                                                     . (arch() =~ /x86_64/ ? 'lib64' : 'lib')
                                                     .'/alsa-lib/libasound_module_pcm_pulse.so');
             }
