214423371da78ca8007ae492ffb9a4f5ef03e466
pterjan <pterjan@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-03-08 18:14:15 +0000

    load appropriate modules before trying to mount ext4/reiser4
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@253748 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/mount.c
+++ mdk-stage1/mount.c
@@ -206,11 +206,17 @@ int my_mount(char *dev, char *location, char *fs, int force_rw)
 	if (!strcmp(fs, "reiserfs"))
 		my_insmod("reiserfs", ANY_DRIVER_TYPE, NULL, 1);
 
+	if (!strcmp(fs, "reiser4"))
+		my_insmod("reiser4", ANY_DRIVER_TYPE, NULL, 1);
+
 	if (!strcmp(fs, "jfs"))
 		my_insmod("jfs", ANY_DRIVER_TYPE, NULL, 1);
 
 	if (!strcmp(fs, "xfs"))
 		my_insmod("xfs", ANY_DRIVER_TYPE, NULL, 1);
+
+	if (!strcmp(fs, "ext4"))
+		my_insmod("ext4", ANY_DRIVER_TYPE, NULL, 1);
 
 #endif
 	if (!strcmp(fs, "iso9660"))
