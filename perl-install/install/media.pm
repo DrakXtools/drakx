DrakXtools/drakx

commit 6a26769f5d8bf06e599bbcf726829cd78a8d25ec

(get_media,_adjust_paths_in_urpmi_cfg) only adjust newly added medium regarding current physical medium
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@254610 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/media.pm
@@ -496,6 +496,7 @@ sub get_media {
     use Data::Dumper;
 
     my ($suppl_CDs, $copy_rpms_on_disk, $phys_m);
+    my @names = map { $_->{name} } @{$packages->{media}};
     foreach (@$media) {
 	if ($_->{type} eq 'media_cfg') {
 	    $phys_m = url2mounted_phys_medium($o, $_->{url}, 'media_info');
@@ -536,11 +537,12 @@ sub get_media {
 	    log::l("unknown media type $_->{type}, skipping");
 	}
     }
+    my @new_media = difference2([ map { $_->{name} } @{$packages->{media}} ], \@names);
 
     urpm::media::update_media($packages, distrib => 1, callback => \&urpm::download::sync_logger) or
         log::l('updating media failed');
 
-    _adjust_paths_in_urpmi_cfg($o, $phys_m);
+    _adjust_paths_in_urpmi_cfg($o, $phys_m, @new_media);
 
     urpm::media::configure($packages);
     log::l('urpmi completely set up');
@@ -550,13 +552,14 @@ sub get_media {
 }
 
 sub _adjust_paths_in_urpmi_cfg {
-    my ($o, $phys_m) = @_;
-    if ($o->{stage2_phys_medium}{method} eq 'cdrom') {
+    my ($o, $phys_m, @new_media) = @_;
+    if ($o->{stage2_phys_medium}{method} ne 'cdrom') {
         my $urpm = install::pkgs::empty_packages();
         # force rereading media:
         undef $urpm->{media};
         urpm::media::read_config($urpm);
         foreach my $medium (@{$urpm->{media}}) {
+            next if !member($medium->{name}, @new_media);
             $medium->{url} =~ s!^$phys_m->{real_mntpoint}/!cdrom://!;
         }
         urpm::media::write_config($urpm);

commit dba1dc115b164e2f01851b87bb92161e99bca322

(_adjust_paths_in_urpmi_cfg) fix writing urpmi.cfg as cdrom:// for NFS (#49316) (regression introduced in r254610 on 2009-03-23: "(get_media,_adjust_paths_in_urpmi_cfg) only adjust newly added medium regarding current physical medium"
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255159 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/media.pm
@@ -543,7 +543,7 @@ sub get_media {
 
 sub _adjust_paths_in_urpmi_cfg {
     my ($o, $phys_m, @new_media) = @_;
-    if ($o->{stage2_phys_medium}{method} ne 'cdrom') {
+    if ($o->{stage2_phys_medium}{method} eq 'cdrom') {
         my $urpm = install::pkgs::empty_packages();
         # force rereading media:
         undef $urpm->{media};