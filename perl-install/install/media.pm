DrakXtools/drakx

commit b5175665379e1f717039b51a91a418b1a48916ed

(get_media) always trust stage1's /tmp/image (thus fixing ISO on NFS installation as well as NFS installation from cooker/ instead of eg cooker/i586 (#48874)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255093 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/media.pm
@@ -502,10 +502,8 @@ sub get_media {
 	    $phys_m = url2mounted_phys_medium($o, $_->{url}, 'media_info');
             log::l(Data::Dumper->Dump([ $phys_m ], [ 'phys_m' ]));
             log::l(Data::Dumper->Dump([ $o->{stage2_phys_medium} ], [ 'stage2_phys_medium' ]));
-            my $uri = $o->{stage2_phys_medium}{method} eq 'disk' ? '/tmp/image' :
-              $o->{stage2_phys_medium}{url} =~ m!^(http|ftp)://! && $o->{stage2_phys_medium}{url} ||
-              $phys_m->{method} =~ m!^(ftp|http)://! && $phys_m->{method}
-              || $phys_m->{real_mntpoint} || $phys_m->{url};
+            my $uri = $o->{stage2_phys_medium}{url} =~ m!^(http|ftp)://! && $o->{stage2_phys_medium}{url} ||
+              $phys_m->{method} =~ m!^(ftp|http)://! && $phys_m->{method} || '/tmp/image';
 
             # adjust URI for cdroms if needed:
             if (member($o->{stage2_phys_medium}{method}, qw(iso cdrom))) {

commit a17cb163a97c77fff9b6cd4ac95a492fadb3c914

(adjust_paths_in_urpmi_cfg) fix mount point for CD-ROM media (#49613) (prefix can be either /tmp/image for main media or real mount point)
(regression introduced in r255093 on 2009-03-30: "(get_media) always trust
stage1's /tmp/image (thus fixing ISO on NFS installation as well as NFS
installation from cooker/ instead of eg cooker/i586 (#48874)")


git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@255665 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/install/media.pm
@@ -530,7 +530,8 @@ sub adjust_paths_in_urpmi_cfg {
         my $dir = _get_medium_dir($phys_m);
         my $old_url = $medium->{url};
         if ($phys_m->{method} eq 'cdrom') {
-            $medium->{url} =~ s!^$phys_m->{real_mntpoint}/!cdrom://!;
+            $medium->{url} =~ s!^/tmp/image!cdrom://!; # main media
+            $medium->{url} =~ s!^$phys_m->{real_mntpoint}/!cdrom://!; # supplementary medium
         } elsif ($phys_m->{method} eq 'nfs') {
             $medium->{url} =~ s!^$::prefix!!;
         }