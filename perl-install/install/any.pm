0daff10fab0f9409f14515e0e0ed416408cc3216
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-04-21 12:47:04 +0000

    (upgrade_kde3_to_kde4) do not offer to stop the install when kde3 is installed since kde3 to kde4 upgrade is now done through kde4 packages obsoleting kde3 ones
    just silently select task-kde4
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@256911 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -403,15 +403,6 @@ sub upgrade_kde3_to_kde4 {
     if (-e "$::prefix/usr/bin/kicker"
           && !install::pkgs::packageByName($o->{packages}, 'task-kde3')) {
         log::l("kde3 installed, but task-kde3 not available so can't upgrade correctly");
-
-        my $choice;
-        $o->ask_from_({ messages => formatAlaTeX(N("You have decided to upgrade your system to %s. KDE 3.5 has been detected
-on your system. This installer cannot preserve KDE 3.5 in an upgrade. If you choose to proceed, 
-KDE 4 will replace KDE 3, and you will lose your personal KDE configuration settings. 
-To upgrade with KDE 3.5 and your personal settings preserved, 
-please reboot your system and upgrade using the Mandriva update applet.", 'Mandriva Linux 2009')) },
-                      [ { val => \$choice, type => 'list', list => [ N_("Reboot"), N_("Proceed") ], format => \&translate } ]);
-        $choice eq 'Reboot' and install::steps::rebootNeeded($o);	
         log::l("ok, continuing anyway, but forcing install of task-kde4");
         install::pkgs::select_by_package_names($o->{packages}, ['task-kde4']);
     }
