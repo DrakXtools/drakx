9eb202129fbf27175d081db6adbeec7682626fc7
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-11-25 13:15:01 +0000

    (pci_probe) wrap for readability
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@263685 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/c/stuff.xs.pl
+++ perl-install/c/stuff.xs.pl
@@ -264,7 +264,8 @@ pci_probe()
     for (i = 0; i < entries.nb; i++) {
       struct pciusb_entry *e = &entries.entries[i];
       snprintf(buf, sizeof(buf), "%04x\t%04x\t%04x\t%04x\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s", 
-               e->vendor, e->device, e->subvendor, e->subdevice, e->pci_domain, e->pci_bus, e->pci_device, e->pci_function, e->pci_revision, e->is_pciexpress,
+               e->vendor, e->device, e->subvendor, e->subdevice, e->pci_domain, e->pci_bus,
+               e->pci_device, e->pci_function, e->pci_revision, e->is_pciexpress,
                pci_class2text(e->class_id), e->class, e->module ? e->module : "unknown", e->text);
       PUSHs(sv_2mortal(newSVpv(buf, 0)));
     }
