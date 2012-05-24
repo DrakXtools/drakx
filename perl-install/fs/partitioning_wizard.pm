DrakXtools/drakx

commit 839c461503792b25a3b082202d86320b2e66e89e

partitioning wizard: offer to install on dmraid and existing lvm
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@265764 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/fs/partitioning_wizard.pm
@@ -502,7 +502,9 @@ sub main {
 
         my $mainbox = Gtk2::VBox->new;
 
-        my @kinds = map { diskdrake::hd_gtk::hd2kind($_) } sort { $a->{is_removable} <=> $b->{is_removable} } @{$all_hds->{hds}};
+        my @kinds = map { diskdrake::hd_gtk::hd2kind($_) } sort { $a->{is_removable} <=> $b->{is_removable} } @{$all_hds->{hds} };
+        push @kinds, map { diskdrake::hd_gtk::raid2kind($_) } @{$all_hds->{raids}};
+        push @kinds, map { diskdrake::hd_gtk::lvm2kind($_) } @{$all_hds->{lvms}};
 
         my $hdchoice = Gtk2::HBox->new;
 

commit 9c4f5f1287cdd64cc43a49d84e41199c7d7a5f55

Author: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date:   Fri Apr 20 18:22:13 2012 +0000
    (fs::partitioning_wizard::main) use newly introduced raid2real_kind() in
    order to get data about RAID devices, thus fixing:
    - crashing on selecting a RAID device (calling first_usable_sector on
      undefined value in partition_table::get_normal_parts_and_holes())
    - listing RAID devices
    
    (bug introduced in commit r265764 pterjan by on Feb 4 2010:
    "partitioning wizard: offer to install on dmraid and existing lvm")
    
    sadly we trade "calling method on undefined value" for  "calling method
    on unblessed reference" (see next commit)



git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274147 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/fs/partitioning_wizard.pm
@@ -504,7 +504,7 @@ sub main {
         my $mainbox = Gtk2::VBox->new;
 
         my @kinds = map { diskdrake::hd_gtk::hd2kind($_) } sort { $a->{is_removable} <=> $b->{is_removable} } @{ $all_hds->{hds} };
-        push @kinds, diskdrake::hd_gtk::raid2kind() if @{$all_hds->{raids}};
+        push @kinds, diskdrake::hd_gtk::raid2real_kind($_) foreach @{$all_hds->{raids}};
         push @kinds, map { diskdrake::hd_gtk::lvm2kind($_) } @{$all_hds->{lvms}};
 
         my $hdchoice = Gtk2::HBox->new;