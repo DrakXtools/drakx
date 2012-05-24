a15f08a250b09ef78cfbc5d80d0c28c65ae414ff
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-05-24 20:40:58 +0000

    Author: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date:   Sat Mar 10 18:07:54 2012 +0000
        (default_packages) explain why acpi, acpid & mageia-gfxboot-theme packages are selected
    
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274052 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -559,7 +559,7 @@ sub default_packages {
     my @ltmp = map { $_->{BOOTPROTO} eq 'dhcp' ? $_->{DHCP_CLIENT} || 'dhcp-client' : () } values %{$o->{net}{ifcfg}};
     add_n_log("needed by networking", @ltmp) if @ltmp;
     # will get auto selected at summary stage for bootloader:
-    push @l, qw(acpi acpid mageia-gfxboot-theme);
+    add_n_log("needed later at summary stage", qw(acpi acpid mageia-gfxboot-theme));
     # only needed for CDs/DVDs installations:
     add_n_log("method='cdrom'", 'perl-Hal-Cdroms') if $o->{method} eq 'cdrom';
 
