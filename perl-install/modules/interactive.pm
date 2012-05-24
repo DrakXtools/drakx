c620f9b0e9f9cb52743b61e381418b418430ff9e
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-05-24 22:19:14 +0000

    Author: blino <blino@5b7c3c08-40e5-403b-9995-ace06908e4af> Date:   Thu May 10 22:46:24 2012 +0000
        harddrake2: allow writing 0 as module option, and forbid options with spaces (from Colin)
    
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274191 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/modules/interactive.pm
+++ perl-install/modules/interactive.pm
@@ -20,7 +20,7 @@ sub config_window {
         return;
     }
     if ($in->ask_from(N("Module configuration"), N("You can configure each parameter of the module here."), \@l)) {
-	   my $options = join(' ', map { if_($conf{$_}, "$_=$conf{$_}") } keys %conf);
+	   my $options = join(' ', map { if_($conf{$_} =~ /^\S+$/, "$_=$conf{$_}") } keys %conf);
 	   my $old_options = $modules_conf->get_options($data->{driver});
 	   if ($options ne $old_options) {
 	       $modules_conf->set_options($data->{driver}, $options);
