5f17f7e18e062f65ca0fe715209c32d920b60110
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-05-24 20:52:52 +0000

    Author: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date:   Wed Mar 14 07:56:14 2012 +0000
        (AUTOLOAD) show which function has issues (mga#4919)
    
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274061 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/c.pm
+++ perl-install/c.pm
@@ -7,11 +7,12 @@ use MDK::Common;
 
 sub AUTOLOAD() {
     $AUTOLOAD =~ /::(.*)/ or return;
-    my @l = eval { &{$c::stuff::{$1}} };
+    my $fct = $1;
+    my @l = eval { &{$c::stuff::{$fct}} };
     if (my $err = $@) {
 	$err =~ /Undefined subroutine &main::/ ?
 	  die("cannot find function $AUTOLOAD\n" . backtrace()) :
-	  die($err);	
+	  die("$fct: " . $err);	
     }
     wantarray() ? @l : $l[0];
 }
