9b7413e3694d6ccef3c8261b835b279d8545e6eb
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-08-10 15:35:23 +0000

    oops, forgot to add this one earlier..
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274681 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ mdk-stage1/init.h
@@ -0,0 +1,23 @@
+/*
+ * Original author:
+ * Guillaume Cottenceau <gc@mandriva.com>
+ *
+ * Current maintainer:
+ * Per Øyvind Karlsen <peroyvind@mandriva.org>
+ * Copyright 2000-2012 Mandriva
+ *
+ * This software may be freely redistributed under the terms of the GNU
+ * public license.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef _INIT_H_
+#define _INIT_H_
+
+int init_main(int argc, char **argv);
+
+#endif
