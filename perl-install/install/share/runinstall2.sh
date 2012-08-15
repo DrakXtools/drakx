27931095007304a02b713d84be0c24823cb11568
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-08-15 15:36:47 +0000

    as we no longer ship gdb-inst, don't refer to it anymore either
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274764 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- perl-install/install/share/runinstall2.sh
+++ perl-install/install/share/runinstall2.sh
@@ -3,6 +3,5 @@
 echo "Starting Udev\n"
 perl -I/usr/lib/libDrakX -Minstall::install2 -e "install::install2::start_udev()"
 echo "You can start the installer by running install2"
-echo "You can run it in GDB by running gdb-inst"
 /usr/bin/busybox sh
 exec install2
