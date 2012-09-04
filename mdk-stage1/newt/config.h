a8c8172b6148df2088b59cb004d0515edce3ba94
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-09-04 20:55:06 +0000

    just trim down config a bit..
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@274961 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/newt/config.h
+++ mdk-stage1/newt/config.h
@@ -1,6 +1,3 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
 /* Define to 1 if NLS support is enabled */
 /* #undef ENABLE_NLS */
 
@@ -45,27 +42,6 @@
 
 /* Default NEWT_COLORS_FILE */
 /* #undef NEWT_COLORS_FILE */
-
-/* Package Name */
-#define PACKAGE "newt"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME ""
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION ""
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
