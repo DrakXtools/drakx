f9bdbb40c5edbf5418c0ebd5664879edcf9203eb
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-16 14:53:58 +0000

    add configs
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@275296 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

---
+++ images/python/dvd-x86_64.py
@@ -0,0 +1,36 @@
+from drakx.distribution import Distribution
+from drakx.isoimage import IsoImage
+from drakx.media import Media
+import os
+
+media = []
+
+for m in "main", "contrib", "non-free":
+    media.append(Media(m))
+
+srcdir = "./"
+includelist = []
+for l in ["basesystem_mini", "input_cat", "theme-free", "kernel64", "languages", "firmware_nonfree", "input_contrib", "input_nonfree"]:
+    includelist.append(srcdir + "lists/" + l)
+excludelist = []
+for e in ["exclude", "exclude_free", "exclude_ancient", "exclude_tofix", "exclude_nonfree", "exclude_contrib64"]:
+    excludelist.append(srcdir + "lists/" + e)
+
+rpmsrate = srcdir + "rpmsrate"
+compssusers = srcdir + "compssUsers.pl"
+filedeps = srcdir + "file-deps"
+
+version = "2012"
+branch = "devel"
+
+arch = "x86_64"
+
+outdir="ut/%s" % arch
+os.system("rm -rf "+outdir)
+
+x86_64 = Distribution(version, branch, arch, media, includelist, excludelist, rpmsrate, compssusers, filedeps, outdir=outdir)
+name="moondrake"
+distrib=[x86_64]
+distribution="mandriva-linux"
+
+image = IsoImage(name, version, arch, x86_64, distribution, outdir)
---
+++ images/python/mini64.py
@@ -0,0 +1,25 @@
+from drakx.distribution import Distribution
+from drakx.media import Media
+
+media = []
+
+for m in "main", "contrib", "non-free":
+    media.append(Media(m))
+
+srcdir = "./"
+includelist = []
+for l in ["basesystem_mini", "kernel64_mini", "languages", "firmware_nonfree"]:
+    includelist.append(srcdir + "lists/" + l)
+excludelist = []
+for e in ["exclude", "exclude_mini", "exclude_ancient", "exclude_tofix", "exclude_nonfree"]:
+    excludelist.append(srcdir + "lists/" + e)
+
+rpmsrate = srcdir + "rpmsrate-mini"
+compssusers = srcdir + "compssUsers-mini.pl"
+filedeps = srcdir + "file-deps"
+
+version = "2012"
+branch = "devel"
+
+arch = "x86_64"
+distrib = Distribution(version, branch, arch, media, includelist, excludelist, rpmsrate, compssusers, filedeps, "ut/"+ arch)
