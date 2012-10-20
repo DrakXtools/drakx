5e44a85ec0a91d7e8e9656c828ccabb179d0799e
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-20 14:08:27 +0000

    merge all dual arch configuration into one file
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@275325 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- images/python/mini.py
+++ images/python/mini.py
@@ -1,16 +1,36 @@
 from drakx.isoimage import IsoImage
+from drakx.releaseconfig import ReleaseConfig
+from drakx.media import Media
+from drakx.distribution import Distribution
 import os
 
-outdir="ut"
-os.system("rm -rf "+outdir)
-from mini32 import distrib as i586
-from mini64 import distrib as x86_64
+config = ReleaseConfig("2012", "OurDiva", "Non-Free", subversion="Alpha 2", medium="CD")
+os.system("rm -rf "+config.outdir)
 
-name="moondrake"
-version="2012"
-arch="dual"
+srcdir = "./"
+rpmsrate = srcdir + "rpmsrate-mini"
+compssusers = srcdir + "compssUsers-mini.pl"
+filedeps = srcdir + "file-deps"
+
+
+media = []
+for m in "main", "contrib", "non-free":
+    media.append(Media(m))
+
+includelist = []
+for l in ["basesystem_mini", "kernel32", "languages", "firmware_nonfree"]:
+    includelist.append(srcdir + "lists/" + l)
+
+includelist32 = includelist + [srcdir + "lists/" + "kernel32"]
+includelist64 = includelist + [srcdir + "lists/" + "kernel64_mini"]
+
+excludelist = []
+for e in ["exclude", "exclude_mini", "exclude_ancient", "exclude_tofix", "exclude_nonfree"]:
+    excludelist.append(srcdir + "lists/" + e)
+
+i586 = Distribution(config, "i586", media, includelist32, excludelist, rpmsrate, compssusers, filedeps)
+x86_64 = Distribution(config, "x86_64", media, includelist64, excludelist, rpmsrate, compssusers, filedeps)
+
 distrib=[i586,x86_64]
-branch="devel"
-distribution="moondrake-linux"
 
-image = IsoImage(name, version, arch, distrib, branch, distribution, outdir, maxsize=800)
+image = IsoImage(config, distrib, maxsize=800)
