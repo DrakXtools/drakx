d2137d767fa6ec86c8adbbae4c4d8a1bb1a80c1c
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-24 01:04:55 +0000

    set product
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@275366 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- images/python/drakx/releaseconfig.py
+++ images/python/drakx/releaseconfig.py
@@ -1,14 +1,15 @@
 class ReleaseConfig(object):
-    def __init__(self, version, codename, flavour, subversion = None, outdir="out", branch = "devel", repopath = None, medium = "DVD", vendor = "Moondrake", distribution = "Moondrake GNU/Linux"):
+    def __init__(self, version, codename, product, subversion = None, outdir="out", branch = "devel", repopath = None, medium = "DVD", vendor = "Moondrake", distribution = "Moondrake GNU/Linux"):
         self.version = version
         self.codename = codename
         self.subversion = subversion
-        self.flavour = flavour
+        self.product = product
         self.medium = medium
         self.vendor = vendor
         self.distribution = distribution
         self.outdir = outdir
         self.branch = branch
+        self.product = product
         if (not repopath):
             self.repopath += "%s/%s" % (branch, version)
         else:
