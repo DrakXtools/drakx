d109b3ab88730859dbe1758fc81a9e20a68690e6
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-24 01:06:22 +0000

    don't set product several times..
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@275367 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- images/python/drakx/releaseconfig.py
+++ images/python/drakx/releaseconfig.py
@@ -2,14 +2,13 @@ class ReleaseConfig(object):
     def __init__(self, version, codename, product, subversion = None, outdir="out", branch = "devel", repopath = None, medium = "DVD", vendor = "Moondrake", distribution = "Moondrake GNU/Linux"):
         self.version = version
         self.codename = codename
-        self.subversion = subversion
         self.product = product
+        self.subversion = subversion
         self.medium = medium
         self.vendor = vendor
         self.distribution = distribution
         self.outdir = outdir
         self.branch = branch
-        self.product = product
         if (not repopath):
             self.repopath += "%s/%s" % (branch, version)
         else:
