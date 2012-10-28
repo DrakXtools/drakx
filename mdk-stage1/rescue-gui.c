1ab4af9681755215a880f584420aedc58b7c901a
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-28 18:22:40 +0000

    oops, invoke the right shell...
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@275412 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- mdk-stage1/rescue-gui.c
+++ mdk-stage1/rescue-gui.c
@@ -224,7 +224,7 @@ int rescue_gui_main(int argc __attribute__ ((unused)), char *argv[] __attribute_
 			binary = "/usr/bin/rescue-doc";
 		}
 		if (ptr_begins_static_str(choice, go_to_console)) {
-			binary = "/home/peroyvind/Dokumenter/mandriva/drakx/images/tree/bin/login.bash";
+			binary = "/bin/sh";
 		}
 
 		/* Mandriva Flash entries */
