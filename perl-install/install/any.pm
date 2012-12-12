8dc7964428e87b040686bb3e516250c1705e6350
proyvind (Per Øyvind Karlsen) <peroyvind@mandriva.org>
2012-12-12 14:38:47 +0100

    From: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Thu, 6 Dec 2012 16:13:41 +0000 Subject: [PATCH] (media_screen) only display release & update media
    else there're too many media and it's unreadable
    
    git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@66145b7c3c08-40e5-403b-9995-ace06908e4af

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -325,6 +325,7 @@ sub media_screen {
     # - detect if non-free/tainted were selected previously / are now needed
     #   rpm -qa |grep tainted/non-free + check for kmod with firmwares
     # - use red color in that case (gtk+ version? interactive::gtk version?)
+    # - present media as trees (eg 3 main branches (core/nonfree/tainted and sub medium below (release/updates/...)
     my %descriptions = (
         'Main Release' => N("\"%s\" contains the various pieces of the systems and its applications.", _main_medium()),
         'Contrib Release' => N("\"%s\" contains software that's not officially supported and might not receive the same level of maintenance.", _contrib_medium()),
@@ -346,7 +347,7 @@ sub media_screen {
                 disabled => sub { $medium->{name} eq 'Core Release' },
                 format => sub { $descriptions{$_[0]} || translate(%descriptions) },
             };
-        } @{$urpm->{media}},
+        } grep { $_->{name} =~ /Release$|Updates$/ } @{$urpm->{media}},
     ]);
 
 
