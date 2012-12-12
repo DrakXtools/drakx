a48fb5f2415fcc732fdbea379bc116774dd60dee
proyvind (Per Øyvind Karlsen) <peroyvind@mandriva.org>
2012-12-12 15:02:33 +0100

    From: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Mon, 10 Dec 2012 03:33:12 +0000 Subject: [PATCH] (load_font) simplify
    git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@66615b7c3c08-40e5-403b-9995-ace06908e4af

--- perl-install/install/gtk.pm
+++ perl-install/install/gtk.pm
@@ -74,14 +74,8 @@ sub load_font {
 	$::WizardWindow->move($::rootwidth - $width - $x, $y);
     }
 
-    my $s = q(
-style "default-font" 
-{
-   font_name = ") . lang::l2pango_font($o->{locale}{lang}) . q("
-}
-widget "*" style "default-font"
-
-);
+    my $font = lang::l2pango_font($o->{locale}{lang});
+    my $s = qq(gtk-font-name="$font");
     Gtk2::Rc->parse_string($s);
     mkdir("/root");
     output("/root/.gtkrc-2.0", $s);
