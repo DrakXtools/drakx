f3266c4a0e5e88fc10dfa6afcf41bc45b8345fec
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-12-14 17:13:06 +0000

    (packagesToInstall) logs first packages to install
    git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@6780 5b7c3c08-40e5-403b-9995-ace06908e4af

--- perl-install/install/pkgs.pm
+++ perl-install/install/pkgs.pm
@@ -183,7 +183,7 @@ sub packagesToInstall {
 	log::l("examining packagesToInstall of medium $_->{name}");
 	push @packages, grep { $_->flag_selected } install::media::packagesOfMedium($packages, $_);
     }
-    log::l("found " . scalar(@packages) . " packages to install");
+    log::l("found " . scalar(@packages) . " packages to install: " . formatList(5, map { $_->fullname } @packages));
     @packages;
 }
 
