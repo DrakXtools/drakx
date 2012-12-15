27111afd7f55422581c6365033fc0f525816606c
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-12-15 01:30:37 +0000

    fix checking grub2 installation (mga#8390)
    git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@6785 5b7c3c08-40e5-403b-9995-ace06908e4af

--- perl-install/bootloader.pm
+++ perl-install/bootloader.pm
@@ -2025,9 +2025,10 @@ sub install {
 sub ensure_pkg_is_installed {
     my ($do_pkgs, $bootloader) = @_;
 
+    my %h = ('grub2' => 'grub2-install');
     my $main_method = main_method($bootloader->{method});
     if (member($main_method, qw(grub grub2 lilo))) {
-	$do_pkgs->ensure_binary_is_installed($main_method, $main_method, 1) or return 0;
+	$do_pkgs->ensure_binary_is_installed($main_method, $h{$main_method} || $main_method, 1) or return 0;
 	if ($bootloader->{method} eq 'grub-graphic') {
 	    $do_pkgs->ensure_is_installed('mandriva-gfxboot-theme', '/usr/share/gfxboot/themes/Mandriva/boot/message', 1) or return 0;
 	}
