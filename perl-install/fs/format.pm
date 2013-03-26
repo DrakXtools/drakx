DrakXtools/drakx

commit b1352ad3e4aca1881cfd75564aed64513867c99e

fix preserving UUID when formatting ext* and swap, and handle more FS (#39913)
git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx/trunk@252676 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

perl-install/fs/format.pm
@@ -151,8 +151,13 @@ sub part_raw {
         push @options, '-l', "Untitled";
     } elsif (isAppleBootstrap($part)) {
 	push @options, '-l', 'bootstrap';
-    } elsif (member($fs_type, 'swap', 'ext2', 'ext3', 'ext4')) {
+    }
+
+    # Preserve UUID
+    if (member($fs_type, 'swap', 'ext2', 'ext3', 'ext4')) {
 	push @options, '-U', $part->{device_UUID} if $part->{device_UUID};
+    } elsif ($fs_type eq 'reiserfs') {
+	push @options, '-u', $part->{device_UUID} if $part->{device_UUID};
     }
 
     if ($part->{device_LABEL}) {
@@ -169,6 +174,13 @@ sub part_raw {
 	run_program::raw({ timeout => 'never' }, @args) or die N("%s formatting of %s failed", $fs_type, $dev);
     }
 
+    # Preserve UUID on fs where we couldn't enforce it while formatting
+    if ($fs_type eq 'jfs') {
+	run_program::raw('jfs_tune', '-U', devices::make($dev));
+    } elsif ($fs_type eq 'xfs') {
+	run_program::raw('xfs_admin', '-U', devices::make($dev));
+    } 
+    
     if (member($fs_type, qw(ext3 ext4))) {
 	disable_forced_fsck($dev);
     }

commit 42214d7ac6b4a2e3aa79b4234f2f80b47c60274f

actually preserve UUID when formatting (mga#9428)
bug introduced in commit r252676 by pterjan on Tue Feb 10 2009:

"fix preserving UUID owhen formatting ext* and swap, and handle more
FS (#39913)"

git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@7591 5b7c3c08-40e5-403b-9995-ace06908e4af

perl-install/fs/format.pm
@@ -195,7 +195,7 @@ sub part_raw {
 
     # Preserve UUID on fs where we couldn't enforce it while formatting
     (undef, $cmd) = @{$preserve_UUID{$fs_type}};
-    run_program::raw($cmd, '-U', devices::make($dev)) if $cmd;
+    run_program::raw({}, $cmd, '-U', devices::make($dev)) if $cmd;
     
     if (member($fs_type, qw(ext3 ext4))) {
 	disable_forced_fsck($dev);