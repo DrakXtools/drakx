DrakXtools/drakx

commit 9bef5a4909b5d84af63b89c2f72e16d277825fad

fork displaying help, thus workarounding a webkit segfault (mga#9124)
git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@7467 5b7c3c08-40e5-403b-9995-ace06908e4af

perl-install/interactive/gtk.pm
@@ -807,15 +807,15 @@ sub get_html_file {
     $anchor ? "$url#$anchor" : $url;
 }
 
-sub display_help {
-    my ($o, $common, $mainw) = @_;
+sub display_help_window {
+    my ($o, $common) = @_;
     if (my $file = $common->{interactive_help_id}) {
         require Gtk2::WebKit;
         my $view = gtknew('WebKit_View');
 
         load_from_uri($view, $file);
 
-        my $w = ugtk2->new(N("Help"), transient => $mainw->{real_window}, modal => 1);
+        my $w = ugtk2->new(N("Help"), modal => 1);
         gtkadd($w->{rwindow},
                gtkpack_(Gtk2::VBox->new,
                         1, create_scrolled_window(gtkset_border_width($view, 5),
@@ -837,6 +837,16 @@ sub display_help {
     }
 }
 
+sub display_help {
+    my ($_o, $common) = @_;
+    # not very safe but we run in a restricted environment anyway:
+    my $f = '/tmp/help.txt';
+    if ($common->{interactive_help}) {
+       output($f, $common->{interactive_help}->());
+    }
+    system('display_installer_help', $common->{interactive_help_id} || $f); 
+}
+
 sub ask_fromW {
     my ($o, $common, $l) = @_;
 
@@ -855,7 +865,7 @@ sub ask_fromW {
     my @more_buttons = (
 			if_($common->{interactive_help} || $common->{interactive_help_id}, 
                             [ gtknew('Install_Button', text => N("Help"),
-                                     clicked => sub { display_help($o, $common, $mainw) }), undef, 1 ]),
+                                     clicked => sub { display_help($o, $common) }), undef, 1 ]),
 			if_($common->{more_buttons}, @{$common->{more_buttons}}),
 		       );
     my $buttons_pack = ($common->{ok} || !exists $common->{ok}) && $mainw->create_okcancel($common->{ok}, $common->{cancel}, '', @more_buttons);

perl-install/Makefile.config
@@ -9,7 +9,7 @@ SO_FILES    = c/blib/arch/auto/c/c.so
 PMS_DIRS    = c diskdrake harddrake interactive modules partition_table resize_fat security fs fs/remote
 
 PMS         = *.pm $(PMS_DIRS:%=%/*.pm) install/*.pm install/help/*.pm install/commands install/install2
-STANDALONEPMS_ = adduserdrake bootloader-config display_help display_release_notes.pl diskdrake drakauth drakboot drakbug drakbug_report drakclock drakedm drakdvb drakfont drakhelp drakperm draksec draksound drakupdate_fstab drakups drakxservices drakxtv fileshareset finish-install harddrake2 localedrake logdrake lsnetdrake notify-x11-free-driver-switch remove-unused-packages scannerdrake service_harddrake service_harddrake_confirm
+STANDALONEPMS_ = adduserdrake bootloader-config display_help display_installer_help display_release_notes.pl diskdrake drakauth drakboot drakbug drakbug_report drakclock drakedm drakdvb drakfont drakhelp drakperm draksec draksound drakupdate_fstab drakups drakxservices drakxtv fileshareset finish-install harddrake2 localedrake logdrake lsnetdrake notify-x11-free-driver-switch remove-unused-packages scannerdrake service_harddrake service_harddrake_confirm
 
 STANDALONEPMS = $(STANDALONEPMS_:%=standalone/%)
 ALLPMS	    = $(PMS) $(STANDALONEPMS)

perl-install/install/share/list.xml
@@ -423,6 +423,7 @@
 
 <from dir="../standalone">
   <to dir="/usr/bin">
+    display_installer_help
     display_release_notes.pl
   </to>
 </from>

commit 8b8f6366c4a15de2e4de6614831f4100cf38f448

fix finding translated help (mga#9619)
regression introduced in commit r7467 on 2013-04-04:
"fork displaying help, thus workarounding a webkit segfault (mga#9124)"

git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@7784 5b7c3c08-40e5-403b-9995-ace06908e4af

perl-install/interactive/gtk.pm
@@ -838,13 +838,13 @@ sub display_help_window {
 }
 
 sub display_help {
-    my ($_o, $common) = @_;
+    my ($o, $common) = @_;
     # not very safe but we run in a restricted environment anyway:
     my $f = '/tmp/help.txt';
     if ($common->{interactive_help}) {
        output($f, $common->{interactive_help}->());
     }
-    system('display_installer_help', $common->{interactive_help_id} || $f); 
+    system('display_installer_help', $common->{interactive_help_id} || $f, $o->{locale}{lang}); 
 }
 
 sub ask_fromW {

perl-install/standalone/display_installer_help
@@ -25,4 +25,6 @@ my $in = 'interactive'->vnew('su');
 # so that we popup above drakx:
 any::set_wm_hints_if_needed($in);
 
+# Fake enough $o for retrieving the proper translation:
+$o->{locale}{lang} = $ARGV[1];
 $in->display_help_window({ interactive_help_id => $ARGV[0] });