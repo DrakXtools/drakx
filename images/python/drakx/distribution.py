ad4b46b720d94f103cad3ba35ce7d942513631e3
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2013-04-08 03:38:01 +0200

    fix check

--- images/python/drakx/distribution.py
+++ images/python/drakx/distribution.py
@@ -6,6 +6,10 @@ perl.require("urpm::select")
 
 class Distribution(object):
     def __init__(self, config, arch, media, includelist, excludelist, rpmsrate, compssusers, filedeps, suggests = False, synthfilter = ".cz:gzip -9", stage1=None, stage2="../mdkinst.cpio.xz", advertising="/usr/lib/drakx-installer/root/install/extra/advertising/"):
+        volumeid = ("%s-%s-%s-%s" % (config.vendor, config.product, config.version, arch)).upper()
+        if len(volumeid) > 32:
+            print "length of volumeid '%s' (%d) > 32" % (volumeid, len(volumeid))
+            exit(1)
         self.arch = arch
         self.media = {}
         for m in media:
