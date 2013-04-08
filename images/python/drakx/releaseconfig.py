d0510734f911d59a87ed7db4f4e91de68c348750
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2013-04-08 03:30:46 +0200

    check length of product name

--- images/python/drakx/releaseconfig.py
+++ images/python/drakx/releaseconfig.py
@@ -2,6 +2,9 @@ class ReleaseConfig(object):
     def __init__(self, version, codename, product, subversion = None, outdir="out", branch = "devel", repopath = None, medium = "DVD", vendor = "Moondrake", distribution = "Moondrake GNU/Linux"):
         self.version = version
         self.codename = codename
+        if len(product) > 32:
+            print "length of product name '%s' (%d) > 32" % (product, len(product))
+            exit 1
         self.product = product
         self.subversion = subversion
         self.medium = medium
