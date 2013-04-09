d8e506dd9b12f4891ae45c1dd02b9f4ad5b0ba10
Per Øyvind Karlsen (proyvind) <peroyvind@mandriva.org>
2013-04-09 10:28:50 +0200

    bump version :p

--- perl-install/install/steps.pm
+++ perl-install/install/steps.pm
@@ -501,7 +501,7 @@ Consoles 1,3,4,7 may also contain interesting information";
 	}
     }
     log::l("fix hostname");
-    system("echo mdv2012 &> $::prefix/etc/hostname");
+    system("echo mdk2013 &> $::prefix/etc/hostname");
     
     log::l("fix missing /media/cdrom");
     run_program::rooted($::prefix, "mkdir", "/media/cdrom");
