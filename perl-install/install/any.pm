565c8b1979f3bb8bf6a993e224eb4bc3e559d665
tv@5b7c3c08-40e5-403b-9995-ace06908e4af <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2013-05-01 20:40:56 +0000

    install 'basesystem-minimal' instead of 'basesystem' prior to add extra media
    thus fixing "basesystem package not selected" (mga#9611)
    
    git-svn-id: svn://svn.mageia.org/soft/drakx/trunk@8110 5b7c3c08-40e5-403b-9995-ace06908e4af

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -148,13 +148,13 @@ sub prep_net_suppl_media {
     return if our $net_suppl_media_configured && network::tools::has_network_connection();
     $net_suppl_media_configured = 1;
 
-    # needed so that one can install basesystem before adding suppl network media:
+    # needed so that one can install basesystem-minimal before adding suppl network media:
     install::media::update_media($o->{packages});
     require urpm::media;
     urpm::media::configure($o->{packages});
 
-    #- install basesystem now
-    $o->do_pkgs->ensure_is_installed('basesystem', undef, 1);
+    #- install basesystem-minimal now
+    $o->do_pkgs->ensure_is_installed('basesystem-minimal', undef, 1);
 
     # in case of no network install:
     $o->{net} ||= {};
