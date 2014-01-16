5b38139c7bb2f15d585ae91226a6082115e33fd8
Thierry Vignaud <thierry.vignaud@gmail.com>
2014-01-16 21:48:40 +0100

    do not show labels on partition buttons (mga#11988)
    should prevent window to move at install time

--- perl-install/diskdrake/hd_gtk.pm
+++ perl-install/diskdrake/hd_gtk.pm
@@ -352,8 +352,14 @@ sub create_buttons4partitions {
 	}
 	my $info = $entry->{mntpoint} || $entry->{device_LABEL} || '';
 	$info .= "\n" . ($entry->{size} ? formatXiB($entry->{size}, 512) : N("Unknown")) if $info;
-	my $w = Gtk3::ToggleButton->new_with_label($info) or internal_error('new_with_label');
-	$w->get_child->set_ellipsize('end');
+	my $w;
+	if ($::isInstall) {
+	    $w = Gtk3::ToggleButton->new();
+	}else {
+	    $w = Gtk3::ToggleButton->new_with_label($info);
+	    $w->get_child->set_ellipsize('end');
+	}
+	$w or internal_error('new_with_label');
 	$w->set_tooltip_text($info);
 	$w->signal_connect(clicked => sub { 
 	    $current_button != $w or return;
