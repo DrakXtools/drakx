b009dd1c95fb2fee8ed80f4c4430481e885ed55c
Thierry Vignaud <thierry.vignaud@gmail.com>
2014-02-19 23:20:55 +0100

    fix log message

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -663,7 +663,7 @@ sub default_packages {
     add_n_log("have crypted DM", "cryptsetup") if !is_empty_array_ref($o->{all_hds}{dmcrypts});
     add_n_log("some disks are fake RAID", qw(mdadm dmraid)) if any { fs::type::is_dmraid($_) } @{$o->{all_hds}{hds}};
     add_n_log("CPU needs microcode", "microcode_ctl") if detect_devices::hasCPUMicrocode();
-    add_n_log("CPU needs cpufreq", 'cpufreq') if detect_devices::hasCPUFreq();
+    add_n_log("CPU needs cpupower", 'cpupower') if detect_devices::hasCPUFreq();
     add_n_log("APM support needed", 'apmd') if -e "/proc/apm";
     add_n_log("needed by hardware", detect_devices::probe_name('Pkg'));
     my @ltmp = map { $_->{BOOTPROTO} eq 'dhcp' ? $_->{DHCP_CLIENT} || 'dhcpcd' : () } values %{$o->{net}{ifcfg}};
