f6e4c079458272ce6e0e2ed075faa8fb48d01d8f
Thomas Backlund <tmb@mageia.org>
2015-01-09 13:15:53 +0159

    add POD doc for is_uefi() and is_mgalive()

--- perl-install/common.pm
+++ perl-install/common.pm
@@ -711,10 +711,20 @@ sub update_gnomekderc_no_create {
     }
 }
 
-# check if we are in mga live mode
+=item is_mgalive()
+
+Checks if we are in mga live mode
+
+=cut
+
 sub is_mgalive { -e "/run/mgalive" }
 
-# check if we are on an uefi system
+=item is_uefi()
+
+Checks if we are on an uefi system
+
+=cut
+
 sub is_uefi { -e "/sys/firmware/efi" }
 
 =item cmp_kernel_versions($va, $vb)
