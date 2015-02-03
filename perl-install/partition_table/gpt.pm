d5afb7c1c420224dcfd5a59d19e669dfd3b3bc56
Pascal Terjan <pterjan@gmail.com>
2015-02-03 22:43:25 +0000

    Fix partition number in GPT for empty disks

--- perl-install/partition_table/gpt.pm
+++ perl-install/partition_table/gpt.pm
@@ -155,7 +155,8 @@ sub initialize {
     my ($class, $hd) = @_;
     my @raw;
     for (my $part_number = 0; $part_number < $nb_primary-1; $part_number++) {
-	$raw[$part_number] = { part_number => $part_number };
+        # part_number starts at 1
+        $raw[$part_number] = { part_number => $part_number + 1};
     }
     $hd->{primary} = { raw => \@raw };
     bless $hd, $class;
