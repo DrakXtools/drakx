7eaf70188fc2dc7f510b40584a54da38fb20c413
Martin Whitaker <mageia@martin-whitaker.me.uk>
2015-05-25 05:33:37 -0400

    fix urpmi media for USB install (mga#5690)
    v2: (tv) handle both cases

--- perl-install/install/media.pm
+++ perl-install/install/media.pm
@@ -532,8 +532,8 @@ sub adjust_paths_in_urpmi_cfg {
             $medium->{url} =~ s!^.*?/media/!$phys_m->{url}/!;
         } elsif (member($phys_m->{method}, qw(disk nfs))) {
             # use the real mount point:
-            if ($medium->{url} =~ m!/tmp/image!) {
-                $medium->{url} =~ s!/tmp/image!$phys_m->{mntpoint}!;
+            if ($medium->{url} =~ m!/tmp/image(/media)?!) {
+                $medium->{url} =~ s!/tmp/image(/media).!$phys_m->{mntpoint}$phys_m->{rel_path}!;
             } else {
                 # just remove $::prefix and we already have the real mount point:
                 $medium->{url} =~ s!^$::prefix!!;
