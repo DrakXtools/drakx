e4ea92003220928001e592c982ea5b44b160abe8
Thomas Backlund <tmb@mageia.org>
2016-05-07 16:10:28 +0300

    install nonfree firmware for amdgpu, i915, nouveau

--- perl-install/list_firmwares.pm
+++ perl-install/list_firmwares.pm
@@ -3,12 +3,12 @@
 package list_firmwares;
 
 our @modules_with_nonfree_firmware = 
-    qw(acenic advansys ambassador ar5523 at76c50x-usb ath3k ath6kl_sdio
+    qw(acenic advansys ambassador amdgpu ar5523 at76c50x-usb ath3k ath6kl_sdio
     ath6kl_usb ath9k_htc atmel bcm203x bnx2 bnx2x brcmfmac brcmsmac btmrvl_sdio
     cassini cpia2 csiostor cx18 cx25840 cxgb3 cxgb4 dvb-ttusb-budget dvb-usb-dib0700
-    e100 emi26 emi62 gspca_vicam i2400m-usb ib_qib io_edgeport io_ti ipw2100
+    e100 emi26 emi62 gspca_vicam i2400m-usb i915 ib_qib io_edgeport io_ti ipw2100
     ipw2200 ivtv iwl3945 iwl4965 iwlwifi kaweth keyspan lgs8gxx libertas_cs
-    libertas_sdio mga mwifiex_sdio mwifiex_usb mwl8k myri10ge netxen_nic orinoco
+    libertas_sdio mga mwifiex_sdio mwifiex_usb mwl8k myri10ge netxen_nic nouveau orinoco
     poseidon pvrusb2 qla1280 qla2xxx qlcnic r8169 r8192e_pci r8712u radeon
     rt2800pci rt2800usb rt61pci rt73usb rtl8192ce rtl8192cu rtl8192de rtl8192se
     s2250-loader s2255drv slicoss smc91c92_cs snd-korg1212 snd-maestro3 snd-ymfpci
