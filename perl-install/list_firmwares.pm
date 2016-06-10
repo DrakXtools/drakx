2b9421e87b13a4a3c5871e56316cca72544116a8
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-06-10 15:02:47 +0200

    filter out dobles

--- perl-install/list_firmwares.pm
+++ perl-install/list_firmwares.pm
@@ -4,33 +4,20 @@
 package list_firmwares;
 
 our @modules_with_nonfree_firmware = 
-    qw(acenic acenic advansys advansys ambassador ambassador amdgpu amdgpu
-    ar5523 ar5523 at76c50x-usb at76c50x-usb ath10k_pci ath10k_pci ath3k ath3k
-    ath6kl_sdio ath6kl_sdio ath6kl_usb ath6kl_usb ath9k_htc ath9k_htc atmel
-    atmel bcm203x bcm203x bfa bfa bna bna bnx2 bnx2 bnx2x bnx2x brcmfmac brcmfmac
-    brcmsmac brcmsmac btintel btintel btmrvl_sdio btmrvl_sdio cassini cassini
-    cpia2 cpia2 csiostor csiostor cx18 cx18 cx25840 cx25840 cxgb3 cxgb3 cxgb4
-    cxgb4 dvb-ttusb-budget dvb-ttusb-budget dvb-usb-af9035 dvb-usb-af9035 dvb-usb-dib0700
-    dvb-usb-dib0700 e100 e100 emi26 emi26 emi62 emi62 go7007 go7007 go7007-loader
-    go7007-loader gspca_vicam gspca_vicam i2400m-usb i2400m-usb i915 i915 ib_qib
-    ib_qib io_edgeport io_edgeport io_ti io_ti ipw2100 ipw2100 ipw2200 ipw2200
-    ivtv ivtv iwl3945 iwl3945 iwl4965 iwl4965 iwlwifi iwlwifi kaweth kaweth
-    keyspan keyspan lgs8gxx lgs8gxx libertas_cs libertas_cs libertas_sdio libertas_sdio
-    libertas_spi libertas_spi mga mga mt7601u mt7601u mwifiex_pcie mwifiex_pcie
-    mwifiex_sdio mwifiex_sdio mwifiex_usb mwifiex_usb mwl8k mwl8k myri10ge
-    myri10ge netxen_nic netxen_nic nouveau nouveau orinoco orinoco pvrusb2
-    pvrusb2 qat_dh895xcc qat_dh895xcc qed qed qla1280 qla1280 qla2xxx qla2xxx
-    qlcnic qlcnic r8169 r8169 r8192e_pci r8192e_pci r8712u r8712u r8723au r8723au
-    radeon radeon rp2 rp2 rsi_sdio rsi_sdio rsi_usb rsi_usb rt2800pci rt2800pci
-    rt2800usb rt2800usb rt61pci rt61pci rt73usb rt73usb rtl8188ee rtl8188ee
-    rtl8192ce rtl8192ce rtl8192cu rtl8192cu rtl8192de rtl8192de rtl8192ee rtl8192ee
-    rtl8192se rtl8192se rtl8723be rtl8723be rtl8821ae rtl8821ae rtl8xxxu rtl8xxxu
-    s2255drv s2255drv saa7134-go7007 saa7134-go7007 slicoss slicoss smc91c92_cs
-    smc91c92_cs smsmdtv smsmdtv snd-hda-codec-ca0132 snd-hda-codec-ca0132 snd-korg1212
-    snd-korg1212 snd-maestro3 snd-maestro3 snd-ymfpci snd-ymfpci starfire starfire
-    tehuti tehuti tg3 tg3 ti_usb_3410_5052 ti_usb_3410_5052 typhoon typhoon
-    ueagle-atm ueagle-atm ums-eneub6250 ums-eneub6250 usb8xxx usb8xxx vt6656_stage
-    vt6656_stage whiteheat whiteheat wl1251 wl1251 wl12xx wl12xx wl18xx wl18xx
-    wlcore wlcore xc4000 xc4000 xc5000 xc5000 yam yam);
+    qw(acenic advansys ambassador amdgpu ar5523 at76c50x-usb ath10k_pci
+    ath3k ath6kl_sdio ath6kl_usb ath9k_htc atmel bcm203x bfa bna bnx2 bnx2x
+    brcmfmac brcmsmac btintel btmrvl_sdio cassini cpia2 csiostor cx18 cx25840
+    cxgb3 cxgb4 dvb-ttusb-budget dvb-usb-af9035 dvb-usb-dib0700 e100 emi26
+    emi62 go7007 go7007-loader gspca_vicam i2400m-usb i915 ib_qib io_edgeport
+    io_ti ipw2100 ipw2200 ivtv iwl3945 iwl4965 iwlwifi kaweth keyspan lgs8gxx
+    libertas_cs libertas_sdio libertas_spi mga mt7601u mwifiex_pcie mwifiex_sdio
+    mwifiex_usb mwl8k myri10ge netxen_nic nouveau orinoco pvrusb2 qat_dh895xcc
+    qed qla1280 qla2xxx qlcnic r8169 r8192e_pci r8712u r8723au radeon rp2 rsi_sdio
+    rsi_usb rt2800pci rt2800usb rt61pci rt73usb rtl8188ee rtl8192ce rtl8192cu
+    rtl8192de rtl8192ee rtl8192se rtl8723be rtl8821ae rtl8xxxu s2255drv saa7134-go7007
+    slicoss smc91c92_cs smsmdtv snd-hda-codec-ca0132 snd-korg1212 snd-maestro3
+    snd-ymfpci starfire tehuti tg3 ti_usb_3410_5052 typhoon ueagle-atm ums-eneub6250
+    usb8xxx vt6656_stage whiteheat wl1251 wl12xx wl18xx wlcore xc4000 xc5000
+    yam);
 
 1;
