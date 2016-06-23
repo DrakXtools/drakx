a1a4d116eccb143a55ec2d20fe2e09206dc709ec
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-06-24 01:43:20 +0200

    kill an old useless ia64 check

--- perl-install/any.pm
+++ perl-install/any.pm
@@ -438,11 +438,8 @@ sub setupBootloader__general {
 			{ label => N("Bootloader"), title => 1 },
 			{ label => N("Bootloader to use"), val => \$b->{method},
 				list => \@method_choices, format => \&bootloader::method2text },
-			if_(arch() !~ /ia64/,
-				{ label => N("Boot device"), val => \$b->{boot}, list => \@boot_devices,
-
-					format => sub { $boot_devices{$_[0]} }, disabled => sub { is_uefi() } },
-			),
+			{ label => N("Boot device"), val => \$b->{boot}, list => \@boot_devices,
+				format => sub { $boot_devices{$_[0]} }, disabled => sub { is_uefi() } },
 			{ label => N("Main options"), title => 1 },
 			{ label => N("Delay before booting default image"), val => \$b->{timeout} },
 			{ text => N("Enable ACPI"), val => \$force_acpi, type => 'bool', advanced => 1 },
