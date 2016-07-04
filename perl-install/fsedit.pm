06bd87af6f2d1239687fccdc7fa3a82afdf80541
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-07-04 17:42:41 +0200

    do not warn about no bootloader can boot RAID[^1]
    As grub2 can boot... (mga#11324)
    This should also fix mga#9524
    And thus stop forcing obsolete metadata 0.90 format

--- perl-install/fsedit.pm
+++ perl-install/fsedit.pm
@@ -451,33 +451,6 @@ sub check_mntpoint {
     $mntpoint =~ m|[\x7f-\xff]| and cdie N("Mount points should contain only alphanumerical characters");
     fs::get::mntpoint2part($mntpoint, [ grep { $_ ne $part } fs::get::really_all_fstab($all_hds) ]) and die N("There is already a partition with mount point %s\n", $mntpoint);
 
-    if ($mntpoint eq "/" && isRAID($part) && !fs::get::has_mntpoint("/boot", $all_hds)) {
-	# lilo handles / on RAID1
-	if ($part->{level} ne '1') {
-	    cdie N("You've selected a software RAID partition as root (/).
-No bootloader is able to handle this without a /boot partition.
-Please be sure to add a separate /boot partition");
-	} else {
-	    # LILO only handles 0.90 metadata
-	    if ($part->{isFormatted} && $part->{metadata} && $part->{metadata} ne '0.90') {
-		cdie N("Metadata version unsupported for a boot partition. Please be sure to add a separate /boot partition.");
-	    } else {
-		$part->{metadata} = '0.90';
-	    }
-	}
-    }
-
-    if ($mntpoint eq "/boot" && isRAID($part))  {
-	die N("You've selected a software RAID partition as /boot.
-No bootloader is able to handle this.") if $part->{level} ne '1'; # lilo handles /boot on RAID1
-	# LILO only handles 0.90 metadata
-	if ($part->{isFormatted} && $part->{metadata} && $part->{metadata} ne '0.90') {
-	    die N("Metadata version unsupported for a boot partition.");
-	} else {
-	    $part->{metadata} = '0.90';
-	}
-    }
-
     if ($mntpoint eq "/" && (isLUKS($part) || isRawLUKS($part)) && !fs::get::has_mntpoint("/boot", $all_hds)) {
 	cdie N("You've selected an encrypted partition as root (/).
 No bootloader is able to handle this without a /boot partition.
