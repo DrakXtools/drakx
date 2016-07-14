e86040cfe05750ea1ec50930cf8738e3617fd4a1
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-07-14 23:14:32 +0200

    do not preinstall mageia-gfxboot-theme

--- perl-install/install/any.pm
+++ perl-install/install/any.pm
@@ -842,7 +842,7 @@ sub default_packages {
     my @ltmp = map { $_->{BOOTPROTO} eq 'dhcp' ? $_->{DHCP_CLIENT} || 'dhcpcd' : () } values %{$o->{net}{ifcfg}};
     add_n_log("needed by networking", @ltmp) if @ltmp;
     # will get auto selected at summary stage for bootloader:
-    add_n_log("needed later at summary stage", qw(acpi acpid mandriva-gfxboot-theme));
+    add_n_log("needed later at summary stage", qw(acpi acpid));
     # will get auto selected at summary stage for firewall:
     add_n_log("needed for firewall/security", qw(shorewall shorewall-ipv6 mandi-ifw));
     # only needed for CDs/DVDs installations:
