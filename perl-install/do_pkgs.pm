5e06e81087d15d4481afa08f3bf30bcc292d6f49
Martin Whitaker <mageia@martin-whitaker.me.uk>
2017-04-02 11:54:16 +0100

    Clean up detection of Live install root in do_pkgs_standalone::install().
    Now we have $::isLiveInstall, we can use it. Also use $::prefix
    instead of hardcoding the path.

--- perl-install/do_pkgs.pm
+++ perl-install/do_pkgs.pm
@@ -333,7 +333,7 @@ sub install {
 	return 1;
     }
 
-    my @wrapper = is_mgalive() && -e '/mnt/install' ? qw(chroot /mnt/install) : ();
+    my @wrapper = $::isLiveInstall && $::prefix ? ('chroot', $::prefix) : ();
     my @options = ('--allow-medium-change', '--auto', '--no-verify-rpm', '--expect-install', @l);
     my $ret;
     if (check_for_xserver() && -x '/usr/bin/gurpmi') {
